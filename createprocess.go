acea72d04cb8bf2f9b8e1218c58f828df1019080
John Starks <jostarks@microsoft.com>
2016-01-29 15:39:11 -0800

    Perform std handle pipe IO without blocking system threads
    This should reduce the thread pressure on the docker daemon, which
    may help reduce crashes when there are many containers running.

--- createprocess.go
+++ createprocess.go
@@ -3,9 +3,9 @@ package hcsshim
 import (
 	"encoding/json"
 	"io"
-	"runtime"
 	"syscall"
 
+	"github.com/Microsoft/go-winio"
 	"github.com/Sirupsen/logrus"
 )
 
@@ -20,54 +20,38 @@ type CreateProcessParams struct {
 	ConsoleSize      [2]int
 }
 
-// pipe struct used for the stdin/stdout/stderr pipes
-type pipe struct {
-	handle syscall.Handle
-}
-
-func makePipe(h syscall.Handle) *pipe {
-	p := &pipe{h}
-	runtime.SetFinalizer(p, (*pipe).closeHandle)
-	return p
-}
-
-func (p *pipe) closeHandle() {
-	if p.handle != 0 {
-		syscall.CloseHandle(p.handle)
-		p.handle = 0
-	}
-}
-
-func (p *pipe) Close() error {
-	p.closeHandle()
-	runtime.SetFinalizer(p, nil)
-	return nil
-}
-
-func (p *pipe) Read(b []byte) (int, error) {
-	// syscall.Read returns 0, nil on ERROR_BROKEN_PIPE, but for
-	// our purposes this should indicate EOF. This may be a go bug.
-	var read uint32
-	err := syscall.ReadFile(p.handle, b, &read, nil)
-	if err != nil {
-		if err == syscall.ERROR_BROKEN_PIPE {
-			return 0, io.EOF
+// makeOpenFiles calls winio.MakeOpenFile for each handle in a slice but closes all the handles
+// if there is an error.
+func makeOpenFiles(hs []syscall.Handle) (_ []io.ReadWriteCloser, err error) {
+	fs := make([]io.ReadWriteCloser, len(hs))
+	for i, h := range hs {
+		if h != syscall.Handle(0) {
+			if err == nil {
+				fs[i], err = winio.MakeOpenFile(h)
+			}
+			if err != nil {
+				syscall.Close(h)
+			}
 		}
-		return 0, err
 	}
-	return int(read), nil
-}
-
-func (p *pipe) Write(b []byte) (int, error) {
-	return syscall.Write(p.handle, b)
+	if err != nil {
+		for _, f := range fs {
+			if f != nil {
+				f.Close()
+			}
+		}
+		return nil, err
+	}
+	return fs, nil
 }
 
 // CreateProcessInComputeSystem starts a process in a container. This is invoked, for example,
 // as a result of docker run, docker exec, or RUN in Dockerfile. If successful,
 // it returns the PID of the process.
-func CreateProcessInComputeSystem(id string, useStdin bool, useStdout bool, useStderr bool, params CreateProcessParams) (uint32, io.WriteCloser, io.ReadCloser, io.ReadCloser, uint32, error) {
+func CreateProcessInComputeSystem(id string, useStdin bool, useStdout bool, useStderr bool, params CreateProcessParams) (_ uint32, _ io.WriteCloser, _ io.ReadCloser, _ io.ReadCloser, hr uint32, err error) {
 	title := "HCSShim::CreateProcessInComputeSystem"
 	logrus.Debugf(title+" id=%s", id)
+	hr = 0xFFFFFFFF
 
 	// If we are not emulating a console, ignore any console size passed to us
 	if !params.EmulateConsole {
@@ -77,51 +61,43 @@ func CreateProcessInComputeSystem(id string, useStdin bool, useStdout bool, useS
 
 	paramsJson, err := json.Marshal(params)
 	if err != nil {
-		return 0, nil, nil, nil, 0xFFFFFFFF, err
+		return
 	}
-
-	var pid uint32
 
 	logrus.Debugf(title+" - Calling Win32 %s %s", id, paramsJson)
 
-	var stdinHandle, stdoutHandle, stderrHandle syscall.Handle
+	var pid uint32
+
+	handles := make([]syscall.Handle, 3)
 	var stdinParam, stdoutParam, stderrParam *syscall.Handle
 	if useStdin {
-		stdinParam = &stdinHandle
+		stdinParam = &handles[0]
 	}
 	if useStdout {
-		stdoutParam = &stdoutHandle
+		stdoutParam = &handles[1]
 	}
 	if useStderr {
-		stderrParam = &stderrHandle
+		stderrParam = &handles[2]
 	}
 
 	err = createProcessWithStdHandlesInComputeSystem(id, string(paramsJson), &pid, stdinParam, stdoutParam, stderrParam)
 	if err != nil {
-		err := makeErrorf(err, title, "id=%s params=%v", id, params)
+		winerr := makeErrorf(err, title, "id=%s params=%v", id, params)
+		hr = winerr.HResult()
 		// Windows TP4: Hyper-V Containers may return this error with more than one
 		// concurrent exec. Do not log it as an error
-		if err.HResult() != Win32InvalidArgument {
-			logrus.Error(err)
+		if hr != Win32InvalidArgument {
+			logrus.Error(winerr)
 		}
-		return 0, nil, nil, nil, err.HResult(), err
+		err = winerr
+		return
 	}
 
-	var (
-		stdin          io.WriteCloser
-		stdout, stderr io.ReadCloser
-	)
-
-	if useStdin {
-		stdin = makePipe(stdinHandle)
-	}
-	if useStdout {
-		stdout = makePipe(stdoutHandle)
-	}
-	if useStderr {
-		stderr = makePipe(stderrHandle)
+	pipes, err := makeOpenFiles(handles)
+	if err != nil {
+		return
 	}
 
 	logrus.Debugf(title+" - succeeded id=%s params=%s pid=%d", id, paramsJson, pid)
-	return pid, stdin, stdout, stderr, 0, nil
+	return pid, pipes[0], pipes[1], pipes[2], 0, nil
 }
--- zhcsshim.go
+++ zhcsshim.go
@@ -8,9 +8,10 @@ import "syscall"
 var _ unsafe.Pointer
 
 var (
-	modvmcompute = syscall.NewLazyDLL("vmcompute.dll")
 	modole32     = syscall.NewLazyDLL("ole32.dll")
+	modvmcompute = syscall.NewLazyDLL("vmcompute.dll")
 
+	procCoTaskMemFree                              = modole32.NewProc("CoTaskMemFree")
 	procActivateLayer                              = modvmcompute.NewProc("ActivateLayer")
 	procCopyLayer                                  = modvmcompute.NewProc("CopyLayer")
 	procCreateLayer                                = modvmcompute.NewProc("CreateLayer")
@@ -25,7 +26,6 @@ var (
 	procNameToGuid                                 = modvmcompute.NewProc("NameToGuid")
 	procPrepareLayer                               = modvmcompute.NewProc("PrepareLayer")
 	procUnprepareLayer                             = modvmcompute.NewProc("UnprepareLayer")
-	procCoTaskMemFree                              = modole32.NewProc("CoTaskMemFree")
 	procCreateComputeSystem                        = modvmcompute.NewProc("CreateComputeSystem")
 	procCreateProcessWithStdHandlesInComputeSystem = modvmcompute.NewProc("CreateProcessWithStdHandlesInComputeSystem")
 	procResizeConsoleInComputeSystem               = modvmcompute.NewProc("ResizeConsoleInComputeSystem")
@@ -37,6 +37,11 @@ var (
 	procHNSCall                                    = modvmcompute.NewProc("HNSCall")
 )
 
+func coTaskMemFree(buffer unsafe.Pointer) {
+	syscall.Syscall(procCoTaskMemFree.Addr(), 1, uintptr(buffer), 0, 0)
+	return
+}
+
 func activateLayer(info *driverInfo, id string) (hr error) {
 	var _p0 *uint16
 	_p0, hr = syscall.UTF16PtrFromString(id)
@@ -47,10 +52,9 @@ func activateLayer(info *driverInfo, id string) (hr error) {
 }
 
 func _activateLayer(info *driverInfo, id *uint16) (hr error) {
-	if _perr := procActivateLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procActivateLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procActivateLayer.Addr(), 2, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -77,10 +81,9 @@ func _copyLayer(info *driverInfo, srcId *uint16, dstId *uint16, descriptors []WC
 	if len(descriptors) > 0 {
 		_p2 = &descriptors[0]
 	}
-	if _perr := procCopyLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procCopyLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procCopyLayer.Addr(), 5, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(srcId)), uintptr(unsafe.Pointer(dstId)), uintptr(unsafe.Pointer(_p2)), uintptr(len(descriptors)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -103,10 +106,9 @@ func createLayer(info *driverInfo, id string, parent string) (hr error) {
 }
 
 func _createLayer(info *driverInfo, id *uint16, parent *uint16) (hr error) {
-	if _perr := procCreateLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procCreateLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procCreateLayer.Addr(), 3, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), uintptr(unsafe.Pointer(parent)))
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -133,10 +135,9 @@ func _createSandboxLayer(info *driverInfo, id *uint16, parent *uint16, descripto
 	if len(descriptors) > 0 {
 		_p2 = &descriptors[0]
 	}
-	if _perr := procCreateSandboxLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procCreateSandboxLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procCreateSandboxLayer.Addr(), 5, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), uintptr(unsafe.Pointer(parent)), uintptr(unsafe.Pointer(_p2)), uintptr(len(descriptors)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -154,10 +155,9 @@ func deactivateLayer(info *driverInfo, id string) (hr error) {
 }
 
 func _deactivateLayer(info *driverInfo, id *uint16) (hr error) {
-	if _perr := procDeactivateLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procDeactivateLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procDeactivateLayer.Addr(), 2, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -175,10 +175,9 @@ func destroyLayer(info *driverInfo, id string) (hr error) {
 }
 
 func _destroyLayer(info *driverInfo, id *uint16) (hr error) {
-	if _perr := procDestroyLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procDestroyLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procDestroyLayer.Addr(), 2, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -205,10 +204,9 @@ func _exportLayer(info *driverInfo, id *uint16, path *uint16, descriptors []WC_L
 	if len(descriptors) > 0 {
 		_p2 = &descriptors[0]
 	}
-	if _perr := procExportLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procExportLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procExportLayer.Addr(), 5, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), uintptr(unsafe.Pointer(path)), uintptr(unsafe.Pointer(_p2)), uintptr(len(descriptors)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -226,10 +224,9 @@ func getLayerMountPath(info *driverInfo, id string, length *uintptr, buffer *uin
 }
 
 func _getLayerMountPath(info *driverInfo, id *uint16, length *uintptr, buffer *uint16) (hr error) {
-	if _perr := procGetLayerMountPath.Find(); _perr != nil {
-		return _perr
+	if hr = procGetLayerMountPath.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procGetLayerMountPath.Addr(), 4, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), uintptr(unsafe.Pointer(length)), uintptr(unsafe.Pointer(buffer)), 0, 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -238,10 +235,9 @@ func _getLayerMountPath(info *driverInfo, id *uint16, length *uintptr, buffer *u
 }
 
 func getBaseImages(buffer **uint16) (hr error) {
-	if _perr := procGetBaseImages.Find(); _perr != nil {
-		return _perr
+	if hr = procGetBaseImages.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procGetBaseImages.Addr(), 1, uintptr(unsafe.Pointer(buffer)), 0, 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -268,10 +264,9 @@ func _importLayer(info *driverInfo, id *uint16, path *uint16, descriptors []WC_L
 	if len(descriptors) > 0 {
 		_p2 = &descriptors[0]
 	}
-	if _perr := procImportLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procImportLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procImportLayer.Addr(), 5, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), uintptr(unsafe.Pointer(path)), uintptr(unsafe.Pointer(_p2)), uintptr(len(descriptors)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -289,10 +284,9 @@ func layerExists(info *driverInfo, id string, exists *uint32) (hr error) {
 }
 
 func _layerExists(info *driverInfo, id *uint16, exists *uint32) (hr error) {
-	if _perr := procLayerExists.Find(); _perr != nil {
-		return _perr
+	if hr = procLayerExists.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procLayerExists.Addr(), 3, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), uintptr(unsafe.Pointer(exists)))
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -310,10 +304,9 @@ func nameToGuid(name string, guid *GUID) (hr error) {
 }
 
 func _nameToGuid(name *uint16, guid *GUID) (hr error) {
-	if _perr := procNameToGuid.Find(); _perr != nil {
-		return _perr
+	if hr = procNameToGuid.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procNameToGuid.Addr(), 2, uintptr(unsafe.Pointer(name)), uintptr(unsafe.Pointer(guid)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -335,10 +328,9 @@ func _prepareLayer(info *driverInfo, id *uint16, descriptors []WC_LAYER_DESCRIPT
 	if len(descriptors) > 0 {
 		_p1 = &descriptors[0]
 	}
-	if _perr := procPrepareLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procPrepareLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procPrepareLayer.Addr(), 4, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), uintptr(unsafe.Pointer(_p1)), uintptr(len(descriptors)), 0, 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -356,20 +348,13 @@ func unprepareLayer(info *driverInfo, id string) (hr error) {
 }
 
 func _unprepareLayer(info *driverInfo, id *uint16) (hr error) {
-	if _perr := procUnprepareLayer.Find(); _perr != nil {
-		return _perr
+	if hr = procUnprepareLayer.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procUnprepareLayer.Addr(), 2, uintptr(unsafe.Pointer(info)), uintptr(unsafe.Pointer(id)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
 	}
-	return
-}
-
-func coTaskMemFree(buffer unsafe.Pointer) {
-
-	syscall.Syscall(procCoTaskMemFree.Addr(), 1, uintptr(buffer), 0, 0)
 	return
 }
 
@@ -388,10 +373,9 @@ func createComputeSystem(id string, configuration string) (hr error) {
 }
 
 func _createComputeSystem(id *uint16, configuration *uint16) (hr error) {
-	if _perr := procCreateComputeSystem.Find(); _perr != nil {
-		return _perr
+	if hr = procCreateComputeSystem.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procCreateComputeSystem.Addr(), 2, uintptr(unsafe.Pointer(id)), uintptr(unsafe.Pointer(configuration)), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -414,10 +398,9 @@ func createProcessWithStdHandlesInComputeSystem(id string, paramsJson string, pi
 }
 
 func _createProcessWithStdHandlesInComputeSystem(id *uint16, paramsJson *uint16, pid *uint32, stdin *syscall.Handle, stdout *syscall.Handle, stderr *syscall.Handle) (hr error) {
-	if _perr := procCreateProcessWithStdHandlesInComputeSystem.Find(); _perr != nil {
-		return _perr
+	if hr = procCreateProcessWithStdHandlesInComputeSystem.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procCreateProcessWithStdHandlesInComputeSystem.Addr(), 6, uintptr(unsafe.Pointer(id)), uintptr(unsafe.Pointer(paramsJson)), uintptr(unsafe.Pointer(pid)), uintptr(unsafe.Pointer(stdin)), uintptr(unsafe.Pointer(stdout)), uintptr(unsafe.Pointer(stderr)))
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -435,10 +418,9 @@ func resizeConsoleInComputeSystem(id string, pid uint32, height uint16, width ui
 }
 
 func _resizeConsoleInComputeSystem(id *uint16, pid uint32, height uint16, width uint16, flags uint32) (hr error) {
-	if _perr := procResizeConsoleInComputeSystem.Find(); _perr != nil {
-		return _perr
+	if hr = procResizeConsoleInComputeSystem.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procResizeConsoleInComputeSystem.Addr(), 5, uintptr(unsafe.Pointer(id)), uintptr(pid), uintptr(height), uintptr(width), uintptr(flags), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -456,10 +438,9 @@ func shutdownComputeSystem(id string, timeout uint32) (hr error) {
 }
 
 func _shutdownComputeSystem(id *uint16, timeout uint32) (hr error) {
-	if _perr := procShutdownComputeSystem.Find(); _perr != nil {
-		return _perr
+	if hr = procShutdownComputeSystem.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procShutdownComputeSystem.Addr(), 2, uintptr(unsafe.Pointer(id)), uintptr(timeout), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -477,10 +458,9 @@ func startComputeSystem(id string) (hr error) {
 }
 
 func _startComputeSystem(id *uint16) (hr error) {
-	if _perr := procStartComputeSystem.Find(); _perr != nil {
-		return _perr
+	if hr = procStartComputeSystem.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procStartComputeSystem.Addr(), 1, uintptr(unsafe.Pointer(id)), 0, 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -498,10 +478,9 @@ func terminateComputeSystem(id string) (hr error) {
 }
 
 func _terminateComputeSystem(id *uint16) (hr error) {
-	if _perr := procTerminateComputeSystem.Find(); _perr != nil {
-		return _perr
+	if hr = procTerminateComputeSystem.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procTerminateComputeSystem.Addr(), 1, uintptr(unsafe.Pointer(id)), 0, 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -519,10 +498,9 @@ func terminateProcessInComputeSystem(id string, pid uint32) (hr error) {
 }
 
 func _terminateProcessInComputeSystem(id *uint16, pid uint32) (hr error) {
-	if _perr := procTerminateProcessInComputeSystem.Find(); _perr != nil {
-		return _perr
+	if hr = procTerminateProcessInComputeSystem.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall(procTerminateProcessInComputeSystem.Addr(), 2, uintptr(unsafe.Pointer(id)), uintptr(pid), 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -540,10 +518,9 @@ func waitForProcessInComputeSystem(id string, pid uint32, timeout uint32, exitCo
 }
 
 func _waitForProcessInComputeSystem(id *uint16, pid uint32, timeout uint32, exitCode *uint32) (hr error) {
-	if _perr := procWaitForProcessInComputeSystem.Find(); _perr != nil {
-		return _perr
+	if hr = procWaitForProcessInComputeSystem.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procWaitForProcessInComputeSystem.Addr(), 4, uintptr(unsafe.Pointer(id)), uintptr(pid), uintptr(timeout), uintptr(unsafe.Pointer(exitCode)), 0, 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
@@ -571,10 +548,9 @@ func _hnsCall(method string, path string, object string, response **uint16) (hr 
 }
 
 func __hnsCall(method *uint16, path *uint16, object *uint16, response **uint16) (hr error) {
-	if _perr := procHNSCall.Find(); _perr != nil {
-		return _perr
+	if hr = procHNSCall.Find(); hr != nil {
+		return
 	}
-
 	r0, _, _ := syscall.Syscall6(procHNSCall.Addr(), 4, uintptr(unsafe.Pointer(method)), uintptr(unsafe.Pointer(path)), uintptr(unsafe.Pointer(object)), uintptr(unsafe.Pointer(response)), 0, 0)
 	if r0 != 0 {
 		hr = syscall.Errno(r0)
