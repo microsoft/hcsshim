jstarks/opengcs

Implement mapped directories
These directories are implemented using the Plan9 filesystem. Given a
mount location and a port, the GCS can mount a directory as a Plan9
share to the host.

service/gcs/bridge/bridge_test.go
@@ -22,6 +22,20 @@ const (
 	testTimeout = 5
 )
 
+// TestResourceModificationSettings is a duplicate of
+// prot.ResourceModificationSettings where each field is given explicitly
+// rather than being inherited from embedded types. This allows the test code
+// to easily construct a JSON string for the message without running into the
+// Go JSON marshaller's inability to marshal field names which were inherited
+// from multiple embedded types.
+type TestResourceModificationSettings struct {
+	ContainerPath     string
+	Lun               uint8  `json:",omitempty"`
+	CreateInUtilityVM bool   `json:",omitempty"`
+	ReadOnly          bool   `json:",omitempty"`
+	Port              uint16 `json:",omitempty"`
+}
+
 var _ = Describe("Bridge", func() {
 	var (
 		connChannel    chan *transport.MockConnection
@@ -591,11 +605,12 @@ var _ = Describe("Bridge", func() {
 
 	Describe("calling modifySettings", func() {
 		var (
-			response                       prot.MessageResponseBase
-			callArgs                       mockcore.ModifySettingsCall
-			modificationRequest            prot.ResourceModificationRequestResponse
-			defaultModificationRequest     prot.ResourceModificationRequestResponse
-			unsupportedModificationRequest prot.ResourceModificationRequestResponse
+			response                             prot.MessageResponseBase
+			callArgs                             mockcore.ModifySettingsCall
+			modificationRequest                  prot.ResourceModificationRequestResponse
+			modificationRequestToSend            prot.ResourceModificationRequestResponse
+			defaultModificationRequestToSend     prot.ResourceModificationRequestResponse
+			unsupportedModificationRequestToSend prot.ResourceModificationRequestResponse
 		)
 		BeforeEach(func() {
 			messageType = prot.ComputeSystemModifySettingsV1
@@ -619,15 +634,41 @@ var _ = Describe("Bridge", func() {
 					RequestType:  prot.RtAdd,
 					Settings:     prot.ResourceModificationSettings{MappedVirtualDisk: &disk},
 				}
-				defaultModificationRequest = prot.ResourceModificationRequestResponse{
+
+				// The "ToSend" modification requests are created using
+				// TestResourceModificationSettings, which means they can be
+				// safely marshalled into JSON and sent without running into
+				// the JSON marshaller's problem with ambiguous field names
+				// from embedded types.
+				modificationRequestToSend = prot.ResourceModificationRequestResponse{
+					ResourceType: prot.PtMappedVirtualDisk,
+					RequestType:  prot.RtAdd,
+					Settings: TestResourceModificationSettings{
+						ContainerPath:     disk.ContainerPath,
+						Lun:               disk.Lun,
+						CreateInUtilityVM: disk.CreateInUtilityVM,
+						ReadOnly:          disk.ReadOnly,
+					},
+				}
+				defaultModificationRequestToSend = prot.ResourceModificationRequestResponse{
 					ResourceType: "",
 					RequestType:  "",
-					Settings:     prot.ResourceModificationSettings{MappedVirtualDisk: &disk},
+					Settings: TestResourceModificationSettings{
+						ContainerPath:     disk.ContainerPath,
+						Lun:               disk.Lun,
+						CreateInUtilityVM: disk.CreateInUtilityVM,
+						ReadOnly:          disk.ReadOnly,
+					},
 				}
-				unsupportedModificationRequest = prot.ResourceModificationRequestResponse{
+				unsupportedModificationRequestToSend = prot.ResourceModificationRequestResponse{
 					ResourceType: prot.PtMemory,
 					RequestType:  prot.RtAdd,
-					Settings:     prot.ResourceModificationSettings{MappedVirtualDisk: &disk},
+					Settings: TestResourceModificationSettings{
+						ContainerPath:     disk.ContainerPath,
+						Lun:               disk.Lun,
+						CreateInUtilityVM: disk.CreateInUtilityVM,
+						ReadOnly:          disk.ReadOnly,
+					},
 				}
 			})
 			Context("using non-empty ResourceType and RequestType", func() {
@@ -637,7 +678,7 @@ var _ = Describe("Bridge", func() {
 							ContainerID: containerID,
 							ActivityID:  activityID,
 						},
-						Request: modificationRequest,
+						Request: modificationRequestToSend,
 					}
 				})
 				AssertNoResponseErrors()
@@ -654,7 +695,7 @@ var _ = Describe("Bridge", func() {
 							ContainerID: containerID,
 							ActivityID:  activityID,
 						},
-						Request: defaultModificationRequest,
+						Request: defaultModificationRequestToSend,
 					}
 				})
 				AssertResponseErrors("invalid ResourceType Memory")
@@ -667,7 +708,7 @@ var _ = Describe("Bridge", func() {
 							ContainerID: containerID,
 							ActivityID:  activityID,
 						},
-						Request: unsupportedModificationRequest,
+						Request: unsupportedModificationRequestToSend,
 					}
 				})
 				AssertResponseErrors("invalid ResourceType Memory")

service/gcs/core/gcs/storage.go
@@ -23,6 +23,10 @@ const (
 	// that will be used as the base layer for containers.
 	baseFilesPath = "/mnt/base/"
 
+	// mappedDirectoryPrefix is the directory under which all mapped
+	// directories are mounted.
+	mappedDirectoryPrefix = "/mnt"
+
 	// deviceLookupTimeout is the amount of time before deviceIDToName will
 	// give up trying to look up the device name from its ID.
 	deviceLookupTimeout = time.Second * 2
@@ -172,6 +176,51 @@ func (c *gcsCore) unmountMappedVirtualDisks(disks []prot.MappedVirtualDisk) erro
 		if exists && mounted {
 			if err := c.OS.Unmount(dir, 0); err != nil {
 				return errors.Wrapf(err, "failed to unmount mapped virtual disk path %s", dir)
+			}
+		}
+	}
+	return nil
+}
+
+// mountMappedDirectories mounts the given mapped directories using a Plan9
+// filesystem with the given options.
+func (c *gcsCore) mountMappedDirectories(dirs []prot.MappedDirectory) error {
+	for _, dir := range dirs {
+		if !dir.CreateInUtilityVM {
+			return errors.New("we do not currently support mapping directories inside the container namespace")
+		}
+		mountedPath := filepath.Join(mappedDirectoryPrefix, dir.ContainerPath)
+		if err := c.OS.MkdirAll(mountedPath, 0700); err != nil {
+			return errors.Wrapf(err, "failed to create directory for mapped directory %s", mountedPath)
+		}
+		var mountOptions uintptr = 0
+		var data string = "trans=vsock,port=%d"
+		if dir.ReadOnly {
+			mountOptions |= syscall.MS_RDONLY
+			data += ",noload"
+		}
+		if err := c.OS.Mount(dir.ContainerPath, mountedPath, "9p", mountOptions, data); err != nil {
+			return errors.Wrapf(err, "failed to mount directory for mapped directory %s", mountedPath)
+		}
+	}
+	return nil
+}
+
+// unmountMappedDirectories unmounts the given container's mapped directories.
+func (c *gcsCore) unmountMappedDirectories(dirs []prot.MappedDirectory) error {
+	for _, dir := range dirs {
+		mountedPath := filepath.Join(mappedDirectoryPrefix, dir.ContainerPath)
+		exists, err := c.OS.PathExists(mountedPath)
+		if err != nil {
+			return errors.Wrapf(err, "failed to determine if mapped directory path exists %s", mountedPath)
+		}
+		mounted, err := c.OS.PathIsMounted(mountedPath)
+		if err != nil {
+			return errors.Wrapf(err, "failed to determine if mapped directory path is mounted %s", mountedPath)
+		}
+		if exists && mounted {
+			if err := c.OS.Unmount(mountedPath, 0); err != nil {
+				return errors.Wrapf(err, "failed to unmount mapped directory path %s", mountedPath)
 			}
 		}
 	}

service/gcs/prot/protocol.go
@@ -53,8 +53,8 @@ const (
 )
 
 // GetResponseIdentifier returns the response version of the given request
-// identifier. So, for example, an input of ComputeSystemCreate_v1 would result
-// in an output of ComputeSystemResponseCreate_v1.
+// identifier. So, for example, an input of ComputeSystemCreateV1 would result
+// in an output of ComputeSystemResponseCreateV1.
 func GetResponseIdentifier(identifier MessageIdentifier) MessageIdentifier {
 	return MessageIdentifier(MtResponse | (uint32(identifier) & ^uint32(messageTypeMask)))
 }
@@ -273,6 +273,7 @@ const (
 // ResourceType is checked and only the relevant fields are filled in.
 type ResourceModificationSettings struct {
 	*MappedVirtualDisk
+	*MappedDirectory
 }
 
 // ResourceModificationRequestResponse details a container resource which
@@ -319,6 +320,12 @@ func UnmarshalContainerModifySettings(b []byte) (*ContainerModifySettings, error
 		settings.MappedVirtualDisk = &MappedVirtualDisk{}
 		if err := json.Unmarshal(rawSettings, settings.MappedVirtualDisk); err != nil {
 			return nil, errors.Wrap(err, "failed to unmarshal settings as MappedVirtualDisk")
+		}
+		request.Request.Settings = settings
+	case PtMappedDirectory:
+		settings.MappedDirectory = &MappedDirectory{}
+		if err := json.Unmarshal(rawSettings, settings.MappedDirectory); err != nil {
+			return nil, errors.Wrap(err, "failed to unmarshal settings as MappedDirectory")
 		}
 		request.Request.Settings = settings
 	default:
@@ -413,6 +420,15 @@ type MappedVirtualDisk struct {
 	ReadOnly          bool  `json:",omitempty"`
 }
 
+// MappedDirectory represents a directory on the host which is mapped to a
+// directory on the guest through a technology such as Plan9.
+type MappedDirectory struct {
+	ContainerPath     string
+	CreateInUtilityVM bool   `json:",omitempty"`
+	ReadOnly          bool   `json:",omitempty"`
+	Port              uint16 `json:",omitempty"`
+}
+
 // VMHostedContainerSettings is the set of settings used to specify the initial
 // configuration of a container.
 type VMHostedContainerSettings struct {
@@ -421,6 +437,7 @@ type VMHostedContainerSettings struct {
 	// of the sandbox device.
 	SandboxDataPath    string
 	MappedVirtualDisks []MappedVirtualDisk
+	MappedDirectories  []MappedDirectory
 	NetworkAdapters    []NetworkAdapter `json:",omitempty"`
 }
 

microsoft/hcsshim

service/gcs/bridge/bridge_test.go
@@ -21,6 +21,21 @@ const (
 	testTimeout = 5
 )
 
+// TestResourceModificationSettings is a duplicate of
+// prot.ResourceModificationSettings where each field is given explicitly
+// rather than being inherited from embedded types. This allows the test code
+// to easily construct a JSON string for the message without running into the
+// Go JSON marshaller's inability to marshal field names which were inherited
+// from multiple embedded types.
+type TestResourceModificationSettings struct {
+	ContainerPath     string
+	Lun               uint8  `json:",omitempty"`
+	CreateInUtilityVM bool   `json:",omitempty"`
+	ReadOnly          bool   `json:",omitempty"`
+	AttachOnly        bool   `json:",omitempty"`
+	Port              uint32 `json:",omitempty"`
+}
+
 var _ = Describe("Bridge", func() {
 	var (
 		connChannel    chan *transport.MockConnection
@@ -590,11 +605,12 @@ var _ = Describe("Bridge", func() {
 
 	Describe("calling modifySettings", func() {
 		var (
-			response                       prot.MessageResponseBase
-			callArgs                       mockcore.ModifySettingsCall
-			modificationRequest            prot.ResourceModificationRequestResponse
-			defaultModificationRequest     prot.ResourceModificationRequestResponse
-			unsupportedModificationRequest prot.ResourceModificationRequestResponse
+			response                             prot.MessageResponseBase
+			callArgs                             mockcore.ModifySettingsCall
+			modificationRequest                  prot.ResourceModificationRequestResponse
+			modificationRequestToSend            prot.ResourceModificationRequestResponse
+			defaultModificationRequestToSend     prot.ResourceModificationRequestResponse
+			unsupportedModificationRequestToSend prot.ResourceModificationRequestResponse
 		)
 		BeforeEach(func() {
 			messageType = prot.ComputeSystemModifySettingsV1
@@ -618,15 +634,41 @@ var _ = Describe("Bridge", func() {
 					RequestType:  prot.RtAdd,
 					Settings:     prot.ResourceModificationSettings{MappedVirtualDisk: &disk},
 				}
-				defaultModificationRequest = prot.ResourceModificationRequestResponse{
+
+				// The "ToSend" modification requests are created using
+				// TestResourceModificationSettings, which means they can be
+				// safely marshalled into JSON and sent without running into
+				// the JSON marshaller's problem with ambiguous field names
+				// from embedded types.
+				modificationRequestToSend = prot.ResourceModificationRequestResponse{
+					ResourceType: prot.PtMappedVirtualDisk,
+					RequestType:  prot.RtAdd,
+					Settings: TestResourceModificationSettings{
+						ContainerPath:     disk.ContainerPath,
+						Lun:               disk.Lun,
+						CreateInUtilityVM: disk.CreateInUtilityVM,
+						ReadOnly:          disk.ReadOnly,
+					},
+				}
+				defaultModificationRequestToSend = prot.ResourceModificationRequestResponse{
 					ResourceType: "",
 					RequestType:  "",
-					Settings:     prot.ResourceModificationSettings{MappedVirtualDisk: &disk},
+					Settings: TestResourceModificationSettings{
+						ContainerPath:     disk.ContainerPath,
+						Lun:               disk.Lun,
+						CreateInUtilityVM: disk.CreateInUtilityVM,
+						ReadOnly:          disk.ReadOnly,
+					},
 				}
-				unsupportedModificationRequest = prot.ResourceModificationRequestResponse{
+				unsupportedModificationRequestToSend = prot.ResourceModificationRequestResponse{
 					ResourceType: prot.PtMemory,
 					RequestType:  prot.RtAdd,
-					Settings:     prot.ResourceModificationSettings{MappedVirtualDisk: &disk},
+					Settings: TestResourceModificationSettings{
+						ContainerPath:     disk.ContainerPath,
+						Lun:               disk.Lun,
+						CreateInUtilityVM: disk.CreateInUtilityVM,
+						ReadOnly:          disk.ReadOnly,
+					},
 				}
 			})
 			Context("using non-empty ResourceType and RequestType", func() {
@@ -636,7 +678,7 @@ var _ = Describe("Bridge", func() {
 							ContainerID: containerID,
 							ActivityID:  activityID,
 						},
-						Request: modificationRequest,
+						Request: modificationRequestToSend,
 					}
 				})
 				AssertNoResponseErrors()
@@ -653,7 +695,7 @@ var _ = Describe("Bridge", func() {
 							ContainerID: containerID,
 							ActivityID:  activityID,
 						},
-						Request: defaultModificationRequest,
+						Request: defaultModificationRequestToSend,
 					}
 				})
 				AssertResponseErrors("invalid ResourceType Memory")
@@ -666,7 +708,7 @@ var _ = Describe("Bridge", func() {
 							ContainerID: containerID,
 							ActivityID:  activityID,
 						},
-						Request: unsupportedModificationRequest,
+						Request: unsupportedModificationRequestToSend,
 					}
 				})
 				AssertResponseErrors("invalid ResourceType Memory")

service/gcs/core/gcs/storage.go
@@ -22,6 +22,10 @@ const (
 	// that will be used as the base layer for containers.
 	baseFilesPath = "/tmp/base/"
 
+	// mappedDirectoryPrefix is the directory under which all mapped
+	// directories are mounted.
+	mappedDirectoryPrefix = "/tmp"
+
 	// deviceLookupTimeout is the amount of time before deviceIDToName will
 	// give up trying to look up the device name from its ID.
 	deviceLookupTimeout = time.Second * 2
@@ -171,6 +175,51 @@ func (c *gcsCore) unmountMappedVirtualDisks(disks []prot.MappedVirtualDisk) erro
 		if exists && mounted {
 			if err := c.OS.Unmount(dir, 0); err != nil {
 				return errors.Wrapf(err, "failed to unmount mapped virtual disk path %s", dir)
+			}
+		}
+	}
+	return nil
+}
+
+// mountMappedDirectories mounts the given mapped directories using a Plan9
+// filesystem with the given options.
+func (c *gcsCore) mountMappedDirectories(dirs []prot.MappedDirectory) error {
+	for _, dir := range dirs {
+		if !dir.CreateInUtilityVM {
+			return errors.New("we do not currently support mapping directories inside the container namespace")
+		}
+		mountedPath := filepath.Join(mappedDirectoryPrefix, dir.ContainerPath)
+		if err := c.OS.MkdirAll(mountedPath, 0700); err != nil {
+			return errors.Wrapf(err, "failed to create directory for mapped directory %s", mountedPath)
+		}
+		var mountOptions uintptr = 0
+		var data string = fmt.Sprintf("trans=vsock,port=%d", dir.Port)
+		if dir.ReadOnly {
+			mountOptions |= syscall.MS_RDONLY
+			data += ",noload"
+		}
+		if err := c.OS.Mount(dir.ContainerPath, mountedPath, "9p", mountOptions, data); err != nil {
+			return errors.Wrapf(err, "failed to mount directory for mapped directory %s", mountedPath)
+		}
+	}
+	return nil
+}
+
+// unmountMappedDirectories unmounts the given container's mapped directories.
+func (c *gcsCore) unmountMappedDirectories(dirs []prot.MappedDirectory) error {
+	for _, dir := range dirs {
+		mountedPath := filepath.Join(mappedDirectoryPrefix, dir.ContainerPath)
+		exists, err := c.OS.PathExists(mountedPath)
+		if err != nil {
+			return errors.Wrapf(err, "failed to determine if mapped directory path exists %s", mountedPath)
+		}
+		mounted, err := c.OS.PathIsMounted(mountedPath)
+		if err != nil {
+			return errors.Wrapf(err, "failed to determine if mapped directory path is mounted %s", mountedPath)
+		}
+		if exists && mounted {
+			if err := c.OS.Unmount(mountedPath, 0); err != nil {
+				return errors.Wrapf(err, "failed to unmount mapped directory path %s", mountedPath)
 			}
 		}
 	}

service/gcs/prot/protocol.go
@@ -54,8 +54,8 @@ const (
 )
 
 // GetResponseIdentifier returns the response version of the given request
-// identifier. So, for example, an input of ComputeSystemCreate_v1 would result
-// in an output of ComputeSystemResponseCreate_v1.
+// identifier. So, for example, an input of ComputeSystemCreateV1 would result
+// in an output of ComputeSystemResponseCreateV1.
 func GetResponseIdentifier(identifier MessageIdentifier) MessageIdentifier {
 	return MessageIdentifier(MtResponse | (uint32(identifier) & ^uint32(messageTypeMask)))
 }
@@ -274,6 +274,7 @@ const (
 // ResourceType is checked and only the relevant fields are filled in.
 type ResourceModificationSettings struct {
 	*MappedVirtualDisk
+	*MappedDirectory
 }
 
 // ResourceModificationRequestResponse details a container resource which
@@ -320,6 +321,12 @@ func UnmarshalContainerModifySettings(b []byte) (*ContainerModifySettings, error
 		settings.MappedVirtualDisk = &MappedVirtualDisk{}
 		if err := commonutils.UnmarshalJSONWithHresult(rawSettings, settings.MappedVirtualDisk); err != nil {
 			return nil, errors.Wrap(err, "failed to unmarshal settings as MappedVirtualDisk")
+		}
+		request.Request.Settings = settings
+	case PtMappedDirectory:
+		settings.MappedDirectory = &MappedDirectory{}
+		if err := json.Unmarshal(rawSettings, settings.MappedDirectory); err != nil {
+			return nil, errors.Wrap(err, "failed to unmarshal settings as MappedDirectory")
 		}
 		request.Request.Settings = settings
 	default:
@@ -414,6 +421,15 @@ type MappedVirtualDisk struct {
 	ReadOnly          bool  `json:",omitempty"`
 }
 
+// MappedDirectory represents a directory on the host which is mapped to a
+// directory on the guest through a technology such as Plan9.
+type MappedDirectory struct {
+	ContainerPath     string
+	CreateInUtilityVM bool   `json:",omitempty"`
+	ReadOnly          bool   `json:",omitempty"`
+	Port              uint32 `json:",omitempty"`
+}
+
 // VMHostedContainerSettings is the set of settings used to specify the initial
 // configuration of a container.
 type VMHostedContainerSettings struct {
@@ -422,6 +438,7 @@ type VMHostedContainerSettings struct {
 	// of the sandbox device.
 	SandboxDataPath    string
 	MappedVirtualDisks []MappedVirtualDisk
+	MappedDirectories  []MappedDirectory
 	NetworkAdapters    []NetworkAdapter `json:",omitempty"`
 }
 