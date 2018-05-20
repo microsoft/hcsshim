c39f4c2daf1f4a22d59f1ce2ef287d212836fc3c
John Starks <jostarks@ntdev.microsoft.com>
2018-05-20 13:52:38 -0700

    uvm: Remove unnecessary error wrapper

--- uvm/start.go
+++ uvm/start.go
@@ -1,13 +1,6 @@
 package uvm
 
-import (
-	"fmt"
-)
-
 // Start synchronously starts the utility VM.
 func (uvm *UtilityVM) Start() error {
-	if uvm.hcsSystem == nil {
-		return fmt.Errorf("cannot start - no hcsSystem handle")
-	}
-	return convertSystemError(uvm.hcsSystem.Start(), uvm)
+	return uvm.hcsSystem.Start()
 }
--- uvm/wait.go
+++ uvm/wait.go
@@ -1,13 +1,6 @@
 package uvm
 
-import (
-	"fmt"
-)
-
 // Waits synchronously waits for a utility VM to terminate.
 func (uvm *UtilityVM) Wait() error {
-	if uvm.hcsSystem == nil {
-		return fmt.Errorf("cannot wait - no hcsSystem handle")
-	}
-	return convertSystemError(uvm.hcsSystem.Wait(), uvm)
+	return uvm.hcsSystem.Wait()
 }
