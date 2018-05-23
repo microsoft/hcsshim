d5303919ed7bee661d771609a3eba20422846566
John Starks <jostarks@ntdev.microsoft.com>
2018-05-23 16:20:54 -0700

    runhcs: Add --host flag to run command too

--- cmd/runhcs/create.go
+++ cmd/runhcs/create.go
@@ -5,6 +5,39 @@ import (
 	"github.com/urfave/cli"
 )
 
+var createRunFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:  "bundle, b",
+		Value: "",
+		Usage: `path to the root of the bundle directory, defaults to the current directory`,
+	},
+	cli.StringFlag{
+		Name:  "pid-file",
+		Value: "",
+		Usage: "specify the file to write the process id to",
+	},
+	cli.StringFlag{
+		Name:  "shim-log",
+		Value: "",
+		Usage: "path to the log file for the launched shim process",
+	},
+	cli.StringFlag{
+		Name:  "vm-log",
+		Value: "",
+		Usage: "path to the log file for the launched VM shim process",
+	},
+	cli.StringFlag{
+		Name:  "vm-console",
+		Value: "",
+		Usage: `path to the pipe for the VM's console (e.g. \\.\pipe\debugpipe)`,
+	},
+	cli.StringFlag{
+		Name:  "host",
+		Value: "",
+		Usage: "host container whose VM this container should run in",
+	},
+}
+
 var createCommand = cli.Command{
 	Name:  "create",
 	Usage: "create a container",
@@ -21,38 +54,7 @@ The specification file includes an args parameter. The args parameter is used
 to specify command(s) that get run when the container is started. To change the
 command(s) that get executed on start, edit the args parameter of the spec. See
 "runc spec --help" for more explanation.`,
-	Flags: []cli.Flag{
-		cli.StringFlag{
-			Name:  "bundle, b",
-			Value: "",
-			Usage: `path to the root of the bundle directory, defaults to the current directory`,
-		},
-		cli.StringFlag{
-			Name:  "pid-file",
-			Value: "",
-			Usage: "specify the file to write the process id to",
-		},
-		cli.StringFlag{
-			Name:  "shim-log",
-			Value: "",
-			Usage: "path to the log file for the launched shim process",
-		},
-		cli.StringFlag{
-			Name:  "vm-log",
-			Value: "",
-			Usage: "path to the log file for the launched VM shim process",
-		},
-		cli.StringFlag{
-			Name:  "vm-console",
-			Value: "",
-			Usage: `path to the pipe for the VM's console (e.g. \\.\pipe\debugpipe)`,
-		},
-		cli.StringFlag{
-			Name:  "host",
-			Value: "",
-			Usage: "host container whose VM this container should run in",
-		},
-	},
+	Flags:  append(createRunFlags),
 	Before: appargs.Validate(argID),
 	Action: func(context *cli.Context) error {
 		cfg, err := containerConfigFromContext(context)
