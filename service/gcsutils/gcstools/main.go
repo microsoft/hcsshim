3462633e7f5ea5b65363c1b3f60d1e2e81fa2a71
John Starks <jostarks@microsoft.com>
2018-06-08 16:50:37 -0700

    gcstools: Allow panic to panic

--- service/gcsutils/gcstools/main.go
+++ service/gcsutils/gcstools/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
-	"runtime"
 )
 
 var commands = map[string]func(){
@@ -18,32 +16,15 @@ var commands = map[string]func(){
 
 func main() {
 	cmd := filepath.Base(os.Args[0])
-	if mainFunc, ok := commands[cmd]; ok {
-		defer writePanicLog()
-		mainFunc()
-
-		// The called program might have exited to return a custom return code.
-		// If it returned, then assume success.
-		os.Exit(0)
+	mainFunc := commands[cmd]
+	if mainFunc == nil {
+		fmt.Fprintf(os.Stderr, "unknown command: %s\n", cmd)
+		fmt.Fprintf(os.Stderr, "known commands:\n")
+		for k := range commands {
+			fmt.Fprintf(os.Stderr, "\t%s\n", k)
+		}
+		os.Exit(127)
 	}
 
-	// Unknown command
-	fmt.Fprintf(os.Stderr, "unknown command: %s\n", cmd)
-	fmt.Fprintf(os.Stderr, "known commands:\n")
-	for k := range commands {
-		fmt.Fprintf(os.Stderr, "\t%s\n", k)
-	}
-	os.Exit(127)
-}
-
-// Very basic panic log writer that dumps the stack to file
-func writePanicLog() {
-	recover()
-	var logger *log.Logger
-	f, _ := os.Create(fmt.Sprintf("/tmp/paniclog.%s.%d", filepath.Base(os.Args[0]), os.Getpid()))
-	logger = log.New(f, "", log.LstdFlags)
-	buf := make([]byte, 1<<16)
-	runtime.Stack(buf, true)
-	logger.Printf("%s", buf)
-	os.Exit(1)
+	mainFunc()
 }
