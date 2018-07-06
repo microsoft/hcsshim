da13cbe1318156299c475eb52de5394d38329747
Sarah <scooley@microsoft.com>
2018-07-06 14:14:46 -0700

    updated main to refer  to Hyper-V isolated containers rather than lcow based on references to wcow

--- cmd/runhcs/main.go
+++ cmd/runhcs/main.go
@@ -35,7 +35,7 @@ const (
 	specConfig = "config.json"
 	usage      = `Open Container Initiative runtime
 
-runhcs is fork of runc modified to run on Windows with Hyper-V isolated Linux containers.  It is a command line client for running applications packaged according to the Open Container Initiative (OCI) format and is a compliant implementation of the Open Container Initiative specification.
+runhcs is fork of runc modified to run on Windows with Hyper-V isolated containers.  It is a command line client for running applications packaged according to the Open Container Initiative (OCI) format and is a compliant implementation of the Open Container Initiative specification.
 
 runhcs integrates with existing process supervisors to provide a production container runtime environment for applications. It can be used with your existing process monitoring tools and the container will be spawned as a direct child of the process supervisor.
 
