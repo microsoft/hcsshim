63208dfcaae13d554d44ceb8f99dfbfae03ba6b9
Justin Terry (VM) <juterry@microsoft.com>
2018-07-10 08:50:10 -0700

    Renaming VPMEM and VSMB share paths
    Signed-off-by: Justin Terry (VM) <juterry@microsoft.com>

--- internal/uvm/plan9.go
+++ internal/uvm/plan9.go
@@ -37,7 +37,7 @@ func (uvm *UtilityVM) AddPlan9(hostPath string, uvmPath string, readOnly bool) e
 				Path: hostPath,
 				Port: int32(uvm.plan9Counter), // TODO: Temporary. Will all use a single port (9999)
 			},
-			ResourcePath: fmt.Sprintf("virtualmachine/devices/plan9shares/%d", uvm.plan9Counter),
+			ResourcePath: fmt.Sprintf("virtualmachine/devices/plan9/shares/%d", uvm.plan9Counter),
 			GuestRequest: hostedsettings.GuestRequest{
 				ResourceType: resourcetype.MappedDirectory,
 				RequestType:  requesttype.Add,
@@ -96,7 +96,7 @@ func (uvm *UtilityVM) removePlan9(hostPath, uvmPath string) error {
 			Name: fmt.Sprintf("%d", uvm.plan9Shares[hostPath].idCounter),
 			Port: uvm.plan9Shares[hostPath].port,
 		},
-		ResourcePath: fmt.Sprintf("virtualmachine/devices/plan9shares/%d", uvm.plan9Shares[hostPath].idCounter),
+		ResourcePath: fmt.Sprintf("virtualmachine/devices/plan9/shares/%d", uvm.plan9Shares[hostPath].idCounter),
 		GuestRequest: hostedsettings.GuestRequest{
 			ResourceType: resourcetype.MappedDirectory,
 			RequestType:  requesttype.Remove,
