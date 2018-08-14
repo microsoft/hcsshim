ba6a610b8d36e874c33d7f29f58c2b692d0fb8c1
Justin Terry (VM) <juterry@microsoft.com>
2018-08-14 10:30:26 -0700

    Adding log support on stdio connections
    Signed-off-by: Justin Terry (VM) <juterry@microsoft.com>

--- service/gcs/stdio/connection.go
+++ service/gcs/stdio/connection.go
@@ -1,8 +1,11 @@
 package stdio
 
 import (
+	"os"
+
 	"github.com/Microsoft/opengcs/service/gcs/transport"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 // ConnectionSettings describe the stdin, stdout, stderr ports to connect the
@@ -12,6 +15,40 @@ type ConnectionSettings struct {
 	StdOut *uint32
 	StdErr *uint32
 }
+
+type logConnection struct {
+	con  transport.Connection
+	port uint32
+}
+
+func (lc *logConnection) Read(b []byte) (int, error) {
+	return lc.con.Read(b)
+}
+
+func (lc *logConnection) Write(b []byte) (int, error) {
+	return lc.con.Write(b)
+}
+
+func (lc *logConnection) Close() error {
+	logrus.Debugf("Closing connection on port: %d", lc.port)
+	return lc.con.Close()
+}
+
+func (lc *logConnection) CloseRead() error {
+	logrus.Debugf("Closing read connection on port: %d", lc.port)
+	return lc.con.CloseRead()
+}
+
+func (lc *logConnection) CloseWrite() error {
+	logrus.Debugf("Closing write connection on port: %d", lc.port)
+	return lc.con.CloseWrite()
+}
+
+func (lc *logConnection) File() (*os.File, error) {
+	return lc.con.File()
+}
+
+var _ = (transport.Connection)(&logConnection{})
 
 // Connect returns new transport.Connection instances, one for each stdio pipe
 // to be used. If CreateStd*Pipe for a given pipe is false, the given Connection
@@ -24,21 +61,33 @@ func Connect(tport transport.Transport, settings ConnectionSettings) (_ *Connect
 		}
 	}()
 	if settings.StdIn != nil {
-		connSet.In, err = tport.Dial(*settings.StdIn)
+		c, err := tport.Dial(*settings.StdIn)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed creating stdin Connection")
 		}
+		connSet.In = &logConnection{
+			con:  c,
+			port: *settings.StdIn,
+		}
 	}
 	if settings.StdOut != nil {
-		connSet.Out, err = tport.Dial(*settings.StdOut)
+		c, err := tport.Dial(*settings.StdOut)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed creating stdout Connection")
 		}
+		connSet.Out = &logConnection{
+			con:  c,
+			port: *settings.StdOut,
+		}
 	}
 	if settings.StdErr != nil {
-		connSet.Err, err = tport.Dial(*settings.StdErr)
+		c, err := tport.Dial(*settings.StdErr)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed creating stderr Connection")
+		}
+		connSet.Err = &logConnection{
+			con:  c,
+			port: *settings.StdErr,
 		}
 	}
 	return connSet, nil
