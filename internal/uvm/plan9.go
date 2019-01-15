332962ef0274c44a029f59a09c1ae771fdf04cf1
Justin Terry (VM) <juterry@microsoft.com>
2019-01-15 12:05:49 -0800

    Implement proper Plan9Share support for LCOW
    Signed-off-by: Justin Terry (VM) <juterry@microsoft.com>

--- internal/uvm/plan9.go
+++ internal/uvm/plan9.go
@@ -27,6 +27,19 @@ func (uvm *UtilityVM) AddPlan9(hostPath string, uvmPath string, readOnly bool) e
 		return fmt.Errorf("uvmPath must be passed to AddPlan9")
 	}
 
+	// TODO: JTERRY75 - These are marked private in the schema. For now use them
+	// but when there are public variants we need to switch to them.
+	const (
+		shareFlagsReadOnly      int32 = 0x00000001
+		shareFlagsLinuxMetadata int32 = 0x00000004
+		shareFlagsCaseSensitive int32 = 0x00000008
+	)
+
+	flags := shareFlagsLinuxMetadata | shareFlagsCaseSensitive
+	if readOnly {
+		flags |= shareFlagsReadOnly
+	}
+
 	uvm.m.Lock()
 	defer uvm.m.Unlock()
 	if uvm.plan9Shares == nil {
@@ -38,9 +51,10 @@ func (uvm *UtilityVM) AddPlan9(hostPath string, uvmPath string, readOnly bool) e
 		modification := &hcsschema.ModifySettingRequest{
 			RequestType: requesttype.Add,
 			Settings: hcsschema.Plan9Share{
-				Name: fmt.Sprintf("%d", uvm.plan9Counter),
-				Path: hostPath,
-				Port: int32(uvm.plan9Counter), // TODO: Temporary. Will all use a single port (9999)
+				Name:  fmt.Sprintf("%d", uvm.plan9Counter),
+				Path:  hostPath,
+				Port:  int32(uvm.plan9Counter), // TODO: Temporary. Will all use a single port (9999)
+				Flags: flags,
 			},
 			ResourcePath: fmt.Sprintf("VirtualMachine/Devices/Plan9/Shares"),
 			GuestRequest: guestrequest.GuestRequest{
