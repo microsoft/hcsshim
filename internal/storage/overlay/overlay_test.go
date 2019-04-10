jterry75/opengcs

Move v2 storage calls to golang packages
Signed-off-by: Justin Terry (VM) <juterry@microsoft.com>

internal/storage/overlay/overlay_test.go
@@ -1,0 +1,129 @@
+// +build linux
+
+package overlay
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+type undo struct {
+	osMkdirAll   func(string, os.FileMode) error
+	osRemoveAll  func(string) error
+	syscallMount func(string, string, string, uintptr, string) error
+}
+
+func (u *undo) Close() {
+	osMkdirAll = u.osMkdirAll
+	syscallMount = u.syscallMount
+}
+
+// Captures the actual product function context and returns them on `Close()`.
+// It sets the test dependencies to `nil` so that any unpredicted call to that
+// function will cause the test to panic.
+func captureTestMethods() *undo {
+	u := &undo{
+		osMkdirAll:   osMkdirAll,
+		osRemoveAll:  osRemoveAll,
+		syscallMount: syscallMount,
+	}
+	osMkdirAll = nil
+	osRemoveAll = nil
+	syscallMount = nil
+	return u
+}
+
+func Test_Mount_Success(t *testing.T) {
+	undo := captureTestMethods()
+	defer undo.Close()
+
+	var upperCreated, workCreated, rootCreated bool
+	osMkdirAll = func(path string, perm os.FileMode) error {
+		if perm != 0755 {
+			t.Errorf("os.MkdirAll at: %s, perm: %v expected perm: 0755", path, perm)
+		}
+		switch path {
+		case "/upper":
+			upperCreated = true
+			return nil
+		case "/work":
+			workCreated = true
+			return nil
+		case "/root":
+			rootCreated = true
+			return nil
+		}
+		return errors.New("unexpected os.MkdirAll path")
+	}
+	syscallMount = func(source string, target string, fstype string, flags uintptr, data string) error {
+		if source != "overlay" {
+			t.Errorf("expected source: 'overlay' got: %v", source)
+		}
+		if target != "/root" {
+			t.Errorf("expected target: '/root' got: %v", target)
+		}
+		if fstype != "overlay" {
+			t.Errorf("expected fstype: 'overlay' got: %v", fstype)
+		}
+		if flags != 0 {
+			t.Errorf("expected flags: '0' got: %v", flags)
+		}
+		if data != "lowerdir=/layer1:/layer2,upperdir=/upper,workdir=/work" {
+			t.Errorf("expected data: 'lowerdir=/layer1:/layer2,upperdir=/upper,workdir=/work' got: %v", data)
+		}
+		return nil
+	}
+
+	err := Mount([]string{"/layer1", "/layer2"}, "/upper", "/work", "/root", false)
+	if err != nil {
+		t.Fatalf("expected no error got: %v", err)
+	}
+	if !upperCreated || !workCreated || !rootCreated {
+		t.Fatalf("expected all upper: %v, work: %v, root: %v to be created", upperCreated, workCreated, rootCreated)
+	}
+}
+
+func Test_Mount_Readonly_Success(t *testing.T) {
+	undo := captureTestMethods()
+	defer undo.Close()
+
+	var rootCreated bool
+	osMkdirAll = func(path string, perm os.FileMode) error {
+		if perm != 0755 {
+			t.Errorf("os.MkdirAll at: %s, perm: %v expected perm: 0755", path, perm)
+		}
+		switch path {
+		case "/root":
+			rootCreated = true
+			return nil
+		}
+		return errors.New("unexpected os.MkdirAll path")
+	}
+	syscallMount = func(source string, target string, fstype string, flags uintptr, data string) error {
+		if source != "overlay" {
+			t.Errorf("expected source: 'overlay' got: %v", source)
+		}
+		if target != "/root" {
+			t.Errorf("expected target: '/root' got: %v", target)
+		}
+		if fstype != "overlay" {
+			t.Errorf("expected fstype: 'overlay' got: %v", fstype)
+		}
+		if flags != 0 {
+			t.Errorf("expected flags: '0' got: %v", flags)
+		}
+		if data != "lowerdir=/layer1:/layer2" {
+			t.Errorf("expected data: 'lowerdir=/layer1:/layer2' got: %v", data)
+		}
+		return nil
+	}
+
+	err := Mount([]string{"/layer1", "/layer2"}, "", "", "/root", false)
+	if err != nil {
+		t.Fatalf("expected no error got: %v", err)
+	}
+	if !rootCreated {
+		t.Fatal("expected root to be created")
+	}
+}

internal/storage/plan9/plan9.go
@@ -1,0 +1,76 @@
+// +build linux
+
+package plan9
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"github.com/Microsoft/opengcs/service/gcs/transport"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+// Test dependencies
+var (
+	osMkdirAll   = os.MkdirAll
+	osRemoveAll  = os.RemoveAll
+	syscallMount = syscall.Mount
+)
+
+// Mount dials a connection from `vsock` and mounts a Plan9 share to `target`.
+//
+// `target` will be created. On mount failure the created `target` will be
+// automatically cleaned up.
+func Mount(vsock transport.Transport, target, share string, port uint32, readonly bool) (err error) {
+	activity := "plan9::Mount"
+	log := logrus.WithFields(logrus.Fields{
+		"target":   target,
+		"share":    share,
+		"port":     port,
+		"readonly": readonly,
+	})
+	log.Debug(activity + " - Begin Operation")
+	defer func() {
+		if err != nil {
+			log.Data[logrus.ErrorKey] = err
+			log.Error(activity + " - End Operation")
+		} else {
+			log.Debug(activity + " - End Operation")
+		}
+	}()
+
+	if err := osMkdirAll(target, 0700); err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			osRemoveAll(target)
+		}
+	}()
+	conn, err := vsock.Dial(port)
+	if err != nil {
+		return errors.Wrapf(err, "could not connect to plan9 server for %s", target)
+	}
+	f, err := conn.File()
+	conn.Close()
+	if err != nil {
+		return errors.Wrapf(err, "could not get file for plan9 connection for %s", target)
+	}
+	defer f.Close()
+
+	var mountOptions uintptr
+	data := fmt.Sprintf("trans=fd,rfdno=%d,wfdno=%d", f.Fd(), f.Fd())
+	if readonly {
+		mountOptions |= syscall.MS_RDONLY
+		data += ",noload"
+	}
+	if share != "" {
+		data += ",aname=" + share
+	}
+	if err := syscallMount(target, target, "9p", mountOptions, data); err != nil {
+		return errors.Wrapf(err, "failed to mount directory for mapped directory %s", target)
+	}
+	return nil
+}

internal/storage/pmem/pmem.go
@@ -1,0 +1,54 @@
+// +build linux
+
+package pmem
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Test dependencies
+var (
+	osMkdirAll   = os.MkdirAll
+	osRemoveAll  = os.RemoveAll
+	syscallMount = syscall.Mount
+)
+
+// Mount mounts the pmem device at `/dev/pmem<device>` to `target`.
+//
+// `target` will be created. On mount failure the created `target` will be
+// automatically cleaned up.
+//
+// Note: For now the platform only supports readonly pmem that is assumed to be
+// `dax`, `ext4`.
+func Mount(device uint32, target string) (err error) {
+	activity := "pmem::Mount"
+	log := logrus.WithFields(logrus.Fields{
+		"device": device,
+		"target": target,
+	})
+	log.Debug(activity + " - Begin Operation")
+	defer func() {
+		if err != nil {
+			log.Data[logrus.ErrorKey] = err
+			log.Error(activity + " - End Operation")
+		} else {
+			log.Debug(activity + " - End Operation")
+		}
+	}()
+
+	if err := osMkdirAll(target, 0700); err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			osRemoveAll(target)
+		}
+	}()
+	source := fmt.Sprintf("/dev/pmem%d", device)
+	flags := uintptr(syscall.O_RDONLY)
+	return syscallMount(source, target, "ext4", flags, "noload,dax")
+}

microsoft/hcsshim

internal/storage/overlay/overlay_test.go
@@ -1,0 +1,129 @@
+// +build linux
+
+package overlay
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+type undo struct {
+	osMkdirAll  func(string, os.FileMode) error
+	osRemoveAll func(string) error
+	unixMount   func(string, string, string, uintptr, string) error
+}
+
+func (u *undo) Close() {
+	osMkdirAll = u.osMkdirAll
+	unixMount = u.unixMount
+}
+
+// Captures the actual product function context and returns them on `Close()`.
+// It sets the test dependencies to `nil` so that any unpredicted call to that
+// function will cause the test to panic.
+func captureTestMethods() *undo {
+	u := &undo{
+		osMkdirAll:  osMkdirAll,
+		osRemoveAll: osRemoveAll,
+		unixMount:   unixMount,
+	}
+	osMkdirAll = nil
+	osRemoveAll = nil
+	unixMount = nil
+	return u
+}
+
+func Test_Mount_Success(t *testing.T) {
+	undo := captureTestMethods()
+	defer undo.Close()
+
+	var upperCreated, workCreated, rootCreated bool
+	osMkdirAll = func(path string, perm os.FileMode) error {
+		if perm != 0755 {
+			t.Errorf("os.MkdirAll at: %s, perm: %v expected perm: 0755", path, perm)
+		}
+		switch path {
+		case "/upper":
+			upperCreated = true
+			return nil
+		case "/work":
+			workCreated = true
+			return nil
+		case "/root":
+			rootCreated = true
+			return nil
+		}
+		return errors.New("unexpected os.MkdirAll path")
+	}
+	unixMount = func(source string, target string, fstype string, flags uintptr, data string) error {
+		if source != "overlay" {
+			t.Errorf("expected source: 'overlay' got: %v", source)
+		}
+		if target != "/root" {
+			t.Errorf("expected target: '/root' got: %v", target)
+		}
+		if fstype != "overlay" {
+			t.Errorf("expected fstype: 'overlay' got: %v", fstype)
+		}
+		if flags != 0 {
+			t.Errorf("expected flags: '0' got: %v", flags)
+		}
+		if data != "lowerdir=/layer1:/layer2,upperdir=/upper,workdir=/work" {
+			t.Errorf("expected data: 'lowerdir=/layer1:/layer2,upperdir=/upper,workdir=/work' got: %v", data)
+		}
+		return nil
+	}
+
+	err := Mount([]string{"/layer1", "/layer2"}, "/upper", "/work", "/root", false)
+	if err != nil {
+		t.Fatalf("expected no error got: %v", err)
+	}
+	if !upperCreated || !workCreated || !rootCreated {
+		t.Fatalf("expected all upper: %v, work: %v, root: %v to be created", upperCreated, workCreated, rootCreated)
+	}
+}
+
+func Test_Mount_Readonly_Success(t *testing.T) {
+	undo := captureTestMethods()
+	defer undo.Close()
+
+	var rootCreated bool
+	osMkdirAll = func(path string, perm os.FileMode) error {
+		if perm != 0755 {
+			t.Errorf("os.MkdirAll at: %s, perm: %v expected perm: 0755", path, perm)
+		}
+		switch path {
+		case "/root":
+			rootCreated = true
+			return nil
+		}
+		return errors.New("unexpected os.MkdirAll path")
+	}
+	unixMount = func(source string, target string, fstype string, flags uintptr, data string) error {
+		if source != "overlay" {
+			t.Errorf("expected source: 'overlay' got: %v", source)
+		}
+		if target != "/root" {
+			t.Errorf("expected target: '/root' got: %v", target)
+		}
+		if fstype != "overlay" {
+			t.Errorf("expected fstype: 'overlay' got: %v", fstype)
+		}
+		if flags != 0 {
+			t.Errorf("expected flags: '0' got: %v", flags)
+		}
+		if data != "lowerdir=/layer1:/layer2" {
+			t.Errorf("expected data: 'lowerdir=/layer1:/layer2' got: %v", data)
+		}
+		return nil
+	}
+
+	err := Mount([]string{"/layer1", "/layer2"}, "", "", "/root", false)
+	if err != nil {
+		t.Fatalf("expected no error got: %v", err)
+	}
+	if !rootCreated {
+		t.Fatal("expected root to be created")
+	}
+}

internal/storage/plan9/plan9.go
@@ -1,0 +1,76 @@
+// +build linux
+
+package plan9
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Microsoft/opengcs/service/gcs/transport"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
+)
+
+// Test dependencies
+var (
+	osMkdirAll  = os.MkdirAll
+	osRemoveAll = os.RemoveAll
+	unixMount   = unix.Mount
+)
+
+// Mount dials a connection from `vsock` and mounts a Plan9 share to `target`.
+//
+// `target` will be created. On mount failure the created `target` will be
+// automatically cleaned up.
+func Mount(vsock transport.Transport, target, share string, port uint32, readonly bool) (err error) {
+	activity := "plan9::Mount"
+	log := logrus.WithFields(logrus.Fields{
+		"target":   target,
+		"share":    share,
+		"port":     port,
+		"readonly": readonly,
+	})
+	log.Debug(activity + " - Begin Operation")
+	defer func() {
+		if err != nil {
+			log.Data[logrus.ErrorKey] = err
+			log.Error(activity + " - End Operation")
+		} else {
+			log.Debug(activity + " - End Operation")
+		}
+	}()
+
+	if err := osMkdirAll(target, 0700); err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			osRemoveAll(target)
+		}
+	}()
+	conn, err := vsock.Dial(port)
+	if err != nil {
+		return errors.Wrapf(err, "could not connect to plan9 server for %s", target)
+	}
+	f, err := conn.File()
+	conn.Close()
+	if err != nil {
+		return errors.Wrapf(err, "could not get file for plan9 connection for %s", target)
+	}
+	defer f.Close()
+
+	var mountOptions uintptr
+	data := fmt.Sprintf("trans=fd,rfdno=%d,wfdno=%d", f.Fd(), f.Fd())
+	if readonly {
+		mountOptions |= unix.MS_RDONLY
+		data += ",noload"
+	}
+	if share != "" {
+		data += ",aname=" + share
+	}
+	if err := unixMount(target, target, "9p", mountOptions, data); err != nil {
+		return errors.Wrapf(err, "failed to mount directory for mapped directory %s", target)
+	}
+	return nil
+}

internal/storage/pmem/pmem.go
@@ -1,0 +1,54 @@
+// +build linux
+
+package pmem
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
+)
+
+// Test dependencies
+var (
+	osMkdirAll  = os.MkdirAll
+	osRemoveAll = os.RemoveAll
+	unixMount   = unix.Mount
+)
+
+// Mount mounts the pmem device at `/dev/pmem<device>` to `target`.
+//
+// `target` will be created. On mount failure the created `target` will be
+// automatically cleaned up.
+//
+// Note: For now the platform only supports readonly pmem that is assumed to be
+// `dax`, `ext4`.
+func Mount(device uint32, target string) (err error) {
+	activity := "pmem::Mount"
+	log := logrus.WithFields(logrus.Fields{
+		"device": device,
+		"target": target,
+	})
+	log.Debug(activity + " - Begin Operation")
+	defer func() {
+		if err != nil {
+			log.Data[logrus.ErrorKey] = err
+			log.Error(activity + " - End Operation")
+		} else {
+			log.Debug(activity + " - End Operation")
+		}
+	}()
+
+	if err := osMkdirAll(target, 0700); err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			osRemoveAll(target)
+		}
+	}()
+	source := fmt.Sprintf("/dev/pmem%d", device)
+	flags := uintptr(unix.MS_RDONLY)
+	return unixMount(source, target, "ext4", flags, "noload,dax")
+}