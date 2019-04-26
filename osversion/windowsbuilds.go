ef775b6a90143374b7d5d99ed8e44a512e3cd162
Sebastiaan van Stijn <github@gone.nl>
2019-04-26 10:26:41 -0700

    Fix Windows Server version for RS4 in comment
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- osversion/windowsbuilds.go
+++ osversion/windowsbuilds.go
@@ -14,7 +14,7 @@ const (
 	RS3 = 16299
 
 	// RS4 (version 1803, codename "Redstone 4") corresponds to Windows Server
-	// 1809 (Semi-Annual Channel (SAC)), and Windows 10 (April 2018 Update).
+	// 1803 (Semi-Annual Channel (SAC)), and Windows 10 (April 2018 Update).
 	RS4 = 17134
 
 	// RS5 (version 1809, codename "Redstone 5") corresponds to Windows Server
