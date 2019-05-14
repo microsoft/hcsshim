3c9297345421ffa77a4a5492b5237bd442f0f522
John Starks <jostarks@microsoft.com>
2019-05-14 14:26:08 -0700

    uvmboot: Add WCOW support

--- internal/tools/uvmboot/main.go
+++ internal/tools/uvmboot/main.go
@@ -2,23 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"strings"
 	"sync"
 	"time"
 
-	"github.com/Microsoft/hcsshim/internal/hcsoci"
 	"github.com/Microsoft/hcsshim/internal/uvm"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
 const (
-	kernelArgsArgName           = "kernel-args"
-	rootFSTypeArgName           = "root-fs-type"
-	vpMemMaxCountArgName        = "vpmem-max-count"
-	vpMemMaxSizeArgName         = "vpmem-max-size"
 	cpusArgName                 = "cpus"
 	memoryArgName               = "memory"
 	allowOvercommitArgName      = "allow-overcommit"
@@ -26,15 +19,11 @@ const (
 	measureArgName              = "measure"
 	parallelArgName             = "parallel"
 	countArgName                = "count"
-	kernelDirectArgName         = "kernel-direct"
-	execCommandLineArgName      = "exec"
-	forwardStdoutArgName        = "fwd-stdout"
-	forwardStderrArgName        = "fwd-stderr"
 	debugArgName                = "debug"
-	outputHandlingArgName       = "output-handling"
-	consolePipeArgName          = "console-pipe"
 	gcsArgName                  = "gcs"
 	externalBridgeArgName       = "external-bridge"
+
+	execCommandLineArgName = "exec"
 )
 
 func main() {
@@ -88,171 +77,17 @@ func main() {
 	}
 
 	app.Commands = []cli.Command{
-		{
-			Name:  "lcow",
-			Usage: "Boot an LCOW UVM",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  kernelArgsArgName,
-					Value: "",
-					Usage: "Additional arguments to pass to the kernel",
-				},
-				cli.StringFlag{
-					Name:  rootFSTypeArgName,
-					Usage: "Either 'initrd' or 'vhd'. (default: 'vhd' if rootfs.vhd exists)",
-				},
-				cli.UintFlag{
-					Name:  vpMemMaxCountArgName,
-					Usage: "Number of VPMem devices on the UVM. Uses hcsshim default if not specified",
-				},
-				cli.Uint64Flag{
-					Name:  vpMemMaxSizeArgName,
-					Usage: "Size of each VPMem device, in MB. Uses hcsshim default if not specified",
-				},
-				cli.BoolFlag{
-					Name:  kernelDirectArgName,
-					Usage: "Use kernel direct booting for UVM (default: true on builds >= 18286)",
-				},
-				cli.StringFlag{
-					Name:  execCommandLineArgName,
-					Usage: "Command to execute in the UVM.",
-				},
-				cli.BoolFlag{
-					Name:  forwardStdoutArgName,
-					Usage: "Whether stdout from the process in the UVM should be forwarded",
-				},
-				cli.BoolFlag{
-					Name:  forwardStderrArgName,
-					Usage: "Whether stderr from the process in the UVM should be forwarded",
-				},
-				cli.StringFlag{
-					Name:  outputHandlingArgName,
-					Usage: "Controls how output from UVM is handled. Use 'stdout' to print all output to stdout",
-				},
-				cli.StringFlag{
-					Name:  consolePipeArgName,
-					Usage: "Named pipe for serial console output (which will be enabled)",
-				},
-			},
-			Action: func(c *cli.Context) error {
-				if c.GlobalBool("debug") {
-					logrus.SetLevel(logrus.DebugLevel)
-				} else {
-					logrus.SetLevel(logrus.WarnLevel)
-				}
+		lcowCommand,
+		wcowCommand,
+	}
 
-				parallelCount := c.GlobalInt(parallelArgName)
-
-				var wg sync.WaitGroup
-				wg.Add(parallelCount)
-
-				workChan := make(chan int)
-
-				runFunc := func(workChan <-chan int) {
-					for {
-						i, ok := <-workChan
-
-						if !ok {
-							wg.Done()
-							return
-						}
-
-						id := fmt.Sprintf("uvmboot-%d", i)
-
-						options := uvm.NewDefaultOptionsLCOW(id, "")
-						useGcs := c.GlobalBool(gcsArgName)
-						options.UseGuestConnection = useGcs
-
-						if c.GlobalIsSet(cpusArgName) {
-							options.ProcessorCount = int32(c.GlobalUint64(cpusArgName))
-						}
-						if c.GlobalIsSet(memoryArgName) {
-							options.MemorySizeInMB = int32(c.GlobalUint64(memoryArgName))
-						}
-						if c.GlobalIsSet(allowOvercommitArgName) {
-							options.AllowOvercommit = c.GlobalBool(allowOvercommitArgName)
-						}
-						if c.GlobalIsSet(enableDeferredCommitArgName) {
-							options.EnableDeferredCommit = c.GlobalBool(enableDeferredCommitArgName)
-						}
-						if c.GlobalIsSet(externalBridgeArgName) {
-							options.ExternalGuestConnection = c.GlobalBool(externalBridgeArgName)
-						}
-
-						if c.IsSet(kernelDirectArgName) {
-							options.KernelDirect = c.Bool(kernelDirectArgName)
-						}
-						if c.IsSet(rootFSTypeArgName) {
-							switch strings.ToLower(c.String(rootFSTypeArgName)) {
-							case "initrd":
-								options.RootFSFile = uvm.InitrdFile
-								options.PreferredRootFSType = uvm.PreferredRootFSTypeInitRd
-							case "vhd":
-								options.RootFSFile = uvm.VhdFile
-								options.PreferredRootFSType = uvm.PreferredRootFSTypeVHD
-							default:
-								logrus.Fatalf("Unrecognized value '%s' for option %s", c.String(rootFSTypeArgName), rootFSTypeArgName)
-							}
-						}
-						if c.IsSet(kernelArgsArgName) {
-							options.KernelBootOptions = c.String(kernelArgsArgName)
-						}
-						if c.IsSet(vpMemMaxCountArgName) {
-							options.VPMemDeviceCount = uint32(c.Uint(vpMemMaxCountArgName))
-						}
-						if c.IsSet(vpMemMaxSizeArgName) {
-							options.VPMemSizeBytes = c.Uint64(vpMemMaxSizeArgName) * 1024 * 1024 // convert from MB to bytes
-						}
-						if !useGcs {
-							if c.IsSet(execCommandLineArgName) {
-								options.ExecCommandLine = c.String(execCommandLineArgName)
-							}
-							if c.IsSet(forwardStdoutArgName) {
-								options.ForwardStdout = c.Bool(forwardStdoutArgName)
-							}
-							if c.IsSet(forwardStderrArgName) {
-								options.ForwardStderr = c.Bool(forwardStderrArgName)
-							}
-							if c.IsSet(outputHandlingArgName) {
-								switch strings.ToLower(c.String(outputHandlingArgName)) {
-								case "stdout":
-									options.OutputHandler = uvm.OutputHandler(func(r io.Reader) { io.Copy(os.Stdout, r) })
-								default:
-									logrus.Fatalf("Unrecognized value '%s' for option %s", c.String(outputHandlingArgName), outputHandlingArgName)
-								}
-							}
-						}
-						if c.IsSet(consolePipeArgName) {
-							options.ConsolePipe = c.String(consolePipeArgName)
-						}
-
-						if err := run(options, c); err != nil {
-							logrus.WithField("uvm-id", id).Error(err)
-						}
-					}
-				}
-
-				for i := 0; i < parallelCount; i++ {
-					go runFunc(workChan)
-				}
-
-				start := time.Now()
-
-				for i := 0; i < c.GlobalInt(countArgName); i++ {
-					workChan <- i
-				}
-
-				close(workChan)
-
-				wg.Wait()
-
-				if c.GlobalBool(measureArgName) {
-					fmt.Println("Elapsed time:", time.Since(start))
-				}
-
-				return nil
-			},
-		},
+	app.Before = func(c *cli.Context) error {
+		if c.GlobalBool("debug") {
+			logrus.SetLevel(logrus.DebugLevel)
+		} else {
+			logrus.SetLevel(logrus.WarnLevel)
+		}
+		return nil
 	}
 
 	err := app.Run(os.Args)
@@ -261,39 +96,51 @@ func main() {
 	}
 }
 
-func run(options *uvm.OptionsLCOW, c *cli.Context) error {
-	uvm, err := uvm.CreateLCOW(options)
-	if err != nil {
-		return err
+func setGlobalOptions(c *cli.Context, options *uvm.Options) {
+	if c.GlobalIsSet(cpusArgName) {
+		options.ProcessorCount = int32(c.GlobalUint64(cpusArgName))
 	}
-	defer uvm.Close()
-
-	if err := uvm.Start(); err != nil {
-		return err
+	if c.GlobalIsSet(memoryArgName) {
+		options.MemorySizeInMB = int32(c.GlobalUint64(memoryArgName))
 	}
-
-	if options.UseGuestConnection {
-		if err := execViaGcs(uvm, c); err != nil {
-			return err
-		}
-		uvm.Terminate()
-		uvm.Wait()
-		return uvm.ExitError()
+	if c.GlobalIsSet(allowOvercommitArgName) {
+		options.AllowOvercommit = c.GlobalBool(allowOvercommitArgName)
 	}
-
-	return uvm.Wait()
+	if c.GlobalIsSet(enableDeferredCommitArgName) {
+		options.EnableDeferredCommit = c.GlobalBool(enableDeferredCommitArgName)
+	}
+	if c.GlobalIsSet(externalBridgeArgName) {
+		options.ExternalGuestConnection = c.GlobalBool(externalBridgeArgName)
+	}
 }
 
-func execViaGcs(vm *uvm.UtilityVM, c *cli.Context) error {
-	cmd := hcsoci.Command(vm, "/bin/sh", "-c", c.String(execCommandLineArgName))
-	cmd.Log = logrus.NewEntry(logrus.StandardLogger())
-	if c.String(outputHandlingArgName) == "stdout" {
-		if c.Bool(forwardStdoutArgName) {
-			cmd.Stdout = os.Stdout
-		}
-		if c.Bool(forwardStderrArgName) {
-			cmd.Stderr = os.Stdout // match non-GCS behavior and forward to stdout
-		}
+func runMany(c *cli.Context, runFunc func(id string) error) {
+	parallelCount := c.GlobalInt(parallelArgName)
+
+	var wg sync.WaitGroup
+	wg.Add(parallelCount)
+	workChan := make(chan int)
+	for i := 0; i < parallelCount; i++ {
+		go func() {
+			for i := range workChan {
+				id := fmt.Sprintf("uvmboot-%d", i)
+				err := runFunc(id)
+				if err != nil {
+					logrus.WithField("uvm-id", id).WithError(err).Error("failed to run UVM")
+				}
+			}
+			wg.Done()
+		}()
 	}
-	return cmd.Run()
+
+	start := time.Now()
+	for i := 0; i < c.GlobalInt(countArgName); i++ {
+		workChan <- i
+	}
+
+	close(workChan)
+	wg.Wait()
+	if c.GlobalBool(measureArgName) {
+		fmt.Println("Elapsed time:", time.Since(start))
+	}
 }
