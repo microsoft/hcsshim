2dcf9121abc025834baaa0151f00c8bd3dc8bc25
Daniel Canter <dcanter@microsoft.com>
2021-04-28 02:45:49 -0700

    Getting ready for hcsshim merge
    * Move entire repo into opengcs subdir
    * `gofmt -s -w .` all the files to satisfy linter
    
    Signed-off-by: Daniel Canter <dcanter@microsoft.com>

---
+++ opengcs/internal/runtime/hcsv2/nvidia_utils.go
@@ -0,0 +1,105 @@
+// +build linux
+
+package hcsv2
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/Microsoft/opengcs/internal/storage/pci"
+	"github.com/Microsoft/opengcs/service/gcsutils/gcstools/generichook"
+	oci "github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/pkg/errors"
+)
+
+// path that the shim mounts the nvidia gpu vhd to in the uvm
+// this MUST match the path mapped to in the shim
+const lcowNvidiaMountPath = "/run/nvidia"
+
+// annotation to find the gpu capabilities on the container spec
+// must match the hcsshim annotation string for gpu capabilities
+const annotationContainerGPUCapabilities = "io.microsoft.container.gpu.capabilities"
+const nvidiaDebugFilePath = "/nvidia-container.log"
+
+const nvidiaToolBinary = "nvidia-container-cli"
+
+// TODO katiewasnothere: prestart hooks will be depracated, this needs to be moved to a createRuntime hook
+// described here: https://github.com/opencontainers/runtime-spec/blob/39c287c415bf86fb5b7506528d471db5405f8ca8/config.md#posix-platform-hooks
+// addNvidiaDevicePreHook builds the arguments for nvidia-container-cli and creates the prestart hook
+func addNvidiaDevicePreHook(ctx context.Context, spec *oci.Spec) error {
+	genericHookBinary := "generichook"
+	genericHookPath, err := exec.LookPath(genericHookBinary)
+	if err != nil {
+		return errors.Wrapf(err, "failed to find %s for container device support", genericHookBinary)
+	}
+
+	debugOption := fmt.Sprintf("--debug=%s", nvidiaDebugFilePath)
+
+	// TODO katiewasnothere: right now both host and container ldconfig do not work as expected for nvidia-container-cli
+	// ldconfig needs to be run in the container to setup the correct symlinks to the library files nvidia-container-cli
+	// maps into the container
+	args := []string{
+		genericHookPath,
+		nvidiaToolBinary,
+		debugOption,
+		"--load-kmods",
+		"--no-pivot",
+		"configure",
+		"--ldconfig=@/sbin/ldconfig",
+	}
+	if capabilities, ok := spec.Annotations[annotationContainerGPUCapabilities]; ok {
+		caps := strings.Split(capabilities, ",")
+		for _, c := range caps {
+			args = append(args, fmt.Sprintf("--%s", c))
+		}
+	}
+
+	for _, d := range spec.Windows.Devices {
+		switch d.IDType {
+		case "gpu":
+			busLocation, err := pci.FindDeviceBusLocationFromVMBusGUID(ctx, d.ID)
+			if err != nil {
+				return errors.Wrapf(err, "failed to find nvidia gpu bus location")
+			}
+			args = append(args, fmt.Sprintf("--device=%s", busLocation))
+		}
+	}
+
+	// add template for pid argument to be injected later by the generic hook binary
+	args = append(args, "--no-cgroups", "--pid={{pid}}", spec.Root.Path)
+
+	if spec.Hooks == nil {
+		spec.Hooks = &oci.Hooks{}
+	}
+
+	hookLogDebugFileEnvOpt := fmt.Sprintf("%s=%s", generichook.LogDebugFileEnvKey, nvidiaDebugFilePath)
+	hookEnv := append(updateEnvWithNvidiaVariables(), hookLogDebugFileEnvOpt)
+	nvidiaHook := oci.Hook{
+		Path: genericHookPath,
+		Args: args,
+		Env:  hookEnv,
+	}
+
+	spec.Hooks.Prestart = append(spec.Hooks.Prestart, nvidiaHook)
+	return nil
+}
+
+// updateEnvWithNvidiaVariables creates an env with the nvidia gpu vhd in PATH and insecure mode set
+func updateEnvWithNvidiaVariables() []string {
+	pathPrefix := "PATH="
+	nvidiaBin := fmt.Sprintf("%s/bin", lcowNvidiaMountPath)
+	env := os.Environ()
+	for i, v := range env {
+		if strings.HasPrefix(v, pathPrefix) {
+			newPath := fmt.Sprintf("%s:%s", v, nvidiaBin)
+			env[i] = newPath
+		}
+	}
+	// NVC_INSECURE_MODE allows us to run nvidia-container-cli without seccomp
+	// we don't currently use seccomp in the uvm, so avoid using it here for now as well
+	env = append(env, "NVC_INSECURE_MODE=1")
+	return env
+}
---
+++ opengcs/internal/storage/pci/pci_test.go
@@ -0,0 +1,43 @@
+// +build linux
+
+package pci
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func Test_WaitForPCIDeviceFromVMBusGUID_Success(t *testing.T) {
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+
+	vmBusGUID := "1111-2222-3333-4444"
+	pciDir := "pci1234:00"
+	busLocation := "1234:00:00.0"
+
+	vmbusWaitForDevicePath = func(ctx context.Context, vmbusGUIDPattern string) (string, error) {
+		vmBusPath := filepath.Join("/sys/bus/vmbus/devices", vmbusGUIDPattern)
+		vmBusDirPath, targetPattern := filepath.Split(vmBusPath)
+		if targetPattern == "pci*" {
+			return filepath.Join(vmBusDirPath, pciDir), nil
+		}
+		return "", nil
+	}
+
+	storageWaitForFileMatchingPattern = func(ctx context.Context, pattern string) (string, error) {
+		vmBusPciDirPath, targetPattern := filepath.Split(pattern)
+		if targetPattern == "1234:00*" {
+			return filepath.Join(vmBusPciDirPath, busLocation), nil
+		}
+		return "", nil
+	}
+
+	resultBusLocation, err := FindDeviceBusLocationFromVMBusGUID(ctx, vmBusGUID)
+	if err != nil {
+		t.Fatalf("expected to succeed, instead got: %v", err)
+	}
+	if resultBusLocation != busLocation {
+		t.Fatalf("result %s does not match expected result %s", resultBusLocation, busLocation)
+	}
+}
