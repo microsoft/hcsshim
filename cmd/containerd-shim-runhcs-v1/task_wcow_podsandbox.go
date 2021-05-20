bbf558965c70f14fac7d8649be1be8071d28b8a2
Daniel Canter <dcanter@microsoft.com>
2021-05-19 22:17:18 -0700

    Add containerd-shim plumbing for job containers
    * Add the necessary plumbing in containerd shim to be able to create a job container
    if asked for via the annotation.
    
    * Rework jobcontainers package a bit to return a resources struct to avoid some hacks during cleanup.
    This was resource cleanup for wcow/lcow is the exact same for job containers in the shim.
    
    * Change some of the layer code to handle taking in a volume mount point
    
    Signed-off-by: Daniel Canter <dcanter@microsoft.com>

--- cmd/containerd-shim-runhcs-v1/task_wcow_podsandbox.go
+++ cmd/containerd-shim-runhcs-v1/task_wcow_podsandbox.go
@@ -284,6 +284,9 @@ func (wpst *wcowPodSandboxTask) Share(ctx context.Context, req *shimdiag.ShareRe
 
 func (wpst *wcowPodSandboxTask) Stats(ctx context.Context) (*stats.Statistics, error) {
 	stats := &stats.Statistics{}
+	if wpst.host == nil {
+		return stats, nil
+	}
 	vmStats, err := wpst.host.Stats(ctx)
 	if err != nil && !isStatsNotFound(err) {
 		return nil, err
