f01e3e931b93351b7d569c5dece07e43b64e19ca
Daniel Canter <dcanter@microsoft.com>
2021-06-22 16:19:03 -0700

    Get rid of redundant logs in HCN version range checks
    Kubeproxy logs are filled with redudnant version check spam from an unexported call that's invoked
    as part of checking if a feature is supported. The logs don't detail what feature(s) are even being checked
    so it just seems like spam. With the way things are implemented all of the hcn features are checked for support in
    any of the `hcn.XSupported()` calls not just the one being checked, so these logs come up quite a bit if there's
    many features that aren't supported on the machine.
    
    Add two new logs in a sync.Once that logs the HNS version and supported features. This should be enough
    to investigate version issues.
    
    Should remedy https://github.com/microsoft/hcsshim/issues/1043
    
    Signed-off-by: Daniel Canter <dcanter@microsoft.com>
    (cherry picked from commit 6288bb97125c8a25d79b87ffd3a597258fa4e0bc)
    Signed-off-by: Daniel Canter <dcanter@microsoft.com>

--- hcn/hcnsupport.go
+++ hcn/hcnsupport.go
@@ -1,8 +1,13 @@
 package hcn
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/sirupsen/logrus"
 )
+
+var versionOnce sync.Once
 
 // SupportedFeatures are the features provided by the Service.
 type SupportedFeatures struct {
@@ -72,6 +77,17 @@ func GetSupportedFeatures() SupportedFeatures {
 	features.L4WfpProxy = isFeatureSupported(globals.Version, L4WfpProxyPolicyVersion)
 	features.TierAcl = isFeatureSupported(globals.Version, TierAclPolicyVersion)
 
+	// Only print the HCN version and features supported once, instead of everytime this is invoked. These logs are useful to
+	// debug incidents where there's confusion on if a feature is supported on the host machine. The sync.Once helps to avoid redundant
+	// spam of these anytime a check needs to be made for if an HCN feature is supported. This is a common occurrence in kubeproxy
+	// for example.
+	versionOnce.Do(func() {
+		logrus.WithFields(logrus.Fields{
+			"version":           fmt.Sprintf("%+v", globals.Version),
+			"supportedFeatures": fmt.Sprintf("%+v", features),
+		}).Info("HCN feature check")
+	})
+
 	return features
 }
 
@@ -87,19 +103,15 @@ func isFeatureSupported(currentVersion Version, versionsSupported VersionRanges)
 
 func isFeatureInRange(currentVersion Version, versionRange VersionRange) bool {
 	if currentVersion.Major < versionRange.MinVersion.Major {
-		logrus.Infof("currentVersion.Major < versionRange.MinVersion.Major: %v, %v", currentVersion.Major, versionRange.MinVersion.Major)
 		return false
 	}
 	if currentVersion.Major > versionRange.MaxVersion.Major {
-		logrus.Infof("currentVersion.Major > versionRange.MaxVersion.Major: %v, %v", currentVersion.Major, versionRange.MaxVersion.Major)
 		return false
 	}
 	if currentVersion.Major == versionRange.MinVersion.Major && currentVersion.Minor < versionRange.MinVersion.Minor {
-		logrus.Infof("currentVersion.Minor < versionRange.MinVersion.Major: %v, %v", currentVersion.Minor, versionRange.MinVersion.Minor)
 		return false
 	}
 	if currentVersion.Major == versionRange.MaxVersion.Major && currentVersion.Minor > versionRange.MaxVersion.Minor {
-		logrus.Infof("currentVersion.Minor > versionRange.MaxVersion.Major: %v, %v", currentVersion.Minor, versionRange.MaxVersion.Minor)
 		return false
 	}
 	return true
