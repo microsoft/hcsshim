57c5d6094e233bbb15f08b662e9eb3edafb0b131
Daniel Canter <dcanter@microsoft.com>
2021-08-19 13:24:02 -0700

    Fix GetSupportedFeatures' comment to please godoc
    Signed-off-by: Daniel Canter <dcanter@microsoft.com>

--- hcn/hcnsupport.go
+++ hcn/hcnsupport.go
@@ -65,8 +65,9 @@ func GetCachedSupportedFeatures() (SupportedFeatures, error) {
 	return supportedFeatures, featuresErr
 }
 
-// Deprecated: Use GetCachedSupportedFeatures instead.
 // GetSupportedFeatures returns the features supported by the Service.
+//
+// Deprecated: Use GetCachedSupportedFeatures instead.
 func GetSupportedFeatures() SupportedFeatures {
 	features, err := GetCachedSupportedFeatures()
 	if err != nil {
