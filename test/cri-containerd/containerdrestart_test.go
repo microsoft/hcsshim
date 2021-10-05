2d35b70f54af6e3306800c2bb565005900f7db39
Kevin Parsons <kevpar@microsoft.com>
2021-10-05 16:18:53 -0700

    cri-containerd.test: Add containerd restart test
    Adds a test case that runs a pod+container, restarts containerd, then
    verifies that the pod+container were terminated. This validates the
    change made in the CRI fork [1] to terminate containers when containerd
    is restarted.
    
    [1]: https://github.com/kevpar/cri/commit/f8e83e63cc027d0e9c0c984f9db3cba58d3672d4
    
    Signed-off-by: Kevin Parsons <kevpar@microsoft.com>

---
+++ test/cri-containerd/containerdrestart_test.go
@@ -0,0 +1,69 @@
+// +build functional
+
+package cri_containerd
+
+import (
+	"context"
+	"testing"
+
+	runtime "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+)
+
+// CRI will terminate any running containers when it is restarted.
+// Run a container, restart containerd, validate the container is terminated.
+func Test_ContainerdRestart_LCOW(t *testing.T) {
+	requireFeatures(t, featureLCOW)
+
+	pullRequiredLCOWImages(t, []string{imageLcowK8sPause, imageLcowAlpine})
+
+	client := newTestRuntimeClient(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sandboxRequest := getRunPodSandboxRequest(t, lcowRuntimeHandler, nil)
+
+	podID := runPodSandbox(t, client, ctx, sandboxRequest)
+	defer removePodSandbox(t, client, ctx, podID)
+	defer stopPodSandbox(t, client, ctx, podID)
+
+	request := &runtime.CreateContainerRequest{
+		PodSandboxId: podID,
+		Config: &runtime.ContainerConfig{
+			Metadata: &runtime.ContainerMetadata{
+				Name: t.Name() + "-Container",
+			},
+			Image: &runtime.ImageSpec{
+				Image: imageLcowAlpine,
+			},
+			Command: []string{
+				"top",
+			},
+		},
+		SandboxConfig: sandboxRequest.Config,
+	}
+
+	containerID := createContainer(t, client, ctx, request)
+	defer removeContainer(t, client, ctx, containerID)
+	startContainer(t, client, ctx, containerID)
+	defer stopContainer(t, client, ctx, containerID)
+
+	t.Log("Restart containerd")
+	stopContainerd(t)
+	startContainerd(t)
+	client = newTestRuntimeClient(t)
+
+	containerStatus, err := client.ContainerStatus(ctx, &runtime.ContainerStatusRequest{ContainerId: containerID})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if containerStatus.Status.State != runtime.ContainerState_CONTAINER_EXITED {
+		t.Errorf("Container was not terminated on containerd restart. Status is %d", containerStatus.Status.State)
+	}
+	podStatus, err := client.PodSandboxStatus(ctx, &runtime.PodSandboxStatusRequest{PodSandboxId: podID})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if podStatus.Status.State != runtime.PodSandboxState_SANDBOX_NOTREADY {
+		t.Errorf("Pod was not terminated on containerd restart. Status is %d", podStatus.Status.State)
+	}
+}
