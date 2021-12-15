f8cbd0b11c7ae5d6ab253851d6525aa745ed4f47
Danny Canter <36526702+dcantah@users.noreply.github.com>
2021-12-15 08:01:54 -0800

    Add conpty (pseudo console) package (#1228)
    * Add conpty (pseudo console) package
    
    This change adds a conpty package that houses go friendly wrappers around
    the Pseudo Console API in Windows. This will be used to support tty scenarios
    for Host Process containers.
    
    There's not many tests I can add here as you need to hook this up to a running
    process, where that work is coming.
    
    Signed-off-by: Daniel Canter <dcanter@microsoft.com>

--- test/vendor/github.com/Microsoft/hcsshim/internal/winapi/process.go
+++ test/vendor/github.com/Microsoft/hcsshim/internal/winapi/process.go
@@ -1,10 +1,80 @@
 package winapi
 
+import (
+	"errors"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
 const PROCESS_ALL_ACCESS uint32 = 2097151
 
-// DWORD GetProcessImageFileNameW(
-//	HANDLE hProcess,
-//	LPWSTR lpImageFileName,
-//	DWORD  nSize
+const (
+	PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE = 0x20016
+	PROC_THREAD_ATTRIBUTE_JOB_LIST      = 0x2000D
+)
+
+type ProcThreadAttributeList struct {
+	_ [1]byte
+}
+
+// typedef struct _STARTUPINFOEXW {
+// 	STARTUPINFOW                 StartupInfo;
+// 	LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList;
+// } STARTUPINFOEXW, *LPSTARTUPINFOEXW;
+type StartupInfoEx struct {
+	// This is a recreation of the same binding from the stdlib. The x/sys/windows variant for whatever reason
+	// doesn't work when updating the list for the pseudo console attribute. It has the process immediately exit
+	// with exit code 0xc0000142 shortly after start.
+	windows.StartupInfo
+	ProcThreadAttributeList *ProcThreadAttributeList
+}
+
+// NewProcThreadAttributeList allocates a new ProcThreadAttributeList, with
+// the requested maximum number of attributes. This must be cleaned up by calling
+// DeleteProcThreadAttributeList.
+func NewProcThreadAttributeList(maxAttrCount uint32) (*ProcThreadAttributeList, error) {
+	var size uintptr
+	err := initializeProcThreadAttributeList(nil, maxAttrCount, 0, &size)
+	if err != windows.ERROR_INSUFFICIENT_BUFFER {
+		if err == nil {
+			return nil, errors.New("unable to query buffer size from InitializeProcThreadAttributeList")
+		}
+		return nil, err
+	}
+	al := (*ProcThreadAttributeList)(unsafe.Pointer(&make([]byte, size)[0]))
+	err = initializeProcThreadAttributeList(al, maxAttrCount, 0, &size)
+	if err != nil {
+		return nil, err
+	}
+	return al, nil
+}
+
+// BOOL InitializeProcThreadAttributeList(
+// 	[out, optional] LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList,
+// 	[in]            DWORD                        dwAttributeCount,
+// 					DWORD                        dwFlags,
+// 	[in, out]       PSIZE_T                      lpSize
 // );
-//sys GetProcessImageFileName(hProcess windows.Handle, imageFileName *uint16, nSize uint32) (size uint32, err error) = kernel32.GetProcessImageFileNameW
+//
+//sys initializeProcThreadAttributeList(lpAttributeList *ProcThreadAttributeList, dwAttributeCount uint32, dwFlags uint32, lpSize *uintptr) (err error) = kernel32.InitializeProcThreadAttributeList
+
+// void DeleteProcThreadAttributeList(
+// 	[in, out] LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList
+// );
+//
+//sys DeleteProcThreadAttributeList(lpAttributeList *ProcThreadAttributeList) = kernel32.DeleteProcThreadAttributeList
+
+// BOOL UpdateProcThreadAttribute(
+// 	[in, out]       LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList,
+// 	[in]            DWORD                        dwFlags,
+// 	[in]            DWORD_PTR                    Attribute,
+// 	[in]            PVOID                        lpValue,
+// 	[in]            SIZE_T                       cbSize,
+// 	[out, optional] PVOID                        lpPreviousValue,
+// 	[in, optional]  PSIZE_T                      lpReturnSize
+// );
+//
+//sys UpdateProcThreadAttribute(lpAttributeList *ProcThreadAttributeList, dwFlags uint32, attribute uintptr, lpValue unsafe.Pointer, cbSize uintptr, lpPreviousValue unsafe.Pointer, lpReturnSize *uintptr) (err error) = kernel32.UpdateProcThreadAttribute
+
+//sys CreateProcessAsUser(token windows.Token, appName *uint16, commandLine *uint16, procSecurity *windows.SecurityAttributes, threadSecurity *windows.SecurityAttributes, inheritHandles bool, creationFlags uint32, env *uint16, currentDir *uint16, startupInfo *windows.StartupInfo, outProcInfo *windows.ProcessInformation) (err error) = advapi32.CreateProcessAsUserW
