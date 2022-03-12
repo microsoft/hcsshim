e7e271722cb8e30d2a0228dcb776021f46a09df7
Maksim An <maksiman@microsoft.com>
2022-03-11 17:12:39 -0800

    Add tests for security policy enforcement
    Add basic positive and negative tests for security policy enforcement.
    
    Signed-off-by: Maksim An <maksiman@microsoft.com>

--- test/cri-containerd/policy_test.go
+++ test/cri-containerd/policy_test.go
@@ -9,10 +9,11 @@ import (
 	"strings"
 	"testing"
 
+	runtime "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+
 	"github.com/Microsoft/hcsshim/internal/tools/securitypolicy/helpers"
 	"github.com/Microsoft/hcsshim/pkg/annotations"
 	"github.com/Microsoft/hcsshim/pkg/securitypolicy"
-	"k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 )
 
 var (
@@ -24,6 +25,20 @@ type configOpt func(*securitypolicy.ContainerConfig) error
 func withExpectedMounts(em []string) configOpt {
 	return func(conf *securitypolicy.ContainerConfig) error {
 		conf.ExpectedMounts = append(conf.ExpectedMounts, em...)
+		return nil
+	}
+}
+
+func withEnvVarRules(envRules []securitypolicy.EnvRuleConfig) configOpt {
+	return func(config *securitypolicy.ContainerConfig) error {
+		config.EnvRules = append(config.EnvRules, envRules...)
+		return nil
+	}
+}
+
+func withWorkingDir(workingDir string) configOpt {
+	return func(config *securitypolicy.ContainerConfig) error {
+		config.WorkingDir = workingDir
 		return nil
 	}
 }
@@ -50,7 +65,7 @@ func sandboxSecurityPolicy(t *testing.T) string {
 	return policyString
 }
 
-func alpineSecurityPolicy(t *testing.T) string {
+func alpineSecurityPolicy(t *testing.T, opts ...configOpt) string {
 	defaultContainers := helpers.DefaultContainerConfigs()
 	alpineContainer := securitypolicy.NewContainerConfig(
 		"alpine:latest",
@@ -61,6 +76,12 @@ func alpineSecurityPolicy(t *testing.T) string {
 		[]string{},
 	)
 
+	for _, o := range opts {
+		if err := o(&alpineContainer); err != nil {
+			t.Fatalf("failed to apply config opt: %s", err)
+		}
+	}
+
 	containers := append(defaultContainers, alpineContainer)
 	policyString, err := securityPolicyFromContainers(containers)
 	if err != nil {
@@ -69,7 +90,7 @@ func alpineSecurityPolicy(t *testing.T) string {
 	return policyString
 }
 
-func sandboxRequestWithPolicy(t *testing.T, policy string) *v1alpha2.RunPodSandboxRequest {
+func sandboxRequestWithPolicy(t *testing.T, policy string) *runtime.RunPodSandboxRequest {
 	return getRunPodSandboxRequest(
 		t,
 		lcowRuntimeHandler,
@@ -148,8 +169,8 @@ func syncContainerConfigs(writePath, waitPath string) (writer, waiter *securityp
 func syncContainerRequests(
 	writer, waiter *securitypolicy.ContainerConfig,
 	podID string,
-	podConfig *v1alpha2.PodSandboxConfig,
-) (writerReq, waiterReq *v1alpha2.CreateContainerRequest) {
+	podConfig *runtime.PodSandboxConfig,
+) (writerReq, waiterReq *runtime.CreateContainerRequest) {
 	writerReq = getCreateContainerRequest(
 		podID,
 		"alpine-writer",
@@ -157,7 +178,7 @@ func syncContainerRequests(
 		writer.Command,
 		podConfig,
 	)
-	writerReq.Config.Mounts = append(writerReq.Config.Mounts, &v1alpha2.Mount{
+	writerReq.Config.Mounts = append(writerReq.Config.Mounts, &runtime.Mount{
 		HostPath:      "sandbox://host/path",
 		ContainerPath: "/mnt/shared/container-A",
 	})
@@ -169,7 +190,7 @@ func syncContainerRequests(
 		waiter.Command,
 		podConfig,
 	)
-	waiterReq.Config.Mounts = append(waiterReq.Config.Mounts, &v1alpha2.Mount{
+	waiterReq.Config.Mounts = append(waiterReq.Config.Mounts, &runtime.Mount{
 		// The HostPath must be the same as for the "writer" container
 		HostPath:      "sandbox://host/path",
 		ContainerPath: "/mnt/shared/container-B",
@@ -246,7 +267,7 @@ func Test_RunContainers_WithSyncHooks_InvalidWaitPath(t *testing.T) {
 	defer removeContainer(t, client, ctx, cidWriter)
 	defer stopContainer(t, client, ctx, cidWriter)
 
-	_, err = client.StartContainer(ctx, &v1alpha2.StartContainerRequest{
+	_, err = client.StartContainer(ctx, &runtime.StartContainerRequest{
 		ContainerId: cidWaiter,
 	})
 	expectedErrString := "timeout while waiting for path"
@@ -258,5 +279,144 @@ func Test_RunContainers_WithSyncHooks_InvalidWaitPath(t *testing.T) {
 		if !strings.Contains(err.Error(), expectedErrString) {
 			t.Fatalf("expected error: %q, got: %q", expectedErrString, err)
 		}
+	}
+}
+
+func Test_RunContainer_ValidContainerConfigs_Allowed(t *testing.T) {
+	type sideEffect func(*runtime.CreateContainerRequest)
+	type config struct {
+		name string
+		sf   sideEffect
+		opts []configOpt
+	}
+
+	client := newTestRuntimeClient(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	requireFeatures(t, featureLCOW)
+	pullRequiredLCOWImages(t, []string{imageLcowK8sPause, imageLcowAlpine})
+
+	for _, testConfig := range []config{
+		{
+			name: "WorkingDir",
+			sf: func(req *runtime.CreateContainerRequest) {
+				req.Config.WorkingDir = "/root"
+			},
+			opts: []configOpt{withWorkingDir("/root")},
+		},
+		{
+			name: "EnvironmentVariable",
+			sf: func(req *runtime.CreateContainerRequest) {
+				req.Config.Envs = append(req.Config.Envs, &runtime.KeyValue{
+					Key:   "KEY",
+					Value: "VALUE",
+				})
+			},
+			opts: []configOpt{
+				withEnvVarRules(
+					[]securitypolicy.EnvRuleConfig{
+						{
+							Strategy: securitypolicy.EnvVarRuleString,
+							Rule:     "KEY=VALUE",
+						},
+					}),
+			},
+		},
+	} {
+		t.Run(testConfig.name, func(t *testing.T) {
+			alpinePolicy := alpineSecurityPolicy(t, testConfig.opts...)
+			sandboxRequest := sandboxRequestWithPolicy(t, alpinePolicy)
+
+			podID := runPodSandbox(t, client, ctx, sandboxRequest)
+			defer removePodSandbox(t, client, ctx, podID)
+			defer stopPodSandbox(t, client, ctx, podID)
+
+			containerRequest := getCreateContainerRequest(
+				podID,
+				"alpine-with-policy",
+				"alpine:latest",
+				validPolicyAlpineCommand,
+				sandboxRequest.Config,
+			)
+			testConfig.sf(containerRequest)
+
+			containerID := createContainer(t, client, ctx, containerRequest)
+			startContainer(t, client, ctx, containerID)
+			defer removeContainer(t, client, ctx, containerID)
+			defer stopContainer(t, client, ctx, containerID)
+		})
+	}
+}
+
+func Test_RunContainer_InvalidContainerConfigs_NotAllowed(t *testing.T) {
+	type sideEffect func(*runtime.CreateContainerRequest)
+	type config struct {
+		name          string
+		sf            sideEffect
+		expectedError string
+	}
+
+	client := newTestRuntimeClient(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	requireFeatures(t, featureLCOW)
+	pullRequiredLCOWImages(t, []string{imageLcowK8sPause, imageLcowAlpine})
+
+	alpinePolicy := alpineSecurityPolicy(t)
+	for _, testConfig := range []config{
+		{
+			name: "InvalidWorkingDir",
+			sf: func(req *runtime.CreateContainerRequest) {
+				req.Config.WorkingDir = "/non/existent"
+			},
+			expectedError: "working_dir \"/non/existent\" unmatched by policy rule",
+		},
+		{
+			name: "InvalidCommand",
+			sf: func(req *runtime.CreateContainerRequest) {
+				req.Config.Command = []string{"ash", "-c", "echo 'invalid command'"}
+			},
+			expectedError: "command [ash -c echo 'invalid command'] doesn't match policy",
+		},
+		{
+			name: "InvalidEnvironmentVariable",
+			sf: func(req *runtime.CreateContainerRequest) {
+				req.Config.Envs = append(req.Config.Envs, &runtime.KeyValue{
+					Key:   "KEY",
+					Value: "VALUE",
+				})
+			},
+			expectedError: "env variable KEY=VALUE unmatched by policy rule",
+		},
+	} {
+		t.Run(testConfig.name, func(t *testing.T) {
+			sandboxRequest := sandboxRequestWithPolicy(t, alpinePolicy)
+
+			podID := runPodSandbox(t, client, ctx, sandboxRequest)
+			defer removePodSandbox(t, client, ctx, podID)
+			defer stopPodSandbox(t, client, ctx, podID)
+
+			containerRequest := getCreateContainerRequest(
+				podID,
+				"alpine-with-policy",
+				"alpine:latest",
+				validPolicyAlpineCommand,
+				sandboxRequest.Config,
+			)
+			testConfig.sf(containerRequest)
+
+			containerID := createContainer(t, client, ctx, containerRequest)
+			_, err := client.StartContainer(ctx, &runtime.StartContainerRequest{
+				ContainerId: containerID,
+			})
+			if err == nil {
+				t.Fatal("expected container start failure")
+			}
+			if !strings.Contains(err.Error(), testConfig.expectedError) {
+				t.Fatalf("expected %q in error message, got: %q", testConfig.expectedError, err)
+			}
+		})
 	}
 }
