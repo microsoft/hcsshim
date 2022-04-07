6b920444509e2cd25ff44f5f918cf7fbc8ae438d
Maksim An <maksiman@microsoft.com>
2022-04-06 18:59:51 -0700

    Extend GrantVmGroupAccess to support write/execute/all permissions
    Add masks for GENERIC_WRITE, GENERIC_EXECUTE and GENERIC_ALL and update
    function signatures accordingly.
    Update grantvmgroupaccess tool to support granting permissions from above.
    
    Ignore various linter errors resurfaced after code copy-paste.
    Remove mksyscall_windows.go and update old unit tests and cleanup helper
    functions and packages used.
    
    Add unit test coverage for new functionality.
    
    Signed-off-by: Maksim An <maksiman@microsoft.com>

--- internal/security/grantvmgroupaccess.go
+++ internal/security/grantvmgroupaccess.go
@@ -1,3 +1,4 @@
+//go:build windows
 // +build windows
 
 package security
@@ -19,25 +20,37 @@ type (
 	securityInformation uint32
 	trusteeForm         uint32
 	trusteeType         uint32
-
-	explicitAccess struct {
-		accessPermissions accessMask
-		accessMode        accessMode
-		inheritance       inheritMode
-		trustee           trustee
-	}
-
-	trustee struct {
-		multipleTrustee          *trustee
-		multipleTrusteeOperation int32
-		trusteeForm              trusteeForm
-		trusteeType              trusteeType
-		name                     uintptr
-	}
 )
 
+type explicitAccess struct {
+	//nolint:structcheck
+	accessPermissions accessMask
+	//nolint:structcheck
+	accessMode accessMode
+	//nolint:structcheck
+	inheritance inheritMode
+	//nolint:structcheck
+	trustee trustee
+}
+
+type trustee struct {
+	//nolint:unused,structcheck
+	multipleTrustee *trustee
+	//nolint:unused,structcheck
+	multipleTrusteeOperation int32
+	trusteeForm              trusteeForm
+	trusteeType              trusteeType
+	name                     uintptr
+}
+
 const (
-	accessMaskDesiredPermission accessMask = 1 << 31 // GENERIC_READ
+	AccessMaskNone    accessMask = 0
+	AccessMaskRead    accessMask = 1 << 31 // GENERIC_READ
+	AccessMaskWrite   accessMask = 1 << 30 // GENERIC_WRITE
+	AccessMaskExecute accessMask = 1 << 29 // GENERIC_EXECUTE
+	AccessMaskAll     accessMask = 1 << 28 // GENERIC_ALL
+
+	accessMaskDesiredPermission = AccessMaskRead
 
 	accessModeGrant accessMode = 1
 
@@ -56,6 +69,7 @@ const (
 	shareModeRead  shareMode = 0x1
 	shareModeWrite shareMode = 0x2
 
+	//nolint:stylecheck // ST1003
 	sidVmGroup = "S-1-5-83-0"
 
 	trusteeFormIsSid trusteeForm = 0
@@ -63,11 +77,20 @@ const (
 	trusteeTypeWellKnownGroup trusteeType = 5
 )
 
-// GrantVMGroupAccess sets the DACL for a specified file or directory to
+// GrantVmGroupAccess sets the DACL for a specified file or directory to
 // include Grant ACE entries for the VM Group SID. This is a golang re-
 // implementation of the same function in vmcompute, just not exported in
 // RS5. Which kind of sucks. Sucks a lot :/
-func GrantVmGroupAccess(name string) error {
+func GrantVmGroupAccess(name string) error { //nolint:stylecheck // ST1003
+	return GrantVmGroupAccessWithMask(name, accessMaskDesiredPermission)
+}
+
+// GrantVmGroupAccessWithMask sets the desired DACL for a specified file or
+// directory.
+func GrantVmGroupAccessWithMask(name string, access accessMask) error { //nolint:stylecheck // ST1003
+	if access == 0 || access<<4 != 0 {
+		return fmt.Errorf("invalid access mask: 0x%08x", access)
+	}
 	// Stat (to determine if `name` is a directory).
 	s, err := os.Stat(name)
 	if err != nil {
@@ -79,7 +102,9 @@ func GrantVmGroupAccess(name string) error {
 	if err != nil {
 		return err // Already wrapped
 	}
-	defer syscall.CloseHandle(fd)
+	defer func() {
+		_ = syscall.CloseHandle(fd)
+	}()
 
 	// Get the current DACL and Security Descriptor. Must defer LocalFree on success.
 	ot := objectTypeFileObject
@@ -89,15 +114,19 @@ func GrantVmGroupAccess(name string) error {
 	if err := getSecurityInfo(fd, uint32(ot), uint32(si), nil, nil, &origDACL, nil, &sd); err != nil {
 		return fmt.Errorf("%s GetSecurityInfo %s: %w", gvmga, name, err)
 	}
-	defer syscall.LocalFree((syscall.Handle)(unsafe.Pointer(sd)))
+	defer func() {
+		_, _ = syscall.LocalFree((syscall.Handle)(unsafe.Pointer(sd)))
+	}()
 
 	// Generate a new DACL which is the current DACL with the required ACEs added.
 	// Must defer LocalFree on success.
-	newDACL, err := generateDACLWithAcesAdded(name, s.IsDir(), origDACL)
+	newDACL, err := generateDACLWithAcesAdded(name, s.IsDir(), access, origDACL)
 	if err != nil {
 		return err // Already wrapped
 	}
-	defer syscall.LocalFree((syscall.Handle)(unsafe.Pointer(newDACL)))
+	defer func() {
+		_, _ = syscall.LocalFree((syscall.Handle)(unsafe.Pointer(newDACL)))
+	}()
 
 	// And finally use SetSecurityInfo to apply the updated DACL.
 	if err := setSecurityInfo(fd, uint32(ot), uint32(si), uintptr(0), uintptr(0), newDACL, uintptr(0)); err != nil {
@@ -110,7 +139,10 @@ func GrantVmGroupAccess(name string) error {
 // createFile is a helper function to call [Nt]CreateFile to get a handle to
 // the file or directory.
 func createFile(name string, isDir bool) (syscall.Handle, error) {
-	namep := syscall.StringToUTF16(name)
+	namep, err := syscall.UTF16FromString(name)
+	if err != nil {
+		return 0, fmt.Errorf("syscall.UTF16FromString %s: %w", name, err)
+	}
 	da := uint32(desiredAccessReadControl | desiredAccessWriteDac)
 	sm := uint32(shareModeRead | shareModeWrite)
 	fa := uint32(syscall.FILE_ATTRIBUTE_NORMAL)
@@ -126,7 +158,7 @@ func createFile(name string, isDir bool) (syscall.Handle, error) {
 
 // generateDACLWithAcesAdded generates a new DACL with the two needed ACEs added.
 // The caller is responsible for LocalFree of the returned DACL on success.
-func generateDACLWithAcesAdded(name string, isDir bool, origDACL uintptr) (uintptr, error) {
+func generateDACLWithAcesAdded(name string, isDir bool, desiredAccess accessMask, origDACL uintptr) (uintptr, error) {
 	// Generate pointers to the SIDs based on the string SIDs
 	sid, err := syscall.StringToSid(sidVmGroup)
 	if err != nil {
@@ -139,8 +171,8 @@ func generateDACLWithAcesAdded(name string, isDir bool, origDACL uintptr) (uintp
 	}
 
 	eaArray := []explicitAccess{
-		explicitAccess{
-			accessPermissions: accessMaskDesiredPermission,
+		{
+			accessPermissions: desiredAccess,
 			accessMode:        accessModeGrant,
 			inheritance:       inheritance,
 			trustee: trustee{
--- internal/security/grantvmgroupaccess_test.go
+++ internal/security/grantvmgroupaccess_test.go
@@ -1,16 +1,15 @@
-//+build windows
+//go:build windows
+// +build windows
 
 package security
 
 import (
-	"io/ioutil"
+	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
-	"strings"
 	"testing"
-
-	exec "golang.org/x/sys/execabs"
 )
 
 const (
@@ -35,37 +34,38 @@ const (
 // S-1-15-3-1024-2268835264-3721307629-241982045-173645152-1490879176-104643441-2915960892-1612460704:(I)(R,W)
 // S-1-5-83-1-3166535780-1122986932-343720105-43916321:(I)(R,W)
 
-func TestGrantVmGroupAccess(t *testing.T) {
-	f, err := ioutil.TempFile("", "gvmgafile")
+func TestGrantVmGroupAccessDefault(t *testing.T) {
+	f1Path := filepath.Join(t.TempDir(), "gvmgafile")
+	f, err := os.Create(f1Path)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer func() {
-		f.Close()
-		os.Remove(f.Name())
+		_ = f.Close()
+		_ = os.Remove(f1Path)
 	}()
 
-	d, err := ioutil.TempDir("", "gvmgadir")
+	dir2 := t.TempDir()
+	f2Path := filepath.Join(dir2, "find.txt")
+	find, err := os.Create(f2Path)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.RemoveAll(d)
+	defer func() {
+		_ = find.Close()
+		_ = os.Remove(f2Path)
+	}()
 
-	find, err := os.Create(filepath.Join(d, "find.txt"))
-	if err != nil {
+	if err := GrantVmGroupAccess(f1Path); err != nil {
 		t.Fatal(err)
 	}
 
-	if err := GrantVmGroupAccess(f.Name()); err != nil {
-		t.Fatal(err)
-	}
-
-	if err := GrantVmGroupAccess(d); err != nil {
+	if err := GrantVmGroupAccess(dir2); err != nil {
 		t.Fatal(err)
 	}
 
 	verifyVMAccountDACLs(t,
-		f.Name(),
+		f1Path,
 		[]string{`(R)`},
 	)
 
@@ -80,15 +80,184 @@ func TestGrantVmGroupAccess(t *testing.T) {
 	// In properties for the directory, advanced security settings, this will
 	// show as a single line "Allow/Virtual Machines/Read/Inherited from none/This folder, subfolder and files
 	verifyVMAccountDACLs(t,
-		d,
+		dir2,
 		[]string{`(R)`, `(OI)(CI)(IO)(GR)`},
 	)
 
 	verifyVMAccountDACLs(t,
-		find.Name(),
+		f2Path,
 		[]string{`(I)(R)`},
 	)
 
+}
+
+func TestGrantVMGroupAccess_File_DesiredPermissions(t *testing.T) {
+	type config struct {
+		name                string
+		desiredAccess       accessMask
+		expectedPermissions []string
+	}
+
+	for _, cfg := range []config{
+		{
+			name:                "Read",
+			desiredAccess:       AccessMaskRead,
+			expectedPermissions: []string{`(R)`},
+		},
+		{
+			name:                "Write",
+			desiredAccess:       AccessMaskWrite,
+			expectedPermissions: []string{`(W,Rc)`},
+		},
+		{
+			name:                "Execute",
+			desiredAccess:       AccessMaskExecute,
+			expectedPermissions: []string{`(Rc,S,X,RA)`},
+		},
+		{
+			name:                "ReadWrite",
+			desiredAccess:       AccessMaskRead | AccessMaskWrite,
+			expectedPermissions: []string{`(R,W)`},
+		},
+		{
+			name:                "ReadExecute",
+			desiredAccess:       AccessMaskRead | AccessMaskExecute,
+			expectedPermissions: []string{`(RX)`},
+		},
+		{
+			name:                "WriteExecute",
+			desiredAccess:       AccessMaskWrite | AccessMaskExecute,
+			expectedPermissions: []string{`(W,Rc,X,RA)`},
+		},
+		{
+			name:                "ReadWriteExecute",
+			desiredAccess:       AccessMaskRead | AccessMaskWrite | AccessMaskExecute,
+			expectedPermissions: []string{`(RX,W)`},
+		},
+		{
+			name:                "All",
+			desiredAccess:       AccessMaskAll,
+			expectedPermissions: []string{`(F)`},
+		},
+	} {
+		t.Run(cfg.name, func(t *testing.T) {
+			dir := t.TempDir()
+			fd, err := os.Create(filepath.Join(dir, "test.txt"))
+			if err != nil {
+				t.Fatalf("failed to create temporary file: %s", err)
+			}
+			defer func() {
+				_ = fd.Close()
+				_ = os.Remove(fd.Name())
+			}()
+
+			if err := GrantVmGroupAccessWithMask(fd.Name(), cfg.desiredAccess); err != nil {
+				t.Fatal(err)
+			}
+			verifyVMAccountDACLs(t, fd.Name(), cfg.expectedPermissions)
+		})
+	}
+}
+
+func TestGrantVMGroupAccess_Directory_Permissions(t *testing.T) {
+	type config struct {
+		name            string
+		access          accessMask
+		filePermissions []string
+		dirPermissions  []string
+	}
+
+	for _, cfg := range []config{
+		{
+			name:            "Read",
+			access:          AccessMaskRead,
+			filePermissions: []string{`(I)(R)`},
+			dirPermissions:  []string{`(R)`, `(OI)(CI)(IO)(GR)`},
+		},
+		{
+			name:            "Write",
+			access:          AccessMaskWrite,
+			filePermissions: []string{`(I)(W,Rc)`},
+			dirPermissions:  []string{`(W,Rc)`, `(OI)(CI)(IO)(GW)`},
+		},
+		{
+			name:            "Execute",
+			access:          AccessMaskExecute,
+			filePermissions: []string{`(I)(Rc,S,X,RA)`},
+			dirPermissions:  []string{`(Rc,S,X,RA)`, `(OI)(CI)(IO)(GE)`},
+		},
+		{
+			name:            "ReadWrite",
+			access:          AccessMaskRead | AccessMaskWrite,
+			filePermissions: []string{`(I)(R,W)`},
+			dirPermissions:  []string{`(R,W)`, `(OI)(CI)(IO)(GR,GW)`},
+		},
+		{
+			name:            "ReadExecute",
+			access:          AccessMaskRead | AccessMaskExecute,
+			filePermissions: []string{`(I)(RX)`},
+			dirPermissions:  []string{`(RX)`, `(OI)(CI)(IO)(GR,GE)`},
+		},
+		{
+			name:            "WriteExecute",
+			access:          AccessMaskWrite | AccessMaskExecute,
+			filePermissions: []string{`(I)(W,Rc,X,RA)`},
+			dirPermissions:  []string{`(W,Rc,X,RA)`, `(OI)(CI)(IO)(GW,GE)`},
+		},
+		{
+			name:            "ReadWriteExecute",
+			access:          AccessMaskRead | AccessMaskWrite | AccessMaskExecute,
+			filePermissions: []string{`(I)(RX,W)`},
+			dirPermissions:  []string{`(RX,W)`, `(OI)(CI)(IO)(GR,GW,GE)`},
+		},
+		{
+			name:            "All",
+			access:          AccessMaskAll,
+			filePermissions: []string{`(I)(F)`},
+			dirPermissions:  []string{`(F)`, `(OI)(CI)(IO)(F)`},
+		}} {
+		t.Run(cfg.name, func(t *testing.T) {
+			dir := t.TempDir()
+			fd, err := os.Create(filepath.Join(dir, "test.txt"))
+			if err != nil {
+				t.Fatalf("failed to create temporary file: %s", err)
+			}
+			defer func() {
+				_ = fd.Close()
+				_ = os.Remove(fd.Name())
+			}()
+
+			if err := GrantVmGroupAccessWithMask(dir, cfg.access); err != nil {
+				t.Fatal(err)
+			}
+			verifyVMAccountDACLs(t, dir, cfg.dirPermissions)
+			verifyVMAccountDACLs(t, fd.Name(), cfg.filePermissions)
+		})
+	}
+}
+
+func TestGrantVmGroupAccess_Invalid_AccessMask(t *testing.T) {
+	for _, access := range []accessMask{
+		0,          // no bits set
+		1,          // invalid bit set
+		0x02000001, // invalid extra bit set
+	} {
+		t.Run(fmt.Sprintf("AccessMask_0x%x", access), func(t *testing.T) {
+			dir := t.TempDir()
+			fd, err := os.Create(filepath.Join(dir, "test.txt"))
+			if err != nil {
+				t.Fatalf("failed to create temporary file: %s", err)
+			}
+			defer func() {
+				_ = fd.Close()
+				_ = os.Remove(fd.Name())
+			}()
+
+			if err := GrantVmGroupAccessWithMask(fd.Name(), access); err == nil {
+				t.Fatalf("expected an error for mask: %x", access)
+			}
+		})
+	}
 }
 
 func verifyVMAccountDACLs(t *testing.T, name string, permissions []string) {
@@ -101,8 +270,7 @@ func verifyVMAccountDACLs(t *testing.T, name string, permissions []string) {
 
 	for _, p := range permissions {
 		// Avoid '(' and ')' being part of match groups
-		p = strings.Replace(p, "(", "\\(", -1)
-		p = strings.Replace(p, ")", "\\)", -1)
+		p = regexp.QuoteMeta(p)
 
 		nameToCheck := vmAccountName + ":" + p
 		sidToCheck := vmAccountSID + ":" + p
@@ -114,7 +282,7 @@ func verifyVMAccountDACLs(t *testing.T, name string, permissions []string) {
 		matchesSID := rxSID.FindAllStringIndex(out, -1)
 
 		if len(matchesName) != 1 && len(matchesSID) != 1 {
-			t.Fatalf("expected one match for %s or %s\n%s", nameToCheck, sidToCheck, out)
+			t.Fatalf("expected one match for %s or %s\n%s\n", nameToCheck, sidToCheck, out)
 		}
 	}
 }
