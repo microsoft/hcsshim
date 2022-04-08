70b87e3d4b7ca6dd33335a01f2b8241178d1f686
Maksim An <maksiman@microsoft.com>
2022-04-07 18:07:52 -0700

    Add tests for security policy enforcement (#1325)
    Add basic positive and negative tests for security policy enforcement.
    Hide policy tests behind LCOWIntegrity feature flag.
    Add ContainerConfigOpt and builder functions for creating security
    policy configs.
    
    Signed-off-by: Maksim An <maksiman@microsoft.com>

---
+++ pkg/securitypolicy/opts.go
@@ -0,0 +1,27 @@
+package securitypolicy
+
+type ContainerConfigOpt func(*ContainerConfig) error
+
+// WithEnvVarRules adds environment variable constraints to container policy config.
+func WithEnvVarRules(envs []EnvRuleConfig) ContainerConfigOpt {
+	return func(c *ContainerConfig) error {
+		c.EnvRules = append(c.EnvRules, envs...)
+		return nil
+	}
+}
+
+// WithExpectedMounts adds expected mounts to container policy config.
+func WithExpectedMounts(em []string) ContainerConfigOpt {
+	return func(c *ContainerConfig) error {
+		c.ExpectedMounts = append(c.ExpectedMounts, em...)
+		return nil
+	}
+}
+
+// WithWorkingDir sets working directory in container policy config.
+func WithWorkingDir(wd string) ContainerConfigOpt {
+	return func(c *ContainerConfig) error {
+		c.WorkingDir = wd
+		return nil
+	}
+}
