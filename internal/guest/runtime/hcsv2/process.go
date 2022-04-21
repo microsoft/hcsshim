2dfda310edfdd664093f9729851bd6e027f7d1d7
Hamza El-Saawy <hamzaelsaawy@microsoft.com>
2022-04-21 18:44:27 -0400

    spelling
    Signed-off-by: Hamza El-Saawy <hamzaelsaawy@microsoft.com>

--- internal/guest/runtime/hcsv2/process.go
+++ internal/guest/runtime/hcsv2/process.go
@@ -55,7 +55,7 @@ type containerProcess struct {
 	// (runtime.Process).Wait() call returns, and exitCode has been updated.
 	exitWg sync.WaitGroup
 
-	// Used to allow addtion/removal to the writersWg after an initial wait has
+	// Used to allow addition/removal to the writersWg after an initial wait has
 	// already been issued. It is not safe to call Add/Done without holding this
 	// lock.
 	writersSyncRoot sync.Mutex
