1e9fb98ec499edff65b4c9b7ba226e1a54cbd967
Hamza El-Saawy <hamzaelsaawy@microsoft.com>
2022-04-28 12:20:15 -0400

    Update hcs errors to gov1.13 style
    Add `.Is(` and `.Unwrap(` to `internal\hcs\errors.go` to using them with
    `errors.Is(`, as well as `internal\gcs\bridge.go`.
    
    Signed-off-by: Hamza El-Saawy <hamzaelsaawy@microsoft.com>

--- hcsshim.go
+++ hcsshim.go
@@ -6,7 +6,7 @@
 package hcsshim
 
 import (
-	"syscall"
+	"golang.org/x/sys/windows"
 
 	"github.com/Microsoft/hcsshim/internal/hcserror"
 )
@@ -19,9 +19,9 @@ const (
 	// Specific user-visible exit codes
 	WaitErrExecFailed = 32767
 
-	ERROR_GEN_FAILURE          = hcserror.ERROR_GEN_FAILURE
-	ERROR_SHUTDOWN_IN_PROGRESS = syscall.Errno(1115)
-	WSAEINVAL                  = syscall.Errno(10022)
+	ERROR_GEN_FAILURE          = windows.ERROR_GEN_FAILURE
+	ERROR_SHUTDOWN_IN_PROGRESS = windows.ERROR_SHUTDOWN_IN_PROGRESS
+	WSAEINVAL                  = windows.WSAEINVAL
 
 	// Timeout on wait calls
 	TimeoutInfinite = 0xFFFFFFFF
---
+++ internal/hcs/errors_test.go
@@ -0,0 +1,146 @@
+package hcs
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"testing"
+)
+
+type MyError struct {
+	S string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("error happened: %s", e.S)
+}
+
+func TestHcsErrorUnwrap(t *testing.T) {
+	err := &MyError{"test test"}
+	herr := HcsError{
+		Op:  t.Name(),
+		Err: err,
+	}
+
+	for _, nerr := range []net.Error{
+		&herr,
+		&SystemError{
+			ID:       t.Name(),
+			HcsError: herr,
+		},
+		&ProcessError{
+			SystemID: t.Name(),
+			HcsError: herr,
+		},
+	} {
+		t.Run(fmt.Sprintf("%T", nerr), func(t *testing.T) {
+			if !errors.Is(nerr, err) {
+				t.Errorf("error '%v' did not unwrap to %v", nerr, err)
+			}
+
+			if err2 := (&MyError{}); !(errors.As(nerr, &err2) && err2.S == err.S) {
+				t.Errorf("error '%v' did not unwrap '%v' properly", errors.Unwrap(nerr), err2)
+			}
+
+			if nerr.Timeout() {
+				t.Errorf("expected .Timeout() on '%v' to be false", nerr)
+			}
+
+			if nerr.Temporary() {
+				t.Errorf("expected .Temporary() on '%v' to be false", nerr)
+			}
+		})
+	}
+}
+
+func TestHcsErrorUnwrapTimeout(t *testing.T) {
+	err := fmt.Errorf("error: %w", ErrTimeout)
+	herr := HcsError{
+		Op:  "test",
+		Err: err,
+	}
+
+	for _, nerr := range []net.Error{
+		&herr,
+		&SystemError{
+			ID:       t.Name(),
+			HcsError: herr,
+		},
+		&ProcessError{
+			SystemID: t.Name(),
+			HcsError: herr,
+		},
+	} {
+		t.Run(fmt.Sprintf("%T", nerr), func(t *testing.T) {
+			if !errors.Is(nerr, ErrTimeout) {
+				t.Errorf("error '%v' did not unwrap to %v", nerr, ErrTimeout)
+			}
+
+			if !errors.Is(nerr, err) {
+				t.Errorf("error '%v' did not unwrap to %v", nerr, err)
+			}
+
+			if !IsTimeout(nerr) {
+				t.Errorf("expected error '%v' to be timeout", nerr)
+			}
+
+			if nerr.Timeout() {
+				t.Errorf("expected .Timeout() on '%v' to be false", nerr)
+			}
+
+			if nerr.Temporary() {
+				t.Errorf("expected .Temporary() on '%v' to be false", nerr)
+			}
+		})
+	}
+}
+
+var errNet = netError{}
+
+type netError struct{}
+
+func (e netError) Error() string   { return "temporary timeout" }
+func (e netError) Timeout() bool   { return true }
+func (e netError) Temporary() bool { return true }
+
+func TestHcsErrorUnwrapNet(t *testing.T) {
+	err := fmt.Errorf("error: %w", errNet)
+	herr := HcsError{
+		Op:  "test",
+		Err: err,
+	}
+
+	for _, nerr := range []net.Error{
+		&herr,
+		&SystemError{
+			ID:       t.Name(),
+			HcsError: herr,
+		},
+		&ProcessError{
+			SystemID: t.Name(),
+			HcsError: herr,
+		},
+	} {
+		t.Run(fmt.Sprintf("%T", nerr), func(t *testing.T) {
+			if !errors.Is(nerr, errNet) {
+				t.Errorf("error '%v' did not unwrap to %v", nerr, errNet)
+			}
+
+			if !errors.Is(nerr, err) {
+				t.Errorf("error '%v' did not unwrap to %v", nerr, err)
+			}
+
+			if !IsTimeout(nerr) {
+				t.Errorf("expected error '%v' to be timeout", nerr)
+			}
+
+			if !nerr.Timeout() {
+				t.Errorf("expected .Timeout() on '%v' to be true", nerr)
+			}
+
+			if !nerr.Temporary() {
+				t.Errorf("expected .Temporary() on '%v' to be true", nerr)
+			}
+		})
+	}
+}
