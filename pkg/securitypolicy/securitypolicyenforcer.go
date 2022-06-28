f6b45ccb15b9944b0e5e2bcc6c16247adad14739
Maksim An <maksiman@microsoft.com>
2022-06-27 18:17:05 -0700

    Update `StandardSecurityPolicyEnforcer` docs.
    Signed-off-by: Maksim An <maksiman@microsoft.com>

--- pkg/securitypolicy/securitypolicyenforcer.go
+++ pkg/securitypolicy/securitypolicyenforcer.go
@@ -119,77 +119,57 @@ type securityPolicyContainer struct {
 	AllowElevated bool
 }
 
+// StandardSecurityPolicyEnforcer implements SecurityPolicyEnforcer interface
+// and is responsible for enforcing various SecurityPolicy constraints.
+//
+// Most of the work that this security policy enforcer does it around managing
+// state needed to map from a container definition in the SecurityPolicy to
+// a specific container ID as we bring up each container. For example, see
+// EnforceCreateContainerPolicy where most of the functionality is handling the
+// case were policy containers share an overlay and have to try to distinguish
+// them based on the command line arguments, environment variables or working
+// directory.
+//
+// Containers that share the same base image, and perhaps further information,
+// will have an entry per container instance in the SecurityPolicy. For example,
+// a policy that has two containers that use Ubuntu 18.04 will have an entry for
+// each even if they share the same command line.
 type StandardSecurityPolicyEnforcer struct {
 	// encodedSecurityPolicy state is needed for key release
 	encodedSecurityPolicy string
-	// Containers from the user supplied security policy.
+	// Containers is the internal representation of users' container policies.
 	Containers []*securityPolicyContainer
-	// Devices and ContainerIndexToContainerIds are used to build up an
-	// understanding of the containers running with a UVM as they come up and
-	// map them back to a container definition from the user supplied
-	// SecurityPolicy
-	//
 	// Devices is a mapping between target and a corresponding root hash. Target
 	// is a path to a particular block device or its mount point inside UVM and
 	// root hash is the dm-verity root hash of that device. Mainly the stored
 	// devices represent read-only container layers, but this may change.
 	// As the UVM goes through its process of bringing up containers, we have to
 	// piece together information about what is going on.
-	//
-	// At the time that devices are being mounted, we do not know a container
-	// that they will be used for; only that there is a device with a given root
-	// hash that being mounted. We check to make sure that the root hash for the
-	// devices is a root hash that exists for 1 or more layers in any container
-	// in the supplied SecurityPolicy. Each "seen" layer is recorded in devices
-	// as it is mounted.
-	//
-	// Later, when overlay filesystems created, we look up the root hash chain
-	// for an incoming overlay and verify it against containers in the policy.
-	// Overlay filesystem creation is the first time we
-	// have a "container id" available to us. The container id identifies the
-	// container in question going forward. We record the mapping of Container
-	// index to container id so that when we have future operations like "run
-	// command" which come with a container id, we can find the corresponding
-	// container index and use that to look up the command in the appropriate
-	// SecurityPolicyContainer instance.
+	Devices map[string]string
+	// ContainerIndexToContainerIds is a mapping between containers in the
+	// SecurityPolicy and possible container IDs that have been created by runc,
+	// but have not yet been run.
 	//
 	// As containers can have exactly the same base image and be "the same" at
-	// the time we are doing overlay, the ContainerIndexToContainerIds in a
-	// set of possible containers for a given container id. Go doesn't have a set
-	// type so we are doing the idiomatic go thing of using a map[string]struct{}
+	// the time we are doing overlay, the ContainerIndexToContainerIds is a set
+	// of possible containers for a given container id. Go doesn't have a set
+	// type, so we are doing the idiomatic go thing of using a map[string]struct{}
 	// to represent the set.
-	//
-	// Containers that share the same base image, and perhaps further
-	// information, will have an entry per container instance in the
-	// SecurityPolicy. For example, a policy that has two containers that
-	// use Ubuntu 18.04 will have an entry for each even if they share the same
-	// command line.
-	//
-	// Most of the work that this security policy enforcer does it around managing
-	// state needed to map from a container definition in the SecurityPolicy to
-	// a specific container ID as we bring up each container. See
-	// enforceCommandPolicy where most of the functionality is handling the case
-	// were policy containers share an overlay and have to try to distinguish them
-	// based on the command line arguments. enforceEnvironmentVariablePolicy can
-	// further narrow based on environment variables if required.
-	//
-	// implementation details are available in:
-	// - EnforceDeviceMountPolicy
-	// - EnforceOverlayMountPolicy
-	// - enforceCommandPolicy
-	// - enforceEnvironmentVariablePolicy
-	// - NewStandardSecurityPolicyEnforcer
-	Devices                      map[string]string
 	ContainerIndexToContainerIds map[int]map[string]struct{}
-	// Set of container IDs that we've allowed to start. Because Go doesn't have
-	// sets as a built-in data structure, we are using a map
+	// startedContainers is a set of container IDs that were allowed to start.
+	// Because Go doesn't have sets as a built-in data structure, we are using a map.
 	startedContainers map[string]struct{}
-	// Mutex to prevent concurrent access to fields
+	// mutex guards against concurrent access to fields.
 	mutex *sync.Mutex
-	// DefaultMounts are mount constraints for container mounts added by CRI and GCS
+	// DefaultMounts are mount constraints for container mounts added by CRI and
+	// GCS. Since default mounts will be allowed for all containers in the UVM
+	// they are not added to each individual policy container and kept as global
+	// policy rules.
 	DefaultMounts []mountInternal
 	// DefaultEnvs are environment variable constraints for variables added
-	// by CRI and GCS
+	// by CRI and GCS. Since default envs will be allowed for all containers
+	// in the UVM they are not added to each individual policy container and
+	// kept as global policy rules.
 	DefaultEnvs []EnvRuleConfig
 }
 
@@ -375,6 +355,16 @@ func stringMapToStringArray(m map[string]string) ([]string, error) {
 	return out, nil
 }
 
+// EnforceDeviceMountPolicy for StandardSecurityPolicyEnforcer validates that
+// the target block device's root hash matches any container in SecurityPolicy.
+// Block device targets with invalid root hashes are rejected.
+//
+// At the time that devices are being mounted, we do not know a container
+// that they will be used for; only that there is a device with a given root
+// hash that being mounted. We check to make sure that the root hash for the
+// devices is a root hash that exists for 1 or more layers in any container
+// in the supplied SecurityPolicy. Each "seen" layer is recorded in devices
+// as it is mounted.
 func (pe *StandardSecurityPolicyEnforcer) EnforceDeviceMountPolicy(target string, deviceHash string) (err error) {
 	pe.mutex.Lock()
 	defer pe.mutex.Unlock()
@@ -399,6 +389,12 @@ func (pe *StandardSecurityPolicyEnforcer) EnforceDeviceMountPolicy(target string
 	return fmt.Errorf("roothash %s for mount %s doesn't match policy", deviceHash, target)
 }
 
+// EnforceDeviceUnmountPolicy for StandardSecurityPolicyEnforcer first validate
+// that the target mount was one of the allowed devices and then removes it from
+// the mapping.
+//
+// When proper protocol enforcement is in place, this will also make sure that
+// the device isn't currently used by any running container in an overlay.
 func (pe *StandardSecurityPolicyEnforcer) EnforceDeviceUnmountPolicy(unmountTarget string) (err error) {
 	pe.mutex.Lock()
 	defer pe.mutex.Unlock()
@@ -411,6 +407,19 @@ func (pe *StandardSecurityPolicyEnforcer) EnforceDeviceUnmountPolicy(unmountTarg
 	return nil
 }
 
+// EnforceOverlayMountPolicy for StandardSecurityPolicyEnforcer validates that
+// layerPaths represent a valid overlay file system and is allowed by the
+// SecurityPolicy.
+//
+// When overlay filesystems created, look up the root hash chain for an incoming
+// overlay and verify it against containers in the policy.
+// Overlay filesystem creation is the first time we have a "container ID"
+// available to us. The container id identifies the container in question going
+// forward. We record the mapping of container index in the policy to a set of
+// possible container IDs so that when we have future operations like
+// "run command" which come with a container ID, we can find the corresponding
+// container index and use that to look up the command in the appropriate
+// security policy container instance.
 func (pe *StandardSecurityPolicyEnforcer) EnforceOverlayMountPolicy(containerID string, layerPaths []string) (err error) {
 	pe.mutex.Lock()
 	defer pe.mutex.Unlock()
@@ -458,6 +467,15 @@ func (pe *StandardSecurityPolicyEnforcer) EnforceOverlayMountPolicy(containerID 
 	return nil
 }
 
+// EnforceCreateContainerPolicy for StandardSecurityPolicyEnforcer validates
+// the input container command, env and working directory against containers in
+// the SecurityPolicy. The enforcement also narrows down the containers that
+// have the same overlays by comparing their command, env and working directory
+// rules.
+//
+// Devices and ContainerIndexToContainerIds are used to build up an
+// understanding of the containers running with a UVM as they come up and map
+// them back to a container definition from the user supplied SecurityPolicy
 func (pe *StandardSecurityPolicyEnforcer) EnforceCreateContainerPolicy(
 	containerID string,
 	argList []string,
@@ -643,6 +661,8 @@ func (pe *StandardSecurityPolicyEnforcer) enforceDefaultMounts(specMount oci.Mou
 	return fmt.Errorf("mount not allowed by default mount constraints: %+v", specMount)
 }
 
+// ExtendDefaultMounts for StandardSecurityPolicyEnforcer adds default mounts
+// added by CRI and GCS to the list of DefaultMounts, which are always allowed.
 func (pe *StandardSecurityPolicyEnforcer) ExtendDefaultMounts(defaultMounts []oci.Mount) error {
 	pe.mutex.Lock()
 	defer pe.mutex.Unlock()
@@ -659,7 +679,9 @@ func (pe *StandardSecurityPolicyEnforcer) ExtendDefaultMounts(defaultMounts []oc
 }
 
 // EnforceMountPolicy for StandardSecurityPolicyEnforcer validates various
-// default mounts injected into container spec by GCS or containerD
+// default mounts injected into container spec by GCS or containerD. As part of
+// the enforcement, the method also narrows down possible container IDs with
+// the same overlay.
 func (pe *StandardSecurityPolicyEnforcer) EnforceMountPolicy(sandboxID, containerID string, spec *oci.Spec) (err error) {
 	pe.mutex.Lock()
 	defer pe.mutex.Unlock()
