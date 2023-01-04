KenGordon/hcsshim-old

Update resource limits via annotations
Allow resource limits to be updated via annotations.

Added function in `hcsoci` to translate annotations to resources,
similar to how its done on container and pod start.

Added test to check annotation is persisted across update (specifically
for CPU) and updated current tests to check that updating annotations
works similar to normal resource limits.

Signed-off-by: Hamza El-Saawy <hamzaelsaawy@microsoft.com>

test/cri-containerd/helper_container_test.go
@@ -5,10 +5,13 @@ package cri_containerd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
 
+	criserver "github.com/containerd/containerd/pkg/cri/server"
+	oci "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/stretchr/testify/require"
 	runtime "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 )
@@ -84,6 +87,25 @@ func assertContainerState(t *testing.T, client runtime.RuntimeServiceClient, ctx
 	if st := getContainerStatus(t, client, ctx, containerID); st != state {
 		t.Fatalf("got container %q state %q; wanted %v", containerID, st.String(), state.String())
 	}
+}
+
+func getContainerOCISpec(tb testing.TB, client runtime.RuntimeServiceClient, ctx context.Context, containerID string) *oci.Spec {
+	tb.Helper()
+	status, err := client.ContainerStatus(ctx, &runtime.ContainerStatusRequest{
+		ContainerId: containerID,
+		Verbose:     true,
+	})
+	if err != nil {
+		tb.Fatalf("failed PodSandboxStatus for sandbox: %s, request with: %v", containerID, err)
+	}
+
+	i := status.Info["info"]
+	var info criserver.ContainerInfo
+	if err := json.Unmarshal([]byte(i), &info); err != nil {
+		tb.Fatalf("could not unmarshal sandbox infospec %s: %v", i, err)
+	}
+
+	return info.RuntimeSpec
 }
 
 func getContainerStatusFull(t *testing.T, client runtime.RuntimeServiceClient, ctx context.Context, containerID string) *runtime.ContainerStatus {

microsoft/hcsshim

Added function, `NormalizeContainerUpdateResourcesRequest, in
`hcsoci` to translate annotations to resources, similar to how
its done on container and pod start.
Since it directly parses annotation values into an int (`uint64`),
`ParseAnnotationsUint64` had to be exported from `"oci"`.
Placing `NormalizeContainerUpdateResourcesRequest` in `"oci"` didn't
feel appropriate since it is similar to the resource functions already
in `"hcsoci"`.

test/cri-containerd/helper_container_test.go
@@ -5,10 +5,13 @@ package cri_containerd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
 
+	criserver "github.com/containerd/containerd/pkg/cri/server"
+	oci "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/stretchr/testify/require"
 	runtime "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 )
@@ -84,6 +87,29 @@ func assertContainerState(t *testing.T, client runtime.RuntimeServiceClient, ctx
 	if st := getContainerStatus(t, client, ctx, containerID); st != state {
 		t.Fatalf("got container %q state %q; wanted %v", containerID, st.String(), state.String())
 	}
+}
+
+func getContainerOCISpec(tb testing.TB, client runtime.RuntimeServiceClient, ctx context.Context, containerID string) *oci.Spec {
+	tb.Helper()
+	status, err := client.ContainerStatus(ctx, &runtime.ContainerStatusRequest{
+		ContainerId: containerID,
+		Verbose:     true,
+	})
+	if err != nil {
+		tb.Fatalf("failed ContainerStatus for container %q: %v", containerID, err)
+	}
+
+	i := status.Info["info"]
+	var info criserver.ContainerInfo
+	if err := json.Unmarshal([]byte(i), &info); err != nil {
+		tb.Fatalf("could not unmarshal container info %q: %v", i, err)
+	}
+
+	spec := info.RuntimeSpec
+	if spec == nil {
+		tb.Fatalf("container %q returned a nil spec", containerID)
+	}
+	return spec
 }
 
 func getContainerStatusFull(t *testing.T, client runtime.RuntimeServiceClient, ctx context.Context, containerID string) *runtime.ContainerStatus {