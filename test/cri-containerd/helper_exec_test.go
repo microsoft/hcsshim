69815bcbb5c01bdca37682c687e6eb85b869b439
Maksim An <maksiman@microsoft.com>
2023-03-13 23:02:43 -0700

    tests: rego exec in uvm cri integration tests (#1648)
    add another wrapper for shim diag.
    
    Signed-off-by: Maksim An <maksiman@microsoft.com>

--- test/cri-containerd/helper_exec_test.go
+++ test/cri-containerd/helper_exec_test.go
@@ -77,13 +77,12 @@ func execInHost(ctx context.Context, client shimdiag.ShimDiagService, args []str
 	return resp.ExitCode, nil
 }
 
-// shimDiagExecOutput is a small wrapper on top of execInHost, that returns the exec output
-func shimDiagExecOutput(ctx context.Context, tb testing.TB, podID string, cmd []string) string {
+func shimDiagExecOutputWithErr(ctx context.Context, tb testing.TB, podID string, cmd []string) (string, error) {
 	tb.Helper()
 	shimName := fmt.Sprintf("k8s.io-%s", podID)
 	shim, err := shimdiag.GetShim(shimName)
 	if err != nil {
-		tb.Fatalf("failed to find shim %v: %v", shimName, err)
+		return "", err
 	}
 	shimClient := shimdiag.NewShimDiagClient(shim)
 
@@ -94,13 +93,23 @@ func shimDiagExecOutput(ctx context.Context, tb testing.TB, podID string, cmd []
 
 	exitCode, err := execInHost(ctx, shimClient, cmd, nil, bw, bwErr)
 	if err != nil {
-		tb.Fatalf("failed to exec request in the host with: %v and %v", err, bufErr.String())
+		return "", fmt.Errorf("failed to exec request in the host with: %s and %s", err, bufErr.String())
+
 	}
 	if exitCode != 0 {
-		tb.Fatalf("exec request in host failed with exit code %v: %v", exitCode, bufErr.String())
+		return "", fmt.Errorf("exec request in host failed with exit code %v: %v", exitCode, bufErr.String())
 	}
+	return strings.TrimSpace(bufOut.String()), nil
+}
 
-	return strings.TrimSpace(bufOut.String())
+// shimDiagExecOutput is a small wrapper on top of execInHost, that returns the exec output
+func shimDiagExecOutput(ctx context.Context, tb testing.TB, podID string, cmd []string) string {
+	tb.Helper()
+	out, err := shimDiagExecOutputWithErr(ctx, tb, podID, cmd)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	return out
 }
 
 func filterStrings(input []string, include string) []string {
