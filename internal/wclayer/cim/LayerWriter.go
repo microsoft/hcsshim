5abf55c8119d77bbd6afd867ee8039ee3aadde18
Amit Barve <ambarve@microsoft.com>
2024-02-27 10:41:33 -0800

    Refactor layer writer interface
    Current layer writer interface forces us to calculate the CIM path from the layer path by
    making assumptions about CIM storage. This isn't a very good approach, better way is to be
    explicit about what information the layer writer needs from containerd. This change
    updates the CIM layer writer to take in layer CIM & parent CIM paths as inputs. This also
    means a corresponding changes needs to be made in containerd.
    
    Signed-off-by: Amit Barve <ambarve@microsoft.com>

--- internal/wclayer/cim/LayerWriter.go
+++ internal/wclayer/cim/LayerWriter.go
@@ -30,7 +30,7 @@ type CimLayerWriter struct {
 	// path to the layer (i.e layer's directory) as provided by the caller.
 	// Even if a layer is stored as a cim in the cim directory, some files associated
 	// with a layer are still stored in this path.
-	path string
+	layerPath string
 	// parent layer paths
 	parentLayerPaths []string
 	// Handle to the layer cim - writes to the cim file
@@ -92,7 +92,7 @@ func (cw *CimLayerWriter) Add(name string, fileInfo *winio.FileBasicInfo, fileSi
 		// create a pending op for this file
 		cw.pendingOps = append(cw.pendingOps, &addOp{
 			pathInCim:          name,
-			hostPath:           filepath.Join(cw.path, name),
+			hostPath:           filepath.Join(cw.layerPath, name),
 			fileInfo:           fileInfo,
 			securityDescriptor: securityDescriptor,
 			extendedAttributes: extendedAttributes,
@@ -177,7 +177,7 @@ func (cw *CimLayerWriter) Close(ctx context.Context) (retErr error) {
 	// Find out the osversion of this layer, both base & non-base layers can have UtilityVM layer.
 	processUtilityVM := false
 	if cw.hasUtilityVM {
-		uvmSoftwareHivePath := filepath.Join(cw.path, wclayer.UtilityVMPath, wclayer.RegFilesPath, "SOFTWARE")
+		uvmSoftwareHivePath := filepath.Join(cw.layerPath, wclayer.UtilityVMPath, wclayer.RegFilesPath, "SOFTWARE")
 		osvStr, err := getOsBuildNumberFromRegistry(uvmSoftwareHivePath)
 		if err != nil {
 			return fmt.Errorf("read os version string from UtilityVM SOFTWARE hive: %w", err)
@@ -189,7 +189,7 @@ func (cw *CimLayerWriter) Close(ctx context.Context) (retErr error) {
 		}
 
 		// write this version to a file for future reference by the shim process
-		if err = wclayer.WriteLayerUvmBuildFile(cw.path, uint16(osv)); err != nil {
+		if err = wclayer.WriteLayerUvmBuildFile(cw.layerPath, uint16(osv)); err != nil {
 			return fmt.Errorf("write uvm build version: %w", err)
 		}
 
@@ -218,7 +218,7 @@ func (cw *CimLayerWriter) Close(ctx context.Context) (retErr error) {
 	return nil
 }
 
-func NewCimLayerWriter(ctx context.Context, path string, parentLayerPaths []string) (_ *CimLayerWriter, err error) {
+func NewCimLayerWriter(ctx context.Context, layerPath, cimPath string, parentLayerPaths, parentLayerCimPaths []string) (_ *CimLayerWriter, err error) {
 	if !cimfs.IsCimFSSupported() {
 		return nil, fmt.Errorf("CimFs not supported on this build")
 	}
@@ -231,38 +231,34 @@ func NewCimLayerWriter(ctx context.Context, path string, parentLayerPaths []stri
 		}
 	}()
 	span.AddAttributes(
-		trace.StringAttribute("path", path),
+		trace.StringAttribute("path", layerPath),
+		trace.StringAttribute("cimPath", cimPath),
+		trace.StringAttribute("parentLayerPaths", strings.Join(parentLayerCimPaths, ", ")),
 		trace.StringAttribute("parentLayerPaths", strings.Join(parentLayerPaths, ", ")))
 
 	parentCim := ""
-	cimDirPath := GetCimDirFromLayer(path)
-	if _, err = os.Stat(cimDirPath); os.IsNotExist(err) {
-		// create cim directory
-		if err = os.Mkdir(cimDirPath, 0755); err != nil {
-			return nil, fmt.Errorf("failed while creating cim layers directory: %w", err)
-		}
-	} else if err != nil {
-		return nil, fmt.Errorf("unable to access cim layers directory: %w", err)
-
-	}
-
 	if len(parentLayerPaths) > 0 {
-		parentCim = GetCimNameFromLayer(parentLayerPaths[0])
+		if filepath.Dir(cimPath) != filepath.Dir(parentLayerCimPaths[0]) {
+			return nil, fmt.Errorf("parent cim can not be stored in different directory")
+		}
+		// We only need to provide parent CIM name, it is assumed that both parent CIM
+		// and newly created CIM are present in the same directory.
+		parentCim = filepath.Base(parentLayerCimPaths[0])
 	}
 
-	cim, err := cimfs.Create(cimDirPath, parentCim, GetCimNameFromLayer(path))
+	cim, err := cimfs.Create(filepath.Dir(cimPath), parentCim, filepath.Base(cimPath))
 	if err != nil {
 		return nil, fmt.Errorf("error in creating a new cim: %w", err)
 	}
 
-	sfw, err := newStdFileWriter(path, parentLayerPaths)
+	sfw, err := newStdFileWriter(layerPath, parentLayerPaths)
 	if err != nil {
 		return nil, fmt.Errorf("error in creating new standard file writer: %w", err)
 	}
 	return &CimLayerWriter{
 		ctx:              ctx,
 		s:                span,
-		path:             path,
+		layerPath:        layerPath,
 		parentLayerPaths: parentLayerPaths,
 		cimWriter:        cim,
 		stdFileWriter:    sfw,
