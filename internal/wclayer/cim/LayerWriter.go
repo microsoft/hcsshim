e0c52cd23b414706ac630ce3d4c9651c3e74c146
Amit Barve <ambarve@microsoft.com>
2024-03-01 01:59:33 -0800

    Remove unused CimFS related code
    CimFS is currently not supported with HyperV isolation. However, we still have code that
    handles processing of UtilityVM layer during image import. After the layer refactoring
    change we need to update this code as well. But since this code isn't being used anywhere
    updating it doesn't make much sense. There are no tests for this either. This code is
    removed for now and we can add it back later once the plan for running HyperV isolated
    containers with CimFS is more solid.
    
    Signed-off-by: Amit Barve <ambarve@microsoft.com>

--- internal/wclayer/cim/LayerWriter.go
+++ internal/wclayer/cim/LayerWriter.go
@@ -6,16 +6,12 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 
 	"github.com/Microsoft/go-winio"
-	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/oc"
 	"github.com/Microsoft/hcsshim/internal/wclayer"
-	"github.com/Microsoft/hcsshim/osversion"
 	"github.com/Microsoft/hcsshim/pkg/cimfs"
 	"go.opencensus.io/trace"
 )
@@ -174,31 +170,8 @@ func (cw *CimLayerWriter) Close(ctx context.Context) (retErr error) {
 		}
 	}()
 
-	// Find out the osversion of this layer, both base & non-base layers can have UtilityVM layer.
+	// UVM based containers aren't supported with CimFS, don't process the UVM layer
 	processUtilityVM := false
-	if cw.hasUtilityVM {
-		uvmSoftwareHivePath := filepath.Join(cw.layerPath, wclayer.UtilityVMPath, wclayer.RegFilesPath, "SOFTWARE")
-		osvStr, err := getOsBuildNumberFromRegistry(uvmSoftwareHivePath)
-		if err != nil {
-			return fmt.Errorf("read os version string from UtilityVM SOFTWARE hive: %w", err)
-		}
-
-		osv, err := strconv.ParseUint(osvStr, 10, 16)
-		if err != nil {
-			return fmt.Errorf("parse os version string (%s): %w", osvStr, err)
-		}
-
-		// write this version to a file for future reference by the shim process
-		if err = wclayer.WriteLayerUvmBuildFile(cw.layerPath, uint16(osv)); err != nil {
-			return fmt.Errorf("write uvm build version: %w", err)
-		}
-
-		// CIMFS for hyperV isolated is only supported after 20348, processing UtilityVM layer on 2048
-		// & lower will cause failures since those images won't have CIMFS specific UVM files (mostly
-		// BCD entries required for CIMFS)
-		processUtilityVM = (osv > osversion.LTSC2022)
-		log.G(ctx).Debugf("import image os version %d, processing UtilityVM layer: %t\n", osv, processUtilityVM)
-	}
 
 	if len(cw.parentLayerPaths) == 0 {
 		if err := cw.processBaseLayer(ctx, processUtilityVM); err != nil {
@@ -263,23 +236,4 @@ func NewCimLayerWriter(ctx context.Context, layerPath, cimPath string, parentLay
 		cimWriter:        cim,
 		stdFileWriter:    sfw,
 	}, nil
-}
-
-// DestroyCimLayer destroys a cim layer i.e it removes all the cimfs files for the given layer as well as
-// all of the other files that are stored in the layer directory (at path `layerPath`).
-// If this is not a cimfs layer (i.e a cim file for the given layer does not exist) then nothing is done.
-func DestroyCimLayer(ctx context.Context, layerPath string) error {
-	cimPath := GetCimPathFromLayer(layerPath)
-
-	// verify that such a cim exists first, sometimes containerd tries to call
-	// this with the root snapshot directory as the layer path. We don't want to
-	// destroy everything inside the snapshots directory.
-	if _, err := os.Stat(cimPath); err != nil {
-		if os.IsNotExist(err) {
-			return nil
-		}
-		return err
-	}
-
-	return cimfs.DestroyCim(ctx, cimPath)
 }
