KenGordon/hcsshim-old

Refactor layer mount functions
This commit uses the newly added WCOW layer parsers and the new type for representing
mounted WCOW layers. LCOW functions are also moved around (and renamed) to follow similar
style as that of WCOW functions.

Signed-off-by: Amit Barve <ambarve@microsoft.com>

cmd/containerd-shim-runhcs-v1/pod.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/oci"
 	"github.com/Microsoft/hcsshim/internal/uvm"
@@ -122,22 +123,15 @@ func createPod(ctx context.Context, events publisher, req *task.CreateTaskReques
 				return nil, err
 			}
 		case *uvm.OptionsWCOW:
+			var layerFolders []string
+			if s.Windows != nil {
+				layerFolders = s.Windows.LayerFolders
+			}
 			wopts := (opts).(*uvm.OptionsWCOW)
-
-			// In order for the UVM sandbox.vhdx not to collide with the actual
-			// nested Argon sandbox.vhdx we append the \vm folder to the last
-			// entry in the list.
-			layersLen := len(s.Windows.LayerFolders)
-			layers := make([]string, layersLen)
-			copy(layers, s.Windows.LayerFolders)
-
-			vmPath := filepath.Join(layers[layersLen-1], "vm")
-			err := os.MkdirAll(vmPath, 0)
+			wopts.BootFiles, err = layers.ParseWCOWUVMBootFilesFromLayers(ctx, req.Rootfs, layerFolders)
 			if err != nil {
 				return nil, err
 			}
-			layers[layersLen-1] = vmPath
-			wopts.LayerFolders = layers
 
 			parent, err = uvm.CreateWCOW(ctx, wopts)
 			if err != nil {

cmd/runhcs/container.go
@@ -18,6 +18,7 @@ import (
 	"github.com/Microsoft/hcsshim/internal/cni"
 	"github.com/Microsoft/hcsshim/internal/hcs"
 	"github.com/Microsoft/hcsshim/internal/hcsoci"
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/logfields"
 	"github.com/Microsoft/hcsshim/internal/oci"
 	"github.com/Microsoft/hcsshim/internal/regstate"
@@ -402,21 +403,10 @@ func createContainer(cfg *containerConfig) (_ *container, err error) {
 		case *uvm.OptionsLCOW:
 			opts.ConsolePipe = cfg.VMConsolePipe
 		case *uvm.OptionsWCOW:
-			// In order for the UVM sandbox.vhdx not to collide with the actual
-			// nested Argon sandbox.vhdx we append the \vm folder to the last entry
-			// in the list.
-			layersLen := len(cfg.Spec.Windows.LayerFolders)
-			layers := make([]string, layersLen)
-			copy(layers, cfg.Spec.Windows.LayerFolders)
-
-			vmPath := filepath.Join(layers[layersLen-1], "vm")
-			err := os.MkdirAll(vmPath, 0)
+			opts.BootFiles, err = layers.ParseWCOWUVMBootFilesFromLayers(context.Background(), nil, cfg.Spec.Windows.LayerFolders)
 			if err != nil {
 				return nil, err
 			}
-			layers[layersLen-1] = vmPath
-
-			opts.LayerFolders = layers
 		}
 
 		shim, err := c.startVMShim(cfg.VMLogFile, opts)

internal/tools/uvmboot/wcow.go
@@ -15,6 +15,7 @@ import (
 	"github.com/urfave/cli"
 
 	"github.com/Microsoft/hcsshim/internal/cmd"
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/uvm"
 )
@@ -55,19 +56,19 @@ var wcowCommand = cli.Command{
 		runMany(c, func(id string) error {
 			options := uvm.NewDefaultOptionsWCOW(id, "")
 			setGlobalOptions(c, options.Options)
-			var layers []string
+			var layerFolders []string
 			if wcowImage != "" {
 				layer, err := filepath.Abs(wcowImage)
 				if err != nil {
 					return err
 				}
-				layers = []string{layer}
+				layerFolders = []string{layer}
 			} else {
 				if wcowDockerImage == "" {
 					wcowDockerImage = "mcr.microsoft.com/windows/nanoserver:1809"
 				}
 				var err error
-				layers, err = getLayers(wcowDockerImage)
+				layerFolders, err = getLayers(wcowDockerImage)
 				if err != nil {
 					return err
 				}
@@ -77,7 +78,11 @@ var wcowCommand = cli.Command{
 				return err
 			}
 			defer os.RemoveAll(tempDir)
-			options.LayerFolders = append(layers, tempDir)
+			layerFolders = append(layerFolders, tempDir)
+			options.BootFiles, err = layers.ParseWCOWUVMBootFilesFromLayers(context.TODO(), nil, layerFolders)
+			if err != nil {
+				return err
+			}
 			vm, err := uvm.CreateWCOW(context.TODO(), options)
 			if err != nil {
 				return err

test/functional/main_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/urfave/cli/v2"
 	"go.opencensus.io/trace"
 
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/oc"
 	"github.com/Microsoft/hcsshim/internal/sync"
@@ -275,9 +276,11 @@ func defaultWCOWOptions(ctx context.Context, tb testing.TB) *uvm.OptionsWCOW {
 	opts := testuvm.DefaultWCOWOptions(ctx, tb, testName(tb), hcsOwner)
 	uvmLayers := windowsImageLayers(ctx, tb)
 	scratchDir := testlayers.WCOWScratchDir(ctx, tb, "")
-	opts.LayerFolders = append(opts.LayerFolders, uvmLayers...)
-	opts.LayerFolders = append(opts.LayerFolders, scratchDir)
-
+	bootFiles, err := layers.ParseWCOWUVMBootFilesFromLayers(ctx, nil, append(uvmLayers, scratchDir))
+	if err != nil {
+		tb.Fatalf("failed to parse WCOW Boot files: %s", err)
+	}
+	opts.BootFiles = bootFiles
 	return opts
 }
 

test/functional/wcow_test.go
@@ -380,11 +380,16 @@ func TestWCOWArgonShim(t *testing.T) {
 	layers := generateShimLayersStruct(t, imageLayers)
 
 	id := "argon"
-	// This is a cheat but stops us re-writing exactly the same code just for test
-	argonShimLocalMountPath, closer, err := layerspkg.MountWCOWLayers(context.Background(), id, append(imageLayers, argonShimScratchDir), "", nil)
+	wcowLayers, err := layerspkg.ParseWCOWLayers(nil, append(imageLayers, argonShimScratchDir))
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	mountedLayers, closer, err := layerspkg.MountWCOWLayers(context.Background(), id, nil, wcowLayers)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	defer func() {
 		if closer != nil {
 			_ = closer.Release(context.Background())
@@ -393,7 +398,7 @@ func TestWCOWArgonShim(t *testing.T) {
 	argonShim, err := hcsshim.CreateContainer(id, &hcsshim.ContainerConfig{
 		SystemType:      "Container",
 		Name:            "argonShim",
-		VolumePath:      argonShimLocalMountPath,
+		VolumePath:      mountedLayers.RootFS,
 		LayerFolderPath: argonShimScratchDir,
 		Layers:          layers,
 		MappedDirectories: []schema1.MappedDir{
@@ -691,7 +696,10 @@ func TestWCOWXenonOciV2(t *testing.T) {
 	}
 
 	xenonOciOpts := uvm.NewDefaultOptionsWCOW(xenonOci2UVMId, "")
-	xenonOciOpts.LayerFolders = append(imageLayers, xenonOci2UVMScratchDir)
+	xenonOciOpts.BootFiles, err = layerspkg.ParseWCOWUVMBootFilesFromLayers(context.Background(), nil, append(imageLayers, xenonOci2UVMScratchDir))
+	if err != nil {
+		t.Fatalf("Failed to parse UVM boot files: %s", err)
+	}
 	xenonOci2UVM, err = uvm.CreateWCOW(context.Background(), xenonOciOpts)
 	if err != nil {
 		t.Fatalf("Failed create UVM: %s", err)

test/pkg/uvm/wcow.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	hcsschema "github.com/Microsoft/hcsshim/internal/hcs/schema2"
+	shimlayers "github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/uvm"
 
 	"github.com/Microsoft/hcsshim/test/internal/layers"
@@ -36,8 +37,8 @@ func CreateWCOWUVM(ctx context.Context, tb testing.TB, id, image string) (*uvm.U
 func CreateWCOW(ctx context.Context, tb testing.TB, opts *uvm.OptionsWCOW) (*uvm.UtilityVM, CleanupFn) {
 	tb.Helper()
 
-	if opts == nil || len(opts.LayerFolders) < 2 {
-		tb.Fatalf("opts must bet set with LayerFolders")
+	if opts == nil || opts.BootFiles == nil {
+		tb.Fatalf("opts must bet set with BootFiles")
 	}
 
 	vm, err := uvm.CreateWCOW(ctx, opts)
@@ -75,8 +76,11 @@ func CreateWCOWUVMFromOptsWithImage(
 
 	uvmLayers := img.Layers(ctx, tb)
 	scratchDir := tb.TempDir()
-	opts.LayerFolders = append(opts.LayerFolders, uvmLayers...)
-	opts.LayerFolders = append(opts.LayerFolders, scratchDir)
+	bootFiles, err := shimlayers.ParseWCOWUVMBootFilesFromLayers(ctx, nil, append(uvmLayers, scratchDir))
+	if err != nil {
+		tb.Fatalf("failed to parse UVM boot layers: %s", err)
+	}
+	opts.BootFiles = bootFiles
 
 	vm, cleanup := CreateWCOW(ctx, tb, opts)
 	tb.Cleanup(func() { cleanup(ctx) })

microsoft/hcsshim

cmd/containerd-shim-runhcs-v1/pod.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/oci"
 	"github.com/Microsoft/hcsshim/internal/uvm"
@@ -122,22 +123,15 @@ func createPod(ctx context.Context, events publisher, req *task.CreateTaskReques
 				return nil, err
 			}
 		case *uvm.OptionsWCOW:
+			var layerFolders []string
+			if s.Windows != nil {
+				layerFolders = s.Windows.LayerFolders
+			}
 			wopts := (opts).(*uvm.OptionsWCOW)
-
-			// In order for the UVM sandbox.vhdx not to collide with the actual
-			// nested Argon sandbox.vhdx we append the \vm folder to the last
-			// entry in the list.
-			layersLen := len(s.Windows.LayerFolders)
-			layers := make([]string, layersLen)
-			copy(layers, s.Windows.LayerFolders)
-
-			vmPath := filepath.Join(layers[layersLen-1], "vm")
-			err := os.MkdirAll(vmPath, 0)
+			wopts.BootFiles, err = layers.GetWCOWUVMBootFilesFromLayers(ctx, req.Rootfs, layerFolders)
 			if err != nil {
 				return nil, err
 			}
-			layers[layersLen-1] = vmPath
-			wopts.LayerFolders = layers
 
 			parent, err = uvm.CreateWCOW(ctx, wopts)
 			if err != nil {

cmd/runhcs/container.go
@@ -18,6 +18,7 @@ import (
 	"github.com/Microsoft/hcsshim/internal/cni"
 	"github.com/Microsoft/hcsshim/internal/hcs"
 	"github.com/Microsoft/hcsshim/internal/hcsoci"
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/logfields"
 	"github.com/Microsoft/hcsshim/internal/oci"
 	"github.com/Microsoft/hcsshim/internal/regstate"
@@ -402,21 +403,10 @@ func createContainer(cfg *containerConfig) (_ *container, err error) {
 		case *uvm.OptionsLCOW:
 			opts.ConsolePipe = cfg.VMConsolePipe
 		case *uvm.OptionsWCOW:
-			// In order for the UVM sandbox.vhdx not to collide with the actual
-			// nested Argon sandbox.vhdx we append the \vm folder to the last entry
-			// in the list.
-			layersLen := len(cfg.Spec.Windows.LayerFolders)
-			layers := make([]string, layersLen)
-			copy(layers, cfg.Spec.Windows.LayerFolders)
-
-			vmPath := filepath.Join(layers[layersLen-1], "vm")
-			err := os.MkdirAll(vmPath, 0)
+			opts.BootFiles, err = layers.GetWCOWUVMBootFilesFromLayers(context.Background(), nil, cfg.Spec.Windows.LayerFolders)
 			if err != nil {
 				return nil, err
 			}
-			layers[layersLen-1] = vmPath
-
-			opts.LayerFolders = layers
 		}
 
 		shim, err := c.startVMShim(cfg.VMLogFile, opts)

internal/tools/uvmboot/wcow.go
@@ -15,6 +15,7 @@ import (
 	"github.com/urfave/cli"
 
 	"github.com/Microsoft/hcsshim/internal/cmd"
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/uvm"
 )
@@ -55,19 +56,19 @@ var wcowCommand = cli.Command{
 		runMany(c, func(id string) error {
 			options := uvm.NewDefaultOptionsWCOW(id, "")
 			setGlobalOptions(c, options.Options)
-			var layers []string
+			var layerFolders []string
 			if wcowImage != "" {
 				layer, err := filepath.Abs(wcowImage)
 				if err != nil {
 					return err
 				}
-				layers = []string{layer}
+				layerFolders = []string{layer}
 			} else {
 				if wcowDockerImage == "" {
 					wcowDockerImage = "mcr.microsoft.com/windows/nanoserver:1809"
 				}
 				var err error
-				layers, err = getLayers(wcowDockerImage)
+				layerFolders, err = getLayers(wcowDockerImage)
 				if err != nil {
 					return err
 				}
@@ -77,7 +78,11 @@ var wcowCommand = cli.Command{
 				return err
 			}
 			defer os.RemoveAll(tempDir)
-			options.LayerFolders = append(layers, tempDir)
+			layerFolders = append(layerFolders, tempDir)
+			options.BootFiles, err = layers.GetWCOWUVMBootFilesFromLayers(context.TODO(), nil, layerFolders)
+			if err != nil {
+				return err
+			}
 			vm, err := uvm.CreateWCOW(context.TODO(), options)
 			if err != nil {
 				return err

test/functional/main_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/urfave/cli/v2"
 	"go.opencensus.io/trace"
 
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/oc"
 	"github.com/Microsoft/hcsshim/internal/sync"
@@ -275,9 +276,11 @@ func defaultWCOWOptions(ctx context.Context, tb testing.TB) *uvm.OptionsWCOW {
 	opts := testuvm.DefaultWCOWOptions(ctx, tb, testName(tb), hcsOwner)
 	uvmLayers := windowsImageLayers(ctx, tb)
 	scratchDir := testlayers.WCOWScratchDir(ctx, tb, "")
-	opts.LayerFolders = append(opts.LayerFolders, uvmLayers...)
-	opts.LayerFolders = append(opts.LayerFolders, scratchDir)
-
+	bootFiles, err := layers.GetWCOWUVMBootFilesFromLayers(ctx, nil, append(uvmLayers, scratchDir))
+	if err != nil {
+		tb.Fatalf("failed to parse WCOW Boot files: %s", err)
+	}
+	opts.BootFiles = bootFiles
 	return opts
 }
 

test/functional/wcow_test.go
@@ -380,11 +380,16 @@ func TestWCOWArgonShim(t *testing.T) {
 	layers := generateShimLayersStruct(t, imageLayers)
 
 	id := "argon"
-	// This is a cheat but stops us re-writing exactly the same code just for test
-	argonShimLocalMountPath, closer, err := layerspkg.MountWCOWLayers(context.Background(), id, append(imageLayers, argonShimScratchDir), "", nil)
+	wcowLayers, err := layerspkg.ParseWCOWLayers(nil, append(imageLayers, argonShimScratchDir))
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	mountedLayers, closer, err := layerspkg.MountWCOWLayers(context.Background(), id, nil, wcowLayers)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	defer func() {
 		if closer != nil {
 			_ = closer.Release(context.Background())
@@ -393,7 +398,7 @@ func TestWCOWArgonShim(t *testing.T) {
 	argonShim, err := hcsshim.CreateContainer(id, &hcsshim.ContainerConfig{
 		SystemType:      "Container",
 		Name:            "argonShim",
-		VolumePath:      argonShimLocalMountPath,
+		VolumePath:      mountedLayers.RootFS,
 		LayerFolderPath: argonShimScratchDir,
 		Layers:          layers,
 		MappedDirectories: []schema1.MappedDir{
@@ -691,7 +696,10 @@ func TestWCOWXenonOciV2(t *testing.T) {
 	}
 
 	xenonOciOpts := uvm.NewDefaultOptionsWCOW(xenonOci2UVMId, "")
-	xenonOciOpts.LayerFolders = append(imageLayers, xenonOci2UVMScratchDir)
+	xenonOciOpts.BootFiles, err = layerspkg.GetWCOWUVMBootFilesFromLayers(context.Background(), nil, append(imageLayers, xenonOci2UVMScratchDir))
+	if err != nil {
+		t.Fatalf("Failed to parse UVM boot files: %s", err)
+	}
 	xenonOci2UVM, err = uvm.CreateWCOW(context.Background(), xenonOciOpts)
 	if err != nil {
 		t.Fatalf("Failed create UVM: %s", err)

test/pkg/uvm/wcow.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	hcsschema "github.com/Microsoft/hcsshim/internal/hcs/schema2"
+	shimlayers "github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/uvm"
 
 	"github.com/Microsoft/hcsshim/test/internal/layers"
@@ -36,8 +37,8 @@ func CreateWCOWUVM(ctx context.Context, tb testing.TB, id, image string) (*uvm.U
 func CreateWCOW(ctx context.Context, tb testing.TB, opts *uvm.OptionsWCOW) (*uvm.UtilityVM, CleanupFn) {
 	tb.Helper()
 
-	if opts == nil || len(opts.LayerFolders) < 2 {
-		tb.Fatalf("opts must bet set with LayerFolders")
+	if opts == nil || opts.BootFiles == nil {
+		tb.Fatalf("opts must bet set with BootFiles")
 	}
 
 	vm, err := uvm.CreateWCOW(ctx, opts)
@@ -75,8 +76,11 @@ func CreateWCOWUVMFromOptsWithImage(
 
 	uvmLayers := img.Layers(ctx, tb)
 	scratchDir := tb.TempDir()
-	opts.LayerFolders = append(opts.LayerFolders, uvmLayers...)
-	opts.LayerFolders = append(opts.LayerFolders, scratchDir)
+	bootFiles, err := shimlayers.GetWCOWUVMBootFilesFromLayers(ctx, nil, append(uvmLayers, scratchDir))
+	if err != nil {
+		tb.Fatalf("failed to parse UVM boot layers: %s", err)
+	}
+	opts.BootFiles = bootFiles
 
 	vm, cleanup := CreateWCOW(ctx, tb, opts)
 	tb.Cleanup(func() { cleanup(ctx) })