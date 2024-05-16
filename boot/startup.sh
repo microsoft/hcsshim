8e5438a31c954829c7c318732966cf0368907e4a
Maksim An <maksiman@microsoft.com>
2024-05-16 12:02:38 -0700

    always scrub logs in SNP mode (#2143)
    Signed-off-by: Maksim An <maksiman@microsoft.com>

--- boot/startup.sh
+++ boot/startup.sh
@@ -3,4 +3,4 @@
 export PATH="/usr/bin:/usr/local/bin:/bin:/root/bin:/sbin:/usr/sbin:/usr/local/sbin"
 export HOME="/root"
 
-/init -e 1 /bin/vsockexec -o 109 -e 109 /bin/gcs -v4 -log-format json -loglevel debug
+/init -e 1 /bin/vsockexec -o 109 -e 109 /bin/gcs -v4 -log-format json -loglevel debug -scrub-logs
