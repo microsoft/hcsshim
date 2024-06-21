200feabd854da69f615a598ed6a1263ce9531676
Prince Pereira <ppereira@microsoft.com>
2024-06-21 14:41:53 +0530

    Hcsshim wrapper over HNS API needed for exclusion of management mac addresses for VF reassignment.
    Signed-off-by: Prince Pereira <ppereira@microsoft.com>

--- hcn/hnsv1_test.go
+++ hcn/hnsv1_test.go
@@ -129,3 +129,53 @@ func TestNetwork(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestAccelnetNnvManagementMacAddresses(t *testing.T) {
+	network, err := CreateTestNetwork()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	macList := []string{"00-15-5D-0A-B7-C6", "00-15-5D-38-01-00"}
+	newMacList, err := hcsshim.SetNnvManagementMacAddresses(macList)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(newMacList.MacAddressList) != 2 {
+		t.Errorf("After Create: Expected macaddress count %d, got %d", 2, len(newMacList.MacAddressList))
+	}
+
+	newMacList, err = hcsshim.GetNnvManagementMacAddresses()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(newMacList.MacAddressList) != 2 {
+		t.Errorf("Get After Create: Expected macaddress count %d, got %d", 2, len(newMacList.MacAddressList))
+	}
+
+	newMacList, err = hcsshim.DeleteNnvManagementMacAddresses()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(newMacList.MacAddressList) != 0 {
+		t.Errorf("After Delete: Expected macaddress count %d, got %d", 0, len(newMacList.MacAddressList))
+	}
+
+	newMacList, err = hcsshim.GetNnvManagementMacAddresses()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(newMacList.MacAddressList) != 0 {
+		t.Errorf("Get After Delete: Expected macaddress count %d, got %d", 0, len(newMacList.MacAddressList))
+	}
+
+	_, err = network.Delete()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
