b0b5a0c6843178b3ee8c9655621fcf05c4087d4e
Kathryn Baldauf <kabaldau@microsoft.com>
2024-11-25 10:55:25 -0800

    Add build version block for pod CPU limits updating (#2321)
    Signed-off-by: Kathryn Baldauf <kabaldau@microsoft.com>

--- internal/uvm/cpulimits_update.go
+++ internal/uvm/cpulimits_update.go
@@ -7,10 +7,16 @@ import (
 
 	"github.com/Microsoft/hcsshim/internal/hcs/resourcepaths"
 	hcsschema "github.com/Microsoft/hcsshim/internal/hcs/schema2"
+	"github.com/Microsoft/hcsshim/osversion"
+	"github.com/containerd/errdefs"
 )
 
 // UpdateCPULimits updates the CPU limits of the utility vm
 func (uvm *UtilityVM) UpdateCPULimits(ctx context.Context, limits *hcsschema.ProcessorLimits) error {
+	// Support for updating CPU limits was not added until 20H2 build
+	if osversion.Get().Build < osversion.V20H2 {
+		return errdefs.ErrNotImplemented
+	}
 	req := &hcsschema.ModifySettingRequest{
 		ResourcePath: resourcepaths.CPULimitsResourcePath,
 		Settings:     limits,
