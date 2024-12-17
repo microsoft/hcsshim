dd7420482d99f61884eb9d550594599b980b2c91
Amit Barve <ambarve@microsoft.com>
2024-12-17 10:41:38 -0500

    Add LayerWriter for block CIMs
    This commit adds a layer writer that can be used for extracting an image layer tar into a
    Block CIM format.
    
    Existing forked CIM layer writer was renamed to a common base type `cimLayerWriter`.
    Forked CIM layer writer & Block CIM layer writer both now extend this common base type to
    write layers in that specific format.
    
    This commit also removes some code that used `time.Now()` as the default timestamps for
    some files that it creates within the layer CIM. These timestamps cause differences in the
    layer CIMs generated from the same layer tar. This change fixes that.
    
    Signed-off-by: Amit Barve <ambarve@microsoft.com>

---
+++ internal/wclayer/cim/cim_writer_test.go
@@ -0,0 +1,55 @@
+//go:build windows
+
+package cim
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/Microsoft/hcsshim/pkg/cimfs"
+)
+
+func TestSingleFileWriterTypeMismatch(t *testing.T) {
+	if !cimfs.IsBlockCimSupported() {
+		t.Skipf("BlockCIM not supported")
+	}
+
+	layer := &cimfs.BlockCIM{
+		Type:      cimfs.BlockCIMTypeSingleFile,
+		BlockPath: "",
+		CimName:   "",
+	}
+
+	parent := &cimfs.BlockCIM{
+		Type:      cimfs.BlockCIMTypeDevice,
+		BlockPath: "",
+		CimName:   "",
+	}
+
+	_, err := NewBlockCIMLayerWriter(context.TODO(), layer, []*cimfs.BlockCIM{parent})
+	if !errors.Is(err, ErrBlockCIMParentTypeMismatch) {
+		t.Fatalf("expected error `%s`, got `%s`", ErrBlockCIMParentTypeMismatch, err)
+	}
+}
+
+func TestSingleFileWriterInvalidBlockType(t *testing.T) {
+	if !cimfs.IsBlockCimSupported() {
+		t.Skipf("BlockCIM not supported")
+	}
+
+	layer := &cimfs.BlockCIM{
+		BlockPath: "",
+		CimName:   "",
+	}
+
+	parent := &cimfs.BlockCIM{
+		BlockPath: "",
+		CimName:   "",
+	}
+
+	_, err := NewBlockCIMLayerWriter(context.TODO(), layer, []*cimfs.BlockCIM{parent})
+	if !errors.Is(err, ErrBlockCIMWriterNotSupported) {
+		t.Fatalf("expected error `%s`, got `%s`", ErrBlockCIMWriterNotSupported, err)
+	}
+}
