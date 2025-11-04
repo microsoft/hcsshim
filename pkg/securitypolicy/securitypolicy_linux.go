40570794a833959913676401e9443bd3e327bee8
Mahati Chamarthy <mahati.chamarthy@gmail.com>
2025-11-04 17:31:21 +0000

    C-WCOW: PspDriver and hostdata changes in SecurityPolicy pkg
    Signed-off-by: Mahati Chamarthy <mahati.chamarthy@gmail.com>

--- pkg/securitypolicy/securitypolicy_linux.go
+++ pkg/securitypolicy/securitypolicy_linux.go
@@ -4,12 +4,14 @@
 package securitypolicy
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strconv"
 
 	specInternal "github.com/Microsoft/hcsshim/internal/guest/spec"
+	"github.com/Microsoft/hcsshim/pkg/amdsevsnp"
 	"github.com/moby/sys/user"
 	oci "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pkg/errors"
@@ -17,6 +19,28 @@ import (
 
 //nolint:unused
 const osType = "linux"
+
+// validateHostData fetches SNP report (if applicable) and validates `hostData` against
+// HostData set at UVM launch.
+func validateHostData(hostData []byte) error {
+	// If the UVM is not SNP, then don't try to fetch an SNP report.
+	if !amdsevsnp.IsSNP() {
+		return nil
+	}
+	report, err := amdsevsnp.FetchParsedSNPReport(nil)
+	if err != nil {
+		return err
+	}
+
+	if !bytes.Equal(hostData, report.HostData) {
+		return fmt.Errorf(
+			"security policy digest %q doesn't match HostData provided at launch %q",
+			hostData,
+			report.HostData,
+		)
+	}
+	return nil
+}
 
 func ExtendPolicyWithNetworkingMounts(sandboxID string, enforcer SecurityPolicyEnforcer, spec *oci.Spec) error {
 	roSpec := &oci.Spec{
