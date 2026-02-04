microsoft/hcsshim

commit 00ca088f41b4de5d438d3d0ab7bdfbb948e44024

Refactor layer mount functions
This commit uses the newly added WCOW layer parsers and the new type for representing
mounted WCOW layers. LCOW functions are also moved around (and renamed) to follow similar
style as that of WCOW functions.

Signed-off-by: Amit Barve <ambarve@microsoft.com>

internal/hcsoci/resources_wcow.go
@@ -9,7 +9,6 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"os"
 	"path/filepath"
 	"strings"
 
@@ -26,45 +25,23 @@ import (
 	"github.com/Microsoft/hcsshim/internal/schemaversion"
 	"github.com/Microsoft/hcsshim/internal/uvm"
 	"github.com/Microsoft/hcsshim/internal/uvm/scsi"
-	"github.com/Microsoft/hcsshim/internal/wclayer"
 )
 
 const wcowSandboxMountPath = "C:\\SandboxMounts"
 
 func allocateWindowsResources(ctx context.Context, coi *createOptionsInternal, r *resources.Resources, isSandbox bool) error {
-	if coi.Spec == nil || coi.Spec.Windows == nil || coi.Spec.Windows.LayerFolders == nil {
-		return errors.New("field 'Spec.Windows.Layerfolders' is not populated")
-	}
-
-	scratchFolder := coi.Spec.Windows.LayerFolders[len(coi.Spec.Windows.LayerFolders)-1]
-
-	// TODO: Remove this code for auto-creation. Make the caller responsible.
-	// Create the directory for the RW scratch layer if it doesn't exist
-	if _, err := os.Stat(scratchFolder); os.IsNotExist(err) {
-		if err := os.MkdirAll(scratchFolder, 0777); err != nil {
-			return errors.Wrapf(err, "failed to auto-create container scratch folder %s", scratchFolder)
-		}
-	}
-
-	// Create sandbox.vhdx if it doesn't exist in the scratch folder. It's called sandbox.vhdx
-	// rather than scratch.vhdx as in the v1 schema, it's hard-coded in HCS.
-	if _, err := os.Stat(filepath.Join(scratchFolder, "sandbox.vhdx")); os.IsNotExist(err) {
-		if err := wclayer.CreateScratchLayer(ctx, scratchFolder, coi.Spec.Windows.LayerFolders[:len(coi.Spec.Windows.LayerFolders)-1]); err != nil {
-			return errors.Wrap(err, "failed to CreateSandboxLayer")
-		}
-	}
-
 	if coi.Spec.Root == nil {
 		coi.Spec.Root = &specs.Root{}
 	}
 
 	if coi.Spec.Root.Path == "" && (coi.HostingSystem != nil || coi.Spec.Windows.HyperV == nil) {
 		log.G(ctx).Debug("hcsshim::allocateWindowsResources mounting storage")
-		containerRootPath, closer, err := layers.MountWCOWLayers(ctx, coi.actualID, coi.Spec.Windows.LayerFolders, "", coi.HostingSystem)
+		mountedLayers, closer, err := layers.MountWCOWLayers(ctx, coi.actualID, coi.HostingSystem, coi.WCOWLayers)
 		if err != nil {
 			return errors.Wrap(err, "failed to mount container storage")
 		}
-		coi.Spec.Root.Path = containerRootPath
+		coi.Spec.Root.Path = mountedLayers.RootFS
+		coi.mountedWCOWLayers = mountedLayers
 		// If this is the pause container in a hypervisor-isolated pod, we can skip cleanup of
 		// layers, as that happens automatically when the UVM is terminated.
 		if !isSandbox || coi.HostingSystem == nil {

cmd/containerd-shim-runhcs-v1/delete.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -18,10 +17,8 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/Microsoft/hcsshim/internal/hcs"
-	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/memory"
 	"github.com/Microsoft/hcsshim/internal/oc"
-	cimlayer "github.com/Microsoft/hcsshim/internal/wclayer/cim"
 	"github.com/Microsoft/hcsshim/internal/winapi"
 )
 
@@ -126,28 +123,8 @@ The delete command will be executed in the container's bundle as its cwd.
 			fmt.Fprintf(os.Stderr, "failed to delete user %q: %v", username, err)
 		}
 
-		// cleanup the layers mounted for the container. We currently only handle cleanup of CimFS
-		// layers here.  First n-1 values should be the image layerFolders (topmost layer being at
-		// index 0) and the last entry should be the scratch layer
-		var layerFolders []string
-		f, err := os.Open(filepath.Join(bundleFlag, layersFile))
-		if err != nil {
-			if !errors.Is(err, os.ErrNotExist) {
-				fmt.Fprintf(os.Stderr, "open layers file: %s", err)
-			}
-		} else {
-			defer f.Close()
-			if err = json.NewDecoder(f).Decode(&layerFolders); err != nil {
-				fmt.Fprintf(os.Stderr, "decode layers json: %s", err)
-			}
-		}
-		if err == nil && cimlayer.IsCimLayer(layerFolders[0]) {
-			scratchLayerFolderPath := layerFolders[len(layerFolders)-1]
-			err = layers.ReleaseCimFSHostLayers(ctx, scratchLayerFolderPath, idFlag)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "cleanup container %q mounts: %s", idFlag, err)
-			}
-		}
+		// TODO(ambarve):
+		// correctly handle cleanup of cimfs layers in case of shim process crash here.
 
 		if data, err := proto.Marshal(&task.DeleteResponse{
 			ExitedAt:   timestamppb.New(time.Now()),

cmd/containerd-shim-runhcs-v1/main.go
@@ -29,7 +29,6 @@ import (
 
 const usage = ``
 const ttrpcAddressEnv = "TTRPC_ADDRESS"
-const layersFile = "layers.json"
 
 // Add a manifest to get proper Windows version detection.
 //go:generate go run github.com/josephspurrier/goversioninfo/cmd/goversioninfo -platform-specific

cmd/containerd-shim-runhcs-v1/pod.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/oci"
 	"github.com/Microsoft/hcsshim/internal/uvm"
@@ -122,22 +123,15 @@ func createPod(ctx context.Context, events publisher, req *task.CreateTaskReques
 				return nil, err
 			}
 		case *uvm.OptionsWCOW:
+			var layerFolders []string
+			if s.Windows != nil {
+				layerFolders = s.Windows.LayerFolders
+			}
 			wopts := (opts).(*uvm.OptionsWCOW)
-
-			// In order for the UVM sandbox.vhdx not to collide with the actual
-			// nested Argon sandbox.vhdx we append the \vm folder to the last
-			// entry in the list.
-			layersLen := len(s.Windows.LayerFolders)
-			layers := make([]string, layersLen)
-			copy(layers, s.Windows.LayerFolders)
-
-			vmPath := filepath.Join(layers[layersLen-1], "vm")
-			err := os.MkdirAll(vmPath, 0)
+			wopts.BootFiles, err = layers.GetWCOWUVMBootFilesFromLayers(ctx, req.Rootfs, layerFolders)
 			if err != nil {
 				return nil, err
 			}
-			layers[layersLen-1] = vmPath
-			wopts.LayerFolders = layers
 
 			parent, err = uvm.CreateWCOW(ctx, wopts)
 			if err != nil {

cmd/containerd-shim-runhcs-v1/rootfs.go
@@ -1,144 +1,0 @@
-//go:build windows
-
-package main
-
-import (
-	"encoding/json"
-	"fmt"
-	"path/filepath"
-	"strings"
-
-	"github.com/Microsoft/hcsshim/internal/layers"
-	"github.com/containerd/containerd/api/types"
-	"github.com/containerd/containerd/mount"
-	"github.com/containerd/errdefs"
-)
-
-// validateRootfsAndLayers checks to ensure we have appropriate information
-// for setting up the container's root filesystem. It ensures the following:
-// - One and only one of Rootfs or LayerFolders can be provided.
-// - If LayerFolders are provided, there are at least two entries.
-// - If Rootfs is provided, there is a single entry and it does not have a Target set.
-func validateRootfsAndLayers(rootfs []*types.Mount, layerFolders []string) error {
-	if len(rootfs) > 0 && len(layerFolders) > 0 {
-		return fmt.Errorf("cannot pass both a rootfs mount and Windows.LayerFolders: %w", errdefs.ErrFailedPrecondition)
-	}
-	if len(rootfs) == 0 && len(layerFolders) == 0 {
-		return fmt.Errorf("must pass either a rootfs mount or Windows.LayerFolders: %w", errdefs.ErrFailedPrecondition)
-	}
-	if len(rootfs) > 0 {
-		// We have a rootfs.
-
-		if len(rootfs) > 1 {
-			return fmt.Errorf("expected a single rootfs mount: %w", errdefs.ErrFailedPrecondition)
-		}
-		if rootfs[0].Target != "" {
-			return fmt.Errorf("rootfs mount is missing Target path: %w", errdefs.ErrFailedPrecondition)
-		}
-	} else {
-		// We have layerFolders.
-
-		if len(layerFolders) < 2 {
-			return fmt.Errorf("must pass at least two Windows.LayerFolders: %w", errdefs.ErrFailedPrecondition)
-		}
-	}
-
-	return nil
-}
-
-// parseLegacyRootfsMount parses the rootfs mount format that we have traditionally
-// used for both Linux and Windows containers.
-// The mount format consists of:
-//   - The scratch folder path in m.Source, which contains sandbox.vhdx.
-//   - A mount option in the form parentLayerPaths=<JSON>, where JSON is an array of
-//     string paths to read-only layer directories. The exact contents of these layer
-//     directories are intepreteted differently for Linux and Windows containers.
-func parseLegacyRootfsMount(m *types.Mount) (string, []string, error) {
-	// parentLayerPaths are passed in layerN, layerN-1, ..., layer 0
-	//
-	// The OCI spec expects:
-	//   layerN, layerN-1, ..., layer0, scratch
-	var parentLayerPaths []string
-	for _, option := range m.Options {
-		if strings.HasPrefix(option, mount.ParentLayerPathsFlag) {
-			err := json.Unmarshal([]byte(option[len(mount.ParentLayerPathsFlag):]), &parentLayerPaths)
-			if err != nil {
-				return "", nil, fmt.Errorf("unmarshal parent layer paths from mount: %w: %w", err, errdefs.ErrFailedPrecondition)
-			}
-			// Would perhaps be worthwhile to check for unrecognized options and return an error,
-			// but since this is a legacy layer mount we don't do that to avoid breaking anyone.
-			break
-		}
-	}
-	return m.Source, parentLayerPaths, nil
-}
-
-// getLCOWLayers returns a layers.LCOWLayers describing the rootfs that should be set up
-// for an LCOW container. It takes as input the set of rootfs mounts and the layer folders
-// from the OCI spec, it is assumed that these were previously checked with validateRootfsAndLayers
-// such that only one of them is populated.
-func getLCOWLayers(rootfs []*types.Mount, layerFolders []string) (*layers.LCOWLayers, error) {
-	legacyLayer := func(scratchLayer string, parentLayers []string) *layers.LCOWLayers {
-		// Each read-only layer should have a layer.vhd, and the scratch layer should have a sandbox.vhdx.
-		roLayers := make([]*layers.LCOWLayer, 0, len(parentLayers))
-		for _, parentLayer := range parentLayers {
-			roLayers = append(
-				roLayers,
-				&layers.LCOWLayer{
-					VHDPath: filepath.Join(parentLayer, "layer.vhd"),
-				},
-			)
-		}
-		return &layers.LCOWLayers{
-			Layers:         roLayers,
-			ScratchVHDPath: filepath.Join(scratchLayer, "sandbox.vhdx"),
-		}
-	}
-	// Due to previous validation, we know that for a Linux container we either have LayerFolders, or
-	// a single rootfs mount.
-	if len(layerFolders) > 0 {
-		return legacyLayer(layerFolders[len(layerFolders)-1], layerFolders[:len(layerFolders)-1]), nil
-	}
-	m := rootfs[0]
-	switch m.Type {
-	case "lcow-layer":
-		scratchLayer, parentLayers, err := parseLegacyRootfsMount(rootfs[0])
-		if err != nil {
-			return nil, err
-		}
-		return legacyLayer(scratchLayer, parentLayers), nil
-	case "lcow-partitioned-layer":
-		var (
-			scratchPath string
-			layerData   []struct {
-				Path      string
-				Partition uint64
-			}
-		)
-		for _, opt := range m.Options {
-			if optPrefix := "scratch="; strings.HasPrefix(opt, optPrefix) {
-				scratchPath = strings.TrimPrefix(opt, optPrefix)
-			} else if optPrefix := "parent-partitioned-layers="; strings.HasPrefix(opt, optPrefix) {
-				layerJSON := strings.TrimPrefix(opt, optPrefix)
-				if err := json.Unmarshal([]byte(layerJSON), &layerData); err != nil {
-					return nil, err
-				}
-			} else {
-				return nil, fmt.Errorf("unrecognized %s mount option: %s", m.Type, opt)
-			}
-		}
-		roLayers := make([]*layers.LCOWLayer, 0, len(layerData))
-		for _, layer := range layerData {
-			roLayers = append(
-				roLayers,
-				&layers.LCOWLayer{
-					VHDPath:   layer.Path,
-					Partition: layer.Partition,
-				},
-			)
-		}
-		return &layers.LCOWLayers{Layers: roLayers, ScratchVHDPath: scratchPath}, nil
-	default:
-		return nil, fmt.Errorf("unrecognized rootfs mount type: %s", m.Type)
-	}
-}

cmd/containerd-shim-runhcs-v1/service_internal.go
@@ -27,13 +27,6 @@ import (
 
 var empty = &emptypb.Empty{}
 
-// TODO(ambarve): Once we can vendor containerd 2.0 in hcsshim, we should directly reference these types from
-// containerd module
-const (
-	LegacyMountType string = "windows-layer"
-	CimFSMountType  string = "CimFS"
-)
-
 // getPod returns the pod this shim is tracking or else returns `nil`. It is the
 // callers responsibility to verify that `s.isSandbox == true` before calling
 // this method.
@@ -120,53 +113,6 @@ func (s *service) createInternal(ctx context.Context, req *task.CreateTaskReques
 		}
 		if spec.Windows.HyperV == nil {
 			spec.Windows.HyperV = &specs.WindowsHyperV{}
-		}
-	}
-
-	var layerFolders []string
-	if spec.Windows != nil {
-		layerFolders = spec.Windows.LayerFolders
-	}
-	if err := validateRootfsAndLayers(req.Rootfs, layerFolders); err != nil {
-		return nil, err
-	}
-
-	// Only work with Windows here.
-	// Parsing of the rootfs mount for Linux containers occurs later.
-	if spec.Linux == nil && len(req.Rootfs) > 0 {
-		// For Windows containers, we work with LayerFolders throughout
-		// much of the creation logic in the shim. If we were given a
-		// rootfs mount, convert it to LayerFolders here.
-		m := req.Rootfs[0]
-		if m.Type != LegacyMountType && m.Type != CimFSMountType {
-			return nil, fmt.Errorf("unsupported Windows mount type: %s", m.Type)
-		} else if m.Type == CimFSMountType && (shimOpts.SandboxIsolation == runhcsopts.Options_HYPERVISOR) {
-			// For CIMFS layers only process isolation is supported right now.
-			return nil, fmt.Errorf("cimfs doesn't support hyperv isolation")
-		}
-
-		source, parentLayerPaths, err := parseLegacyRootfsMount(m)
-		if err != nil {
-			return nil, err
-		}
-
-		// Append the parents
-		spec.Windows.LayerFolders = append(spec.Windows.LayerFolders, parentLayerPaths...)
-		// Append the scratch
-		spec.Windows.LayerFolders = append(spec.Windows.LayerFolders, source)
-
-		if m.Type == CimFSMountType {
-			// write the layers to a file so that it can be used for proper cleanup during shim
-			// delete. We can't write to the config.json as it is read-only for shim.
-			f, err = os.Create(filepath.Join(req.Bundle, layersFile))
-			if err != nil {
-				return nil, err
-			}
-			if err := json.NewEncoder(f).Encode(spec.Windows.LayerFolders); err != nil {
-				f.Close()
-				return nil, err
-			}
-			f.Close()
 		}
 	}
 

cmd/containerd-shim-runhcs-v1/task_hcs.go
@@ -35,6 +35,7 @@ import (
 	hcsschema "github.com/Microsoft/hcsshim/internal/hcs/schema2"
 	"github.com/Microsoft/hcsshim/internal/hcsoci"
 	"github.com/Microsoft/hcsshim/internal/jobcontainers"
+	"github.com/Microsoft/hcsshim/internal/layers"
 	"github.com/Microsoft/hcsshim/internal/log"
 	"github.com/Microsoft/hcsshim/internal/memory"
 	"github.com/Microsoft/hcsshim/internal/oc"
@@ -82,23 +83,15 @@ func newHcsStandaloneTask(ctx context.Context, events publisher, req *task.Creat
 				return nil, err
 			}
 		case *uvm.OptionsWCOW:
+			var layerFolders []string
+			if s.Windows != nil {
+				layerFolders = s.Windows.LayerFolders
+			}
 			wopts := (opts).(*uvm.OptionsWCOW)
-
-			// In order for the UVM sandbox.vhdx not to collide with the actual
-			// nested Argon sandbox.vhdx we append the \vm folder to the last
-			// entry in the list.
-			layersLen := len(s.Windows.LayerFolders)
-			layers := make([]string, layersLen)
-			copy(layers, s.Windows.LayerFolders)
-
-			vmPath := filepath.Join(layers[layersLen-1], "vm")
-			err := os.MkdirAll(vmPath, 0)
+			wopts.BootFiles, err = layers.GetWCOWUVMBootFilesFromLayers(ctx, req.Rootfs, layerFolders)
 			if err != nil {
 				return nil, err
 			}
-			layers[layersLen-1] = vmPath
-			wopts.LayerFolders = layers
-
 			parent, err = uvm.CreateWCOW(ctx, wopts)
 			if err != nil {
 				return nil, err
@@ -140,8 +133,24 @@ func createContainer(
 		resources *resources.Resources
 	)
 
+	var wcowLayers layers.WCOWLayers
+	var lcowLayers *layers.LCOWLayers
+	var layerFolders []string
+	if s.Windows != nil {
+		layerFolders = s.Windows.LayerFolders
+	}
+	if s.Linux != nil {
+		lcowLayers, err = layers.ParseLCOWLayers(rootfs, layerFolders)
+	} else {
+		wcowLayers, err = layers.ParseWCOWLayers(rootfs, layerFolders)
+	}
+	if err != nil {
+		return nil, nil, err
+	}
+
 	if oci.IsJobContainer(s) {
-		container, resources, err = jobcontainers.Create(ctx, id, s)
+		opts := jobcontainers.CreateOptions{WCOWLayers: wcowLayers}
+		container, resources, err = jobcontainers.Create(ctx, id, s, opts)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -152,18 +161,10 @@ func createContainer(
 			Spec:             s,
 			HostingSystem:    parent,
 			NetworkNamespace: netNS,
+			LCOWLayers:       lcowLayers,
+			WCOWLayers:       wcowLayers,
 		}
-		if s.Linux != nil {
-			var layerFolders []string
-			if s.Windows != nil {
-				layerFolders = s.Windows.LayerFolders
-			}
-			lcowLayers, err := getLCOWLayers(rootfs, layerFolders)
-			if err != nil {
-				return nil, nil, err
-			}
-			opts.LCOWLayers = lcowLayers
-		}
+
 		if shimOpts != nil {
 			opts.ScaleCPULimitsToSandbox = shimOpts.ScaleCpuLimitsToSandbox
 		}

commit 941bfe151e508e05d95e72e23efd39d19b1ec164

WCOW: restore support for client-mounted roots
Commit 00ca088f41b4de5d438d3d0ab7bdfbb948e44024 broke starting
process-isolated containers when Root.Path is set in the container spec.
Root.Path is set by clients such as Docker which mount and unmount the
container's root filesystem themselves. Mounting is skipped, which also
skips over setting coi.mountedWCOWLayers. The
hcsoci.createWindowsContainerDocument function unconditionally
dereferences that field, resulting in a nil-dereference panic when
mounting is skipped. Restore the old behaviour by synthesizing the
MountedWCOWLayers when Root.Path is set.

Signed-off-by: Cory Snider <csnider@mirantis.com>

internal/hcsoci/resources_wcow.go
@@ -34,18 +34,26 @@ func allocateWindowsResources(ctx context.Context, coi *createOptionsInternal, r
 		coi.Spec.Root = &specs.Root{}
 	}
 
-	if coi.Spec.Root.Path == "" && (coi.HostingSystem != nil || coi.Spec.Windows.HyperV == nil) {
-		log.G(ctx).Debug("hcsshim::allocateWindowsResources mounting storage")
-		mountedLayers, closer, err := layers.MountWCOWLayers(ctx, coi.actualID, coi.HostingSystem, coi.WCOWLayers)
-		if err != nil {
-			return errors.Wrap(err, "failed to mount container storage")
-		}
-		coi.Spec.Root.Path = mountedLayers.RootFS
-		coi.mountedWCOWLayers = mountedLayers
-		// If this is the pause container in a hypervisor-isolated pod, we can skip cleanup of
-		// layers, as that happens automatically when the UVM is terminated.
-		if !isSandbox || coi.HostingSystem == nil {
-			r.SetLayers(closer)
+	if coi.HostingSystem != nil || coi.Spec.Windows.HyperV == nil {
+		if coi.Spec.Root.Path == "" {
+			log.G(ctx).Debug("hcsshim::allocateWindowsResources mounting storage")
+			mountedLayers, closer, err := layers.MountWCOWLayers(ctx, coi.actualID, coi.HostingSystem, coi.WCOWLayers)
+			if err != nil {
+				return errors.Wrap(err, "failed to mount container storage")
+			}
+			coi.Spec.Root.Path = mountedLayers.RootFS
+			coi.mountedWCOWLayers = mountedLayers
+			// If this is the pause container in a hypervisor-isolated pod, we can skip cleanup of
+			// layers, as that happens automatically when the UVM is terminated.
+			if !isSandbox || coi.HostingSystem == nil {
+				r.SetLayers(closer)
+			}
+		} else {
+			l, err := layers.MakeMountedWCOWLayers(ctx, coi.WCOWLayers, coi.Spec.Root.Path)
+			if err != nil {
+				return errors.Wrap(err, "failed to use pre-mounted container storage")
+			}
+			coi.mountedWCOWLayers = l
 		}
 	}
 

internal/layers/wcow_mount.go
@@ -49,6 +49,44 @@ func MountWCOWLayers(ctx context.Context, containerID string, vm *uvm.UtilityVM,
 	}
 }
 
+// MakeMountedWCOWLayers creates a MountedWCOWLayers structure from wl, like
+// [MountWCOWLayers], without actually mounting anything.
+func MakeMountedWCOWLayers(ctx context.Context, wl WCOWLayers, mountPath string) (*MountedWCOWLayers, error) {
+	switch l := wl.(type) {
+	case *wcowWCIFSLayers:
+		layersWithID := []MountedWCOWLayer{}
+		for _, l := range l.layerPaths {
+			layerID, err := wclayer.LayerID(ctx, l)
+			if err != nil {
+				return nil, err
+			}
+			layersWithID = append(layersWithID, MountedWCOWLayer{
+				LayerID:     layerID.String(),
+				MountedPath: l,
+			})
+		}
+
+		return &MountedWCOWLayers{
+			RootFS:            mountPath,
+			MountedLayerPaths: layersWithID,
+		}, nil
+	case *wcowForkedCIMLayers, *wcowBlockCIMLayers:
+		layerID, err := cimlayer.LayerID(mountPath)
+		if err != nil {
+			return nil, err
+		}
+		return &MountedWCOWLayers{
+			RootFS: mountPath,
+			MountedLayerPaths: []MountedWCOWLayer{{
+				LayerID:     layerID,
+				MountedPath: mountPath,
+			}},
+		}, nil
+	default:
+		return nil, fmt.Errorf("invalid layer type %T", wl)
+	}
+}
+
 // Represents a single layer that is mounted and ready to use. Depending on the type of
 // layers each individual layer may or may not be mounted. However, HCS still needs paths
 // of individual layers and a unique ID for each layer.