54da180804b20df2630997c9ecf54d6868e46058
Ryan Keith (from Dev Box) <ryankeith@microsoft.com>
2026-05-06 11:23:15 -0700

    Enable scrubbing by default

--- internal/hcsoci/create.go
+++ internal/hcsoci/create.go
@@ -129,7 +129,7 @@ func initializeCreateOptions(ctx context.Context, createOptions *CreateOptions) 
 	}
 
 	log.G(ctx).WithFields(logrus.Fields{
-		"options": log.Format(ctx, createOptions),
+		"options": log.FormatScrub(ctx, createOptions, log.ScrubCreateOptions),
 		"schema":  log.Format(ctx, coi.actualSchemaVersion),
 	}).Debug("hcsshim::initializeCreateOptions")
 
